b707e00b10bea75d705f4350455bbefe0eef4b23
dougshidong <doug.shidong@gmail.com>
2019-02-06 10:00:04 -0500

    Working linear advection with p+1 order of convergence

--- src/main.cpp
+++ src/main.cpp
@@ -10,8 +10,13 @@ int main()
     try
     {
         std::cout << "Starting program..." << std::endl;
-        PHiLiP::PDE<2, double> adv;
-        adv.run();
+
+        PHiLiP::PDE<1, double> adv11(1); adv11.run();
+        PHiLiP::PDE<1, double> adv12(2); adv12.run();
+        PHiLiP::PDE<1, double> adv13(3); adv13.run();
+        //PHiLiP::PDE<2, double> adv21(1); adv21.run();
+        //PHiLiP::PDE<2, double> adv22(2); adv22.run();
+        //PHiLiP::PDE<2, double> adv23(3); adv23.run();
     }
     catch (std::exception &exc)
     {

1266c0226a5b68fd7c063f2efe57e31928bac1f1
Doug Shi-Dong <doug.shidong@gmail.com>
2019-02-07 00:12:01 -0500

    Using Legendre basis

--- src/main.cpp
+++ src/main.cpp
@@ -11,21 +11,14 @@ int main()
     {
         std::cout << "Starting program..." << std::endl;
 
-        PHiLiP::PDE<1, double> adv1_0(0); const int failure1_0 = adv1_0.run();
-        PHiLiP::PDE<1, double> adv1_1(1); const int failure1_1 = adv1_1.run();
-        PHiLiP::PDE<1, double> adv1_2(2); const int failure1_2 = adv1_2.run();
-        PHiLiP::PDE<1, double> adv1_3(3); const int failure1_3 = adv1_3.run();
+        PHiLiP::PDE<1, double> adv1_0(0); const int failure1_0 = adv1_0.run(); if (failure1_0) return 1;
+        PHiLiP::PDE<1, double> adv1_1(1); const int failure1_1 = adv1_1.run(); if (failure1_1) return 1;
+        PHiLiP::PDE<1, double> adv1_2(2); const int failure1_2 = adv1_2.run(); if (failure1_2) return 1;
+        PHiLiP::PDE<1, double> adv1_3(3); const int failure1_3 = adv1_3.run(); if (failure1_3) return 1;
                                                                  
-        PHiLiP::PDE<2, double> adv2_0(0); const int failure2_0 = adv2_0.run();
-        PHiLiP::PDE<2, double> adv2_1(1); const int failure2_1 = adv2_1.run();
-        
-        if (failure1_0) return 1;
-        if (failure1_1) return 1;
-        if (failure1_2) return 1;
-        if (failure1_3) return 1;
+        PHiLiP::PDE<2, double> adv2_0(0); const int failure2_0 = adv2_0.run(); if (failure2_0) return 1;
+        PHiLiP::PDE<2, double> adv2_1(1); const int failure2_1 = adv2_1.run(); if (failure2_1) return 1;
 
-        if (failure2_0) return 1;
-        if (failure2_1) return 1;
     }
     catch (std::exception &exc)
     {

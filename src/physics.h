30cc81b2c6c306cb5137c20b1feff4b7e5289e36
dougshidong <doug.shidong@gmail.com>
2019-04-19 03:13:07 -0400

    DiscontinuousGalerkin blind to LinearAdvection
    DiscontinuousGalerkin currently calls LinearAdvection functions to evaluate its flux.
    Basically DiscontinuousGalerkin does not compute its own velocity anymore.

--- src/physics.h
+++ src/physics.h
@@ -13,6 +13,7 @@ namespace PHiLiP
     class Physics
     {
     public:
+
         virtual void manufactured_solution (const Point<dim,double> pos, real &solution);
 
         // PDE is given by
@@ -21,6 +22,10 @@ namespace PHiLiP
         // Convective fluxes that will be differentiated once in space
         virtual void convective_flux (const real &solution,
                                       Tensor <1, dim, real> &conv_flux) = 0;
+
+        // Spectral radius of convective term Jacobian
+        // Used for scalar dissipation
+        virtual Tensor <1, dim, real> convective_eigenvalues (const real &/*solution*/) = 0;
 
         // Dissipative fluxes that will be differentiated once in space
         virtual void dissipative_flux (const real & solution,
@@ -31,8 +36,14 @@ namespace PHiLiP
         virtual void source_term (const Point<dim,double> pos,
                                   const real &solution,
                                   real &source) = 0;
+    protected:
+        // Some constants used to define manufactured solution
+        const double freq_x = 1*0.59/dim, freq_y = 2*0.81/dim, freq_z = 3*0.76/dim;
+        const double offs_x = 1, offs_y = 0.2, offs_z = 0.5;
     };
 
+    // This class with create a new Physics object corresponding to the pde_type
+    // given as a user input
     template <int dim, int nstate, typename real>
     class PhysicsFactory
     {
@@ -48,8 +59,12 @@ namespace PHiLiP
         // State variable:   u
         // -\nabla \cdot (c*u) = source
 
+    public:
         // Convective flux:  c*u
         void convective_flux (const real &solution, Tensor <1, dim, real> &conv_flux);
+
+        // Spectral radius of convective term Jacobian is simply the maximum 'c'
+        Tensor <1, dim, real> convective_eigenvalues (const real &/*solution*/);
 
         // Dissipative flux: 0
         void dissipative_flux (const real &solution,
@@ -58,6 +73,11 @@ namespace PHiLiP
 
         // Source term is zero or depends on manufactured solution
         void source_term (const Point<dim,double> pos, const real &solution, real &source);
+
+    protected:
+        // Linear advection speed:  c
+        Tensor <1, dim, real> advection_speed ();
+
     };
 
     template <int dim, int nstate, typename real>
@@ -68,8 +88,12 @@ namespace PHiLiP
         // Fdiss = -grad(u)
         // -\nabla \cdot (\nabla u) = source
 
+    public:
         // Convective flux:  0
         void convective_flux (const real &solution, Tensor <1, dim, real> &conv_flux);
+
+        // Spectral radius of convective term Jacobian is 0
+        Tensor <1, dim, real> convective_eigenvalues (const real &/*solution*/);
 
         // Dissipative flux: u
         void dissipative_flux (const real &solution,
@@ -88,8 +112,12 @@ namespace PHiLiP
         // Fdiss = -grad(u)
         // \nabla \cdot (c*u) -\nabla \cdot (\nabla u) = source
 
+    public:
         // Convective flux:  0
         void convective_flux (const real &solution, Tensor <1, dim, real> &conv_flux);
+
+        // Spectral radius of convective term Jacobian is 'c'
+        Tensor <1, dim, real> convective_eigenvalues (const real &/*solution*/);
 
         // Dissipative flux: u
         void dissipative_flux (const real &solution,
@@ -98,6 +126,10 @@ namespace PHiLiP
 
         // Source term is zero or depends on manufactured solution
         void source_term (const Point<dim,double> pos, const real &solution, real &source);
+
+    protected:
+        // Linear advection speed:  c
+        Tensor <1, dim, real> advection_speed ();
     };
 
 } // end of PHiLiP namespace

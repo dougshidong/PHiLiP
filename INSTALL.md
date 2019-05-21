c7168439509f24c73faaed28a743b09f05838dd1
dougshidong <doug.shidong@gmail.com>
2019-05-20 20:01:35 -0400

    Add some documentation

---
+++ INSTALL.md
@@ -0,0 +1,57 @@
+#Installation
+
+## deal.II
+
+This is the main library being used by this code.
+
+The deal.II library has been setup with the following options
+
+>  deal.II configuration:
+>        CMAKE_BUILD_TYPE:       DebugRelease
+>        BUILD_SHARED_LIBS:      ON
+>        CMAKE_INSTALL_PREFIX:   /home/ddong/Codes/dealii/install
+>        CMAKE_SOURCE_DIR:       /home/ddong/Codes/dealii
+>                                (version 9.0.1, shortrev 097bf59e49)
+>        CMAKE_BINARY_DIR:       /home/ddong/Codes/dealii/build
+>        CMAKE_CXX_COMPILER:     GNU 7.3.0 on platform Linux x86_64
+>                                /usr/bin/mpicxx
+>
+>  Configured Features (DEAL_II_ALLOW_BUNDLED = ON, DEAL_II_ALLOW_AUTODETECTION = ON):
+>      ( DEAL_II_WITH_64BIT_INDICES = OFF )
+>        DEAL_II_WITH_ADOLC set up with external dependencies
+>        DEAL_II_WITH_ARPACK set up with external dependencies
+>      ( DEAL_II_WITH_ASSIMP = OFF )
+>        DEAL_II_WITH_BOOST set up with external dependencies
+>      ( DEAL_II_WITH_CUDA = OFF )
+>        DEAL_II_WITH_CXX14 = ON
+>        DEAL_II_WITH_CXX17 = ON
+>        DEAL_II_WITH_GMSH set up with external dependencies
+>        DEAL_II_WITH_GSL set up with external dependencies
+>      ( DEAL_II_WITH_HDF5 = OFF )
+>        DEAL_II_WITH_LAPACK set up with external dependencies
+>        DEAL_II_WITH_METIS set up with external dependencies
+>        DEAL_II_WITH_MPI set up with external dependencies
+>        DEAL_II_WITH_MUPARSER set up with bundled packages
+>      ( DEAL_II_WITH_NANOFLANN = OFF )
+>      ( DEAL_II_WITH_NETCDF = OFF )
+>        DEAL_II_WITH_OPENCASCADE set up with external dependencies
+>        DEAL_II_WITH_P4EST set up with external dependencies
+>        DEAL_II_WITH_PETSC set up with external dependencies
+>        DEAL_II_WITH_SCALAPACK set up with external dependencies
+>        DEAL_II_WITH_SLEPC set up with external dependencies
+>        DEAL_II_WITH_SUNDIALS set up with external dependencies
+>      ( DEAL_II_WITH_THREADS = OFF )
+>        DEAL_II_WITH_TRILINOS set up with external dependencies
+>        DEAL_II_WITH_UMFPACK set up with external dependencies
+>        DEAL_II_WITH_ZLIB set up with external dependencies
+>
+>  Component configuration:
+>      ( DEAL_II_COMPONENT_DOCUMENTATION = OFF )
+>        DEAL_II_COMPONENT_EXAMPLES
+>      ( DEAL_II_COMPONENT_PACKAGE = OFF )
+>      ( DEAL_II_COMPONENT_PYTHON_BINDINGS = OFF )
+>
+>  Detailed information (compiler flags, feature configuration) can be found in detailed.log
+>
+>  Run  $ make info  to print a help message with a list of top level targets
+
--- src/parameters/all_parameters.h
+++ src/parameters/all_parameters.h
@@ -12,34 +12,52 @@ namespace Parameters
 {
     using namespace dealii;
 
-    /// Main parameter class that contains the various other sub-parameter classes
+    /// Main parameter class that contains the various other sub-parameter classes.
     class AllParameters
     {
     public:
+        /// Constructor
+        AllParameters();
 
+        /// Contains parameters for manufactured convergence study
         ManufacturedConvergenceStudyParam manufactured_convergence_study_param;
+        /// Contains parameters for ODE solver
         ODESolverParam ode_solver_param;
+        /// Contains parameters for linear solver
         LinearSolverParam linear_solver_param;
 
+        /// Number of dimensions. Note that it has to match the executable PHiLiP_xD
         unsigned int dimension;
+
+        /// Currently allows to solve advection, diffusion, convection-diffusion
         enum PartialDifferentialEquation { advection, diffusion, convection_diffusion };
+        /// Store the PDE type to be solved
         PartialDifferentialEquation pde_type;
 
+        /// Currently only Lax-Friedrichs can be used as an input parameter
         enum ConvectiveNumericalFlux { lax_friedrichs };
+        /// Store convective flux type
         ConvectiveNumericalFlux conv_num_flux_type;
 
+        /// Currently only symmetric internal penalty can be used as an input parameter
         enum DissipativeNumericalFlux { symm_internal_penalty };
+        /// Store diffusive flux type
         DissipativeNumericalFlux diss_num_flux_type;
 
-        AllParameters();
-        //FunctionParser<dim> initial_conditions;
-        //BoundaryConditions  boundary_conditions[max_n_boundaries];
+        /// Declare parameters that can be set as inputs and set up the default options
+        /** This subroutine should call the sub-parameter classes static declare_parameters()
+          * such that each sub-parameter class is responsible to declare their own parameters.
+          */
         static void declare_parameters (ParameterHandler &prm);
+
+        /// Retrieve parameters from ParameterHandler
+        /** This subroutine should call the sub-parameter classes static parse_parameters()
+          * such that each sub-parameter class is responsible to parse their own parameters.
+          */
         void parse_parameters (ParameterHandler &prm);
 
-        //Parameters::Refinement::declare_parameters (prm);
-        //Parameters::Flux::declare_parameters (prm);
-        //Parameters::Output::declare_parameters (prm);
+        //FunctionParser<dim> initial_conditions;
+        //BoundaryConditions  boundary_conditions[max_n_boundaries];
     };  
 }
 

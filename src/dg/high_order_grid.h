7ed44b8c347ca0bbc2bfd0950177bf87e8d975ac
Doug Shi-Dong <doug.shidong@gmail.com>
2019-10-01 21:09:34 -0400

    Cleaned up the test. Currently passing.

--- src/dg/high_order_grid.h
+++ src/dg/high_order_grid.h
@@ -8,17 +8,21 @@
 
 #include <deal.II/dofs/dof_handler.h>
 
+#include <deal.II/distributed/solution_transfer.h>
+
 #include <deal.II/lac/vector.h>
 
 #include "parameters/all_parameters.h"
 
 namespace PHiLiP {
 
-/** This class is used to generate and control the high-order nodes given a Triangulation and a Manifold.
+/** This HighOrderGrid class basically contains all the different part necessary to generate
+ *  a dealii::MappingFEField that corresponds to the current Triangulation and attached Manifold.
+ *  Once the high order grid is generated, the mesh can be deformed by assigning different values to the
+ *  nodes vector. The dof_handler_grid is used to access and loop through those nodes.
  *  This will especially be useful when performing shape optimization, where the surface and volume 
  *  nodes will need to be displaced.
  */
-//template <int dim, typename real, typename VectorType , typename DoFHandlerType>
 template <int dim = PHILIP_DIM, typename real = double, typename VectorType = dealii::LinearAlgebra::distributed::Vector<double>, typename DoFHandlerType = dealii::DoFHandler<PHILIP_DIM>>
 class HighOrderGrid
 {
@@ -26,20 +30,18 @@ public:
     /// Principal constructor that will call delegated constructor.
     HighOrderGrid(const Parameters::AllParameters *const parameters_input, const unsigned int max_degree, dealii::Triangulation<dim> *const triangulation_input);
 
-    /// Needed after the mesh is refined
+    /// Needed to allocate the correct number of nodes when initializing and after the mesh is refined
     void allocate();
 
+    /// Return a MappingFEField that corresponds to the current node locations
     dealii::MappingFEField<dim,dim,VectorType,DoFHandlerType> get_MappingFEField();
 
     const Parameters::AllParameters *const all_parameters; ///< Pointer to all parameters
 
-    /// Maximum degree of the grid.
+    /// Maximum degree of the geometry polynomial representing the grid.
     const unsigned int max_degree;
 
     dealii::Triangulation<dim> *triangulation; ///< Mesh
-
-    /// Get evaluate high-order grid from Triangulation and Manifold
-
 
     /// Degrees of freedom handler for the high-order grid
     dealii::DoFHandler<dim> dof_handler_grid;
@@ -50,8 +52,7 @@ public:
     /// List of surface points
     dealii::LinearAlgebra::distributed::Vector<double> surface_nodes;
 
-
-    /// RBF mesh deformation
+    /// RBF mesh deformation  -  To be done
     void deform_mesh(dealii::LinearAlgebra::distributed::Vector<double> surface_displacements);
 
     /// Evaluate cell metric Jacobian
@@ -66,17 +67,35 @@ public:
     //dealii::Tensor<2,dim,real> cell_jacobian (const typename dealii::Triangulation<dim,spacedim>::cell_iterator &cell, const dealii::Point<dim> &point) const override
     //{
     //}
+    //
 
 
+    /** Prepares the solution transfer such that the curved refined grid is on top of the curved coarse grid.
+     *  This function needs to be called before Triangulation::execute_coarsening_and_refinement() or Triangulation::refine_global()
+     */
+    void prepare_for_coarsening_and_refinement();
+    /** Transfers the coarse curved curve onto the fine curved grid.
+     *  This function needs to be called after Triangulation::execute_coarsening_and_refinement() or Triangulation::refine_global()
+     */
+    void execute_coarsening_and_refinement();
 
-    /// Using system of FE_Q to represent the grid
+    /// Use Lagrange polynomial to represent the spatial location.
     const dealii::FE_Q<dim>     fe_q;
+    /// Using system of polynomials to represent the x, y, and z directions.
     const dealii::FESystem<dim> fe_system;
 
     dealii::IndexSet locally_owned_dofs_grid; ///< Locally own degrees of freedom for the grid
     dealii::IndexSet ghost_dofs_grid; ///< Locally relevant ghost degrees of freedom for the grid
     dealii::IndexSet locally_relevant_dofs_grid; ///< Union of locally owned degrees of freedom and relevant ghost degrees of freedom for the grid
 protected:
+
+    /// Used for the SolutionTransfer when performing grid adaptation.
+    dealii::LinearAlgebra::distributed::Vector<double> old_nodes;
+
+    /** Transfers the coarse curved curve onto the fine curved grid.
+     *  Used in prepare_for_coarsening_and_refinement() and execute_coarsening_and_refinement()
+     */
+    dealii::parallel::distributed::SolutionTransfer<dim, dealii::LinearAlgebra::distributed::Vector<double>, dealii::DoFHandler<dim>> solution_transfer;
 
     MPI_Comm mpi_communicator; ///< MPI communicator
     dealii::ConditionalOStream pcout; ///< Parallel std::cout that only outputs on mpi_rank==0

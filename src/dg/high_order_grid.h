89dea0ccbca89b22e3755007599d5a47007cb717
Doug Shi-Dong <doug.shidong@gmail.com>
2019-10-21 23:04:23 -0400

    Add function to evaluate Jacobian validity
    And some function to fix it, but it doesn't work yet.

--- src/dg/high_order_grid.h
+++ src/dg/high_order_grid.h
@@ -15,6 +15,10 @@
 
 #include "parameters/all_parameters.h"
 
+#include <deal.II/numerics/data_postprocessor.h>
+#include <deal.II/numerics/data_out.h>
+#include <deal.II/numerics/data_out_dof_data.h>
+
 namespace PHiLiP {
 
 /** This HighOrderGrid class basically contains all the different part necessary to generate
@@ -65,13 +69,82 @@ public:
     dealii::DoFHandler<dim> dof_handler_grid;
 
     /// Current nodal coefficients of the high-order grid.
+    /** Note that this contains all \<dim\> directions.
+     *  By convention, the DoF representing the z-direction follows the DoF representing
+     *  the y-direction, which follows the one representing the x-direction such that
+     *  the integer division "idof_index / dim" gives the coordinates related to the same
+     *  point.
+     */
     Vector nodes;
 
-    /// List of surface points
-    Vector surface_nodes;
+    /// List of surface nodes.
+    /** Note that this contains all \<dim\> directions.
+     *  By convention, the DoF representing the z-direction follows the DoF representing
+     *  the y-direction, which follows the one representing the x-direction such that
+     *  the integer division "idof_index / dim" gives the coordinates related to the same
+     *  point.
+     */
+    std::vector<real> local_surface_nodes;
+    /// List of surface node indices
+    std::vector<unsigned int> locally_owned_surface_nodes_indices;
+    /// List of surface node boundary IDs, corresponding to locally_owned_surface_nodes_indices
+    std::vector<unsigned int> locally_owned_surface_nodes_boundary_id;
+
+    void test_jacobian(); ///< Test metric Jacobian
+
+    /// Evaluates Jacobian of a cell given some points using a global solution vector
+    std::vector<real> evaluate_jacobian_at_points(
+        const VectorType &solution,
+        const typename DoFHandlerType::cell_iterator &cell,
+        const std::vector<dealii::Point<dim>> &points) const;
+
+    /// Evaluates Jacobian given some DoF, associated FE, and some points.
+    /** Calls evaluate_jacobian_at_point() on the vector of points.
+     *  Can be used in conjunction with AD since the type is templated.
+     */
+    template <typename real2>
+    void evaluate_jacobian_at_points(
+        const std::vector<real2> &dofs,
+        const dealii::FESystem<dim> &fe,
+        const std::vector<dealii::Point<dim>> &points,
+        std::vector<real2> &jacobian_determinants) const;
+    /// Evaluates Jacobian given some DoF, associated FE, and some point.
+    /** Can be used in conjunction with AD since the type is templated.
+     */
+    template <typename real2>
+    real2 evaluate_jacobian_at_point(
+        const std::vector<real2> &dofs,
+        const dealii::FESystem<dim> &fe,
+        const dealii::Point<dim> &point) const;
+
+    /// Evaluate exact Jacobian determinant polynomial and uses Bernstein polynomials to determine positivity
+    bool check_valid_cell(const typename DoFHandlerType::cell_iterator &cell) const;
+
+    /// Evaluate exact Jacobian determinant polynomial and uses Bernstein polynomials to determine positivity
+    bool fix_invalid_cell(const typename DoFHandlerType::cell_iterator &cell);
+
+	/// Used to transform coefficients from a Lagrange basis to a Bernstein basis
+    dealii::FullMatrix<double> lagrange_to_bernstein_operator;
+	/// Evaluates the operator to obtain Bernstein coefficients from a set of Lagrange coefficients
+	/** This is used in the evaluation of the Jacobian positivity by checking the convex hull of the
+     *  resulting Bezier curve.
+     */
+	void evaluate_lagrange_to_bernstein_operator(const unsigned int order);
+
+    /// Update surface indices
+    void update_surface_indices();
+    /// Update surface indices
+    void update_surface_nodes();
+
+    void output_results_vtk (const unsigned int cycle) const; ///< Output mesh with metric informations
+
+    /// List of all surface nodes
+    std::vector<real> all_surface_nodes;
+    unsigned int n_surface_nodes; ///< Total number of surface nodes
 
     /// RBF mesh deformation  -  To be done
-    void deform_mesh(Vector surface_displacements);
+    //void deform_mesh(Vector surface_displacements);
+    void deform_mesh(std::vector<real> local_surface_displacements);
 
     /// Evaluate cell metric Jacobian
     /** The metric Jacobian is given by the gradient of the physical location
@@ -88,14 +161,12 @@ public:
     //
 
 
-    /** Prepares the solution transfer such that the curved refined grid is on top of the curved coarse grid.
-     *  This function needs to be called before Triangulation::execute_coarsening_and_refinement() or Triangulation::refine_global()
+    /// Prepares the solution transfer such that the curved refined grid is on top of the curved coarse grid.
+    /** This function needs to be called before dealii::Triangulation::execute_coarsening_and_refinement() or dealii::Triangulation::refine_global()
      */
     void prepare_for_coarsening_and_refinement();
-    /** Transfers the coarse curved curve onto the fine curved grid.
-     *  This function needs to be called after Triangulation::execute_coarsening_and_refinement() or Triangulation::refine_global()
-     */
-    void execute_coarsening_and_refinement();
+
+    void execute_coarsening_and_refinement(const bool output_mesh = false);
 
     /// Use Lagrange polynomial to represent the spatial location.
     const dealii::FE_Q<dim>     fe_q;
@@ -103,8 +174,8 @@ public:
     const dealii::FESystem<dim> fe_system;
 
 
-    /** MappingFEField that will provide the polynomial-based grid.
-     *  It is a shared smart pointer because the constructor requires the dof_handler_grid to be properly initialized.
+    /// MappingFEField that will provide the polynomial-based grid.
+    /** It is a shared smart pointer because the constructor requires the dof_handler_grid to be properly initialized.
      *  See discussion in the following thread:
      *  https://stackoverflow.com/questions/7557153/defining-an-object-without-calling-its-constructor-in-c
      */
@@ -114,6 +185,7 @@ public:
     dealii::IndexSet ghost_dofs_grid; ///< Locally relevant ghost degrees of freedom for the grid
     dealii::IndexSet locally_relevant_dofs_grid; ///< Union of locally owned degrees of freedom and relevant ghost degrees of freedom for the grid
 protected:
+    int nth_refinement;
 
     /// Used for the SolutionTransfer when performing grid adaptation.
     Vector old_nodes;
@@ -126,6 +198,29 @@ protected:
     MPI_Comm mpi_communicator; ///< MPI communicator
     dealii::ConditionalOStream pcout; ///< Parallel std::cout that only outputs on mpi_rank==0
 
+    template <typename real2>
+    real2 determinant(const std::array< dealii::Tensor<1,dim,real2>, dim > jacobian) const;
+
+    void get_position_vector(const DoFHandlerType &dh, VectorType &vector, const dealii::ComponentMask &mask);
+
+
+};
+
+template <int dim>
+class GridPostprocessor : public dealii::DataPostprocessor<dim>
+{
+public:
+    // /// Constructor
+    // GridPostprocessor();
+
+    /// Queries the Physics to output data of a vector-valued problem.
+    virtual void evaluate_vector_field (const dealii::DataPostprocessorInputs::Vector<dim> &inputs, std::vector<dealii::Vector<double>> &computed_quantities) const override;
+    /// Queries the Physics for the names of output data variables.
+    virtual std::vector<std::string> get_names () const override;
+    /// Queries the Physics for the type (scalar/vector) of output data variables.
+    virtual std::vector<dealii::DataComponentInterpretation::DataComponentInterpretation> get_data_component_interpretation () const override;
+    /// Queries the Physics for the required update flags to evaluate output data.
+    virtual dealii::UpdateFlags get_needed_update_flags () const override;
 };
 
 } // namespace PHiLiP

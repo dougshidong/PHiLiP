5a59395e231697c3fc2742e43c145dd6440b881f
Doug Shi-Dong <doug.shidong@gmail.com>
2019-12-05 18:08:35 -0500

    Commit to transfer files

--- tests/unit_tests/sensitivities/symmetric_KKT_hessian.cpp
+++ tests/unit_tests/sensitivities/symmetric_KKT_hessian.cpp
@@ -1,8 +1,3 @@
-#include <deal.II/lac/trilinos_sparsity_pattern.h>
-#include <deal.II/lac/trilinos_sparse_matrix.h>
-#include <deal.II/lac/trilinos_block_sparse_matrix.h>
-#include <deal.II/lac/la_parallel_block_vector.h>
-
 #include <Epetra_RowMatrixTransposer.h>
 
 #include <deal.II/base/conditional_ostream.h>
@@ -18,8 +13,239 @@
 #include "dg/dg.h"
 #include "functional/functional.h"
 
+#include <deal.II/lac/solver_control.h>
+#include <deal.II/lac/trilinos_precondition.h>
+#include <deal.II/lac/trilinos_solver.h>
+
+#include <deal.II/lac/full_matrix.h>
+
+#include <deal.II/lac/solver_bicgstab.h>
+#include <deal.II/lac/solver_cg.h>
+#include <deal.II/lac/solver_gmres.h>
+#include <deal.II/lac/solver_minres.h>
+
+#include <deal.II/lac/block_sparsity_pattern.h>
+
+#include <deal.II/lac/trilinos_block_sparse_matrix.h>
+#include <deal.II/lac/trilinos_precondition.h>
+#include <deal.II/lac/trilinos_solver.h>
+#include <deal.II/lac/trilinos_sparsity_pattern.h>
+#include <deal.II/lac/trilinos_sparse_matrix.h>
+
+#include <deal.II/lac/trilinos_parallel_block_vector.h>
+#include <deal.II/lac/la_parallel_block_vector.h>
+
+
+#include <deal.II/lac/packaged_operation.h>
+#include <deal.II/lac/trilinos_linear_operator.h>
+
+#include <deal.II/lac/read_write_vector.h>
+
 const double STEPSIZE = 1e-7;
 const double TOLERANCE = 1e-6;
+
+std::pair<unsigned int, double>
+solve_linear (
+    dealii::TrilinosWrappers::BlockSparseMatrix &matrix_A,
+    dealii::LinearAlgebra::distributed::BlockVector<double> &right_hand_side,
+    dealii::LinearAlgebra::distributed::BlockVector<double> &solution,
+    const PHiLiP::Parameters::LinearSolverParam &)//param)
+{
+
+    // if (pcout.is_active()) matrix_A.print(pcout.get_stream(), true);
+    // if (pcout.is_active()) solution.print(pcout.get_stream());
+
+    // PHiLiP::Parameters::LinearSolverParam::LinearSolverEnum direct_type = PHiLiP::Parameters::LinearSolverParam::LinearSolverEnum::direct;
+    // PHiLiP::Parameters::LinearSolverParam::LinearSolverEnum gmres_type = PHiLiP::Parameters::LinearSolverParam::LinearSolverEnum::gmres;
+
+    // if (param.linear_solver_output == PHiLiP::Parameters::OutputEnum::verbose) {
+    //     dealii::ConditionalOStream pcout(std::cout, dealii::Utilities::MPI::this_mpi_process(MPI_COMM_WORLD)==0);
+    //     if (pcout.is_active()) right_hand_side.print(pcout.get_stream());
+    //     dealii::FullMatrix<double> fullA(matrix_A.m());
+    //     fullA.copy_from(matrix_A);
+    //     pcout<<"Dense matrix:"<<std::endl;
+    //     if (pcout.is_active()) fullA.print_formatted(pcout.get_stream(), 3, true, 10, "0", 1., 0.);
+    // }
+    // if (param.linear_solver_type == direct_type) {
+
+//  //       dealii::SolverControl solver_control(1, 0);
+//  //       dealii::TrilinosWrappers::SolverDirect::AdditionalData data(false);
+//  //       //dealii::TrilinosWrappers::SolverDirect::AdditionalData data(parameters.output == PHiLiP::Parameters::Solver::verbose);
+//  //       dealii::TrilinosWrappers::SolverDirect direct(solver_control, data);
+//
+//  //       direct.solve(matrix_A, solution, right_hand_side);
+//  //       return {solver_control.last_step(), solver_control.last_value()};
+    // } else if (param.linear_solver_type == gmres_type) {
+	// 	const std::vector<dealii::IndexSet> domain_indices = matrix_A.locally_owned_domain_indices();
+	// 	const std::vector<dealii::IndexSet> range_indices = matrix_A.locally_owned_range_indices();
+	// 	Epetra_Map domain_partitioner = domain_indices.make_trilinos_map();
+	// 	Epetra_Map range_partitioner = range_indices.make_trilinos_map();
+
+    //     Epetra_Vector x(View,
+    //                     domain_partitioner,
+    //                     solution.begin());
+    //     Epetra_Vector b(View,
+    //                     range_partitioner,
+    //                     right_hand_side.begin());
+    //     AztecOO solver;
+    //     solver.SetAztecOption( AZ_output, (param.linear_solver_output ? AZ_all : AZ_none));
+    //     solver.SetAztecOption(AZ_solver, AZ_gmres);
+    //     solver.SetAztecOption(AZ_kspace, param.restart_number);
+    //     solver.SetRHS(&b);
+    //     solver.SetLHS(&x);
+    //     solver.SetAztecOption(AZ_precond, AZ_dom_decomp);
+    //     solver.SetAztecOption(AZ_subdomain_solve, AZ_ilut);
+    //     solver.SetAztecOption(AZ_overlap, 0);
+    //     solver.SetAztecOption(AZ_reorder, 1); // RCM re-ordering
+  
+    //     const double 
+    //       ilut_drop = param.ilut_drop,
+    //       ilut_rtol = param.ilut_rtol,//0.0,//1.1,
+    //       ilut_atol = param.ilut_atol,//0.0,//1e-9,
+    //       linear_residual = param.linear_residual;//1e-4;
+    //     const int 
+    //       ilut_fill = param.ilut_fill,//1,
+    //       max_iterations = param.max_iterations;//200
+  
+    //     solver.SetAztecParam(AZ_drop, ilut_drop);
+    //     solver.SetAztecParam(AZ_ilut_fill, ilut_fill);
+    //     solver.SetAztecParam(AZ_athresh, ilut_atol);
+    //     solver.SetAztecParam(AZ_rthresh, ilut_rtol);
+    //     solver.SetUserMatrix(const_cast<Epetra_CrsMatrix *>(&matrix_A.trilinos_matrix()));
+    //     dealii::ConditionalOStream pcout(std::cout, dealii::Utilities::MPI::this_mpi_process(MPI_COMM_WORLD)==0);
+    //     pcout << " Linear solver max its: " << max_iterations 
+    //           << " Linear residual tolerance: " << linear_residual << std::endl;
+    //     solver.Iterate(max_iterations,
+    //                    linear_residual);
+  
+    //     pcout << " Linear solver took " << solver.NumIters()
+    //           << " iterations resulting in a linear residual of " << solver.ScaledResidual() << std::endl
+    //           << " Current RHS norm: " << right_hand_side.l2_norm()
+    //           << " Newton update norm: " << solution.l2_norm() << std::endl;
+
+    //     return {solver.NumIters(), solver.TrueResidual()};
+    // }
+    {
+		using trilinos_vector_type = dealii::TrilinosWrappers::MPI::Vector;
+		using vector_type = dealii::LinearAlgebra::distributed::Vector<double>;
+
+
+		vector_type &_rhs1 = right_hand_side.block(0);
+		vector_type &_rhs2 = right_hand_side.block(1);
+
+		dealii::IndexSet locally_relevant = _rhs1.locally_owned_elements();
+		locally_relevant.add_indices(_rhs1.get_partitioner()->ghost_indices());
+
+		trilinos_vector_type rhs1;
+		trilinos_vector_type rhs2;
+		trilinos_vector_type soln1;
+		trilinos_vector_type soln2;
+
+		{
+			dealii::LinearAlgebra::ReadWriteVector<double> rw_vector;
+			rw_vector.import(_rhs1, dealii::VectorOperation::insert);
+			rhs1.import(rw_vector, dealii::VectorOperation::insert);
+		}
+		{
+			dealii::LinearAlgebra::ReadWriteVector<double> rw_vector;
+			rw_vector.import(_rhs2, dealii::VectorOperation::insert);
+			rhs2.import(rw_vector, dealii::VectorOperation::insert);
+		}
+
+		//vector_type &soln1 = solution.block(0);
+		//vector_type &soln2 = solution.block(1);
+
+		using matrix_type = dealii::TrilinosWrappers::SparseMatrix;
+		using payload_type = dealii::TrilinosWrappers::internal::LinearOperatorImplementation::TrilinosPayload;
+
+
+		const auto &L11 = matrix_A.block(0,0);
+		const auto &L12 = matrix_A.block(0,1);
+		const auto &L21 = matrix_A.block(1,0);
+		//const auto &L22 = matrix_A.block(1,1);
+
+		const auto op_L11 = dealii::linear_operator<trilinos_vector_type,trilinos_vector_type,payload_type>(L11);
+		const auto op_L12 = dealii::linear_operator<trilinos_vector_type,trilinos_vector_type,payload_type>(L12);
+		const auto op_L21 = dealii::linear_operator<trilinos_vector_type,trilinos_vector_type,payload_type>(L21);
+		//const auto L22 = dealii::linear_operator<trilinos_vector_type>(matrix_A.block(1,1));
+
+		dealii::ReductionControl reduction_control_L11(2000, 1.0e-18, 1.0e-10);
+		dealii::SolverMinRes<trilinos_vector_type> solver_L11(reduction_control_L11);
+
+		dealii::TrilinosWrappers::PreconditionSSOR preconditioner_L11;
+		preconditioner_L11.initialize(L11);
+		//const dealii::TrilinosWrappers::PreconditionBase &preconditioner_L11_base = preconditioner_L11;
+		const auto op_preconditioner_L11 = dealii::linear_operator<trilinos_vector_type,trilinos_vector_type,payload_type>(preconditioner_L11);
+
+		const auto op_L11_inv = dealii::inverse_operator(op_L11, solver_L11, preconditioner_L11);
+		const auto op_Schur = op_L21 * op_L11_inv * op_L12;
+		const auto op_approxSchur = op_L21 * op_preconditioner_L11 * op_L12;
+
+		dealii::IterationNumberControl iteration_number_control_aS(30, 1.e-18);
+		dealii::SolverMinRes<trilinos_vector_type> solver_approxSchur(iteration_number_control_aS);
+		//const auto preconditioner_Schur = dealii::inverse_operator(op_approxSchur, solver_approxSchur, dealii::TrilinosWrappers::PreconditionIdentity());
+		const auto preconditioner_Schur = dealii::inverse_operator(op_approxSchur, solver_approxSchur);
+
+		//auto schur_rhs_expr = op_L11 * rhs1;
+		//auto schur_rhs_expr = op_L11_inv * rhs1;
+		//auto schur_rhs_expr = op_L21 * op_L11_inv * rhs1 - rhs2;
+		//trilinos_vector_type schur_rhs;
+		//schur_rhs_expr.apply(schur_rhs);
+		//const trilinos_vector_type schur_rhs = op_L21 * op_L11_inv * rhs1 - rhs2;
+        //Epetra_Vector x(View,
+        //                L21.trilinos_matrix().DomainMap(),
+        //                _rhs2.begin());
+
+		//std::cout << "vmult rhsdealii" << std::endl;
+        //Epetra_Vector b(View,
+        //                L21.trilinos_matrix().RangeMap(),
+        //                _rhs1.begin());
+		//L21.trilinos_matrix().Apply(b,x);
+		std::cout << "vmult rhstrilinos" << std::endl;
+		trilinos_vector_type schur_rhs;
+		L21.vmult(schur_rhs, rhs1);
+		//L21.vmult(schur_rhs, rhs1);
+		//const trilinos_vector_type schur_rhs = op_L21 * rhs1 - rhs2;
+
+		dealii::SolverControl solver_control_Schur(2000, 1.e-12);
+		dealii::SolverMinRes<trilinos_vector_type> solver_Schur(solver_control_Schur);
+		const auto op_Schur_inv = dealii::inverse_operator(op_Schur, solver_Schur, preconditioner_Schur);
+		soln2 = op_Schur_inv * schur_rhs;
+		std::cout << solver_control_Schur.last_step()
+					<< " CG Schur complement iterations to obtain convergence."
+					<< std::endl;
+		soln1 = op_L11_inv * (rhs1 - op_L12 * soln2);
+
+		{
+			dealii::LinearAlgebra::ReadWriteVector<double> rw_vector;
+			rw_vector.reinit(soln1);
+			solution.block(0).import(rw_vector, dealii::VectorOperation::insert);
+		}
+		{
+			dealii::LinearAlgebra::ReadWriteVector<double> rw_vector;
+			rw_vector.reinit(soln2);
+			solution.block(1).import(rw_vector, dealii::VectorOperation::insert);
+		}
+
+    }
+    //{
+	//  dealii::SolverControl solver_control(std::max<std::size_t>(100000, right_hand_side.size() / 10), 1e-10 * right_hand_side.l2_norm(), true, true);
+	//  dealii::SolverGMRES<dealii::LinearAlgebra::distributed::BlockVector<double>> solver(solver_control);
+	//  //dealii::PreconditionJacobi<dealii::TrilinosWrappers::BlockSparseMatrix<double>> preconditioner;
+	//  //dealii::TrilinosWrappers::PreconditionJacobi<dealii::TrilinosWrappers::BlockSparseMatrix> preconditioner;
+	//  //preconditioner.initialize(matrix_A, 1.0);
+	//  solver.solve(matrix_A, solution, right_hand_side, dealii::PreconditionIdentity());
+	//  dealii::LinearAlgebra::distributed::BlockVector<double> residual(right_hand_side);
+	//  matrix_A.vmult(residual, solution);
+	//  residual -= right_hand_side;
+	//  std::cout << "   Iterations required for convergence: "
+	//			<< solver_control.last_step() << '\n'
+	//			<< "   Max norm of residual:                "
+	//			<< residual.linfty_norm() << '\n';
+
+    //}
+    return {-1.0, -1.0};
+}
 
 template <int dim, int nstate, typename real>
 class L2_Norm_Functional : public PHiLiP::Functional<dim, nstate, real>
@@ -352,6 +578,13 @@ int main(int argc, char *argv[])
                     << " vector_rel_diff: " << vector_rel_diff
                     << std::endl;
     if (vector_abs_diff > tol && vector_rel_diff > tol) fail_bool = true;
+
+    dealii::deallog.depth_console(10);
+    solve_linear (
+        kkt_hessian,
+        Hv, // b
+        Htv, // x
+        all_parameters.linear_solver_param);
 
 	return fail_bool;
 }

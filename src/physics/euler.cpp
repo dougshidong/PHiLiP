31744ae5573657987f86e6e1c91fd5d1cb10868e
Doug Shi-Dong <doug.shidong@gmail.com>
2020-02-08 03:09:26 -0500

    Stop the solver in case of invalid pressure.

--- src/physics/euler.cpp
+++ src/physics/euler.cpp
@@ -214,6 +214,7 @@ inline real Euler<dim,nstate,real>
         std::cout<<"density "<<density<<std::endl;
         for(int d=0;d<dim;d++) std::cout<<"vel"<<d<<" "<<vel[d]<<std::endl;
         std::cout<<"energy "<<tot_energy<<std::endl;
+        throw 1;
     }
     assert(pressure>0.0);
     //if(pressure<1e-4) pressure = 0.01;

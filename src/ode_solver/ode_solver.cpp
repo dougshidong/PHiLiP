374658e05e555fedc989c4d12953578d0a18a689
Doug Shi-Dong <doug.shidong@gmail.com>
2020-02-26 23:18:45 -0500

    Fix weird output number due to invalid cycle.

--- src/ode_solver/ode_solver.cpp
+++ src/ode_solver/ode_solver.cpp
@@ -61,7 +61,6 @@ template <int dim, typename real>
 int ODESolver<dim,real>::steady_state ()
 {
     Parameters::ODESolverParam ode_param = ODESolver<dim,real>::all_parameters->ode_solver_param;
-    if (ode_param.output_solution_every_x_steps >= 0) this->dg->output_results_vtk(this->current_iteration);
     pcout << " Performing steady state analysis... " << std::endl;
     allocate_ode_system ();
 
@@ -69,6 +68,7 @@ int ODESolver<dim,real>::steady_state ()
     this->residual_norm_decrease = 1; // Always do at least 1 iteration
     update_norm = 1; // Always do at least 1 iteration
     this->current_iteration = 0;
+    if (ode_param.output_solution_every_x_steps >= 0) this->dg->output_results_vtk(this->current_iteration);
 
     pcout << " Evaluating right-hand side and setting system_matrix to Jacobian before starting iterations... " << std::endl;
     this->dg->assemble_residual ();
@@ -245,7 +245,7 @@ double Implicit_ODESolver<dim,real>::linesearch ()
         new_residual = this->dg->get_residual_l2norm();
     }
     if (step_length > std::pow(step_reduction,maxline/2)) {
-        this->CFL *= 1.2;
+        //this->CFL *= 1.2;
     } else {
         //this->CFL *= 0.5;
     }

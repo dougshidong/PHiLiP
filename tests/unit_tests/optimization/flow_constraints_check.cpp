24e5e52edef5fa46cfbe13a1ef8068a0f912fc40
Doug Shi-Dong <doug.shidong@gmail.com>
2020-05-20 22:52:35 -0400

    Move flow constraints check to unit test.

--- tests/unit_tests/optimization/flow_constraints_check.cpp
+++ tests/unit_tests/optimization/flow_constraints_check.cpp
@@ -14,8 +14,6 @@
 #include "ROL_LineSearchStep.hpp"
 //#include "ROL_StatusTest.hpp"
 
-#include "euler_bump_optimization.h"
-
 #include "physics/euler.h"
 #include "dg/dg.h"
 #include "ode_solver/ode_solver.h"
@@ -29,7 +27,8 @@
 #include "optimization/flow_constraints.hpp"
 #include "optimization/rol_objective.hpp"
 
-
+const int dim = 2;
+const int nstate = 4;
 const int POLY_DEGREE = 1;
 const double BUMP_HEIGHT = 0.0625;
 const double CHANNEL_LENGTH = 3.0;
@@ -43,22 +42,36 @@ int test(const unsigned int nx_ffd)
     using DealiiVector = dealii::LinearAlgebra::distributed::Vector<double>;
     using ManParam = Parameters::ManufacturedConvergenceStudyParam;
     using GridEnum = ManParam::GridEnum;
-    const Parameters::AllParameters param = *(TestsBase::all_parameters);
 
-    Assert(dim == param.dimension, dealii::ExcDimensionMismatch(dim, param.dimension));
-    Assert(param.pde_type != param.PartialDifferentialEquation::euler, dealii::ExcNotImplemented());
+    const unsigned int mpi_rank = dealii::Utilities::MPI::this_mpi_process(MPI_COMM_WORLD);
 
     dealii::ParameterHandler parameter_handler;
-    PHiLiP::Parameters::EulerParam::declare_parameters (parameter_handler);
-    PHiLiP::Parameters::EulerParam euler_param;
-    euler_param.parse_parameters (parameter_handler);
-    euler_param.mach_inf = 0.3;
+    Parameters::AllParameters::declare_parameters (parameter_handler);
+    parameter_handler.set("pde_type", "euler");
+    parameter_handler.set("conv_num_flux", "roe");
+    parameter_handler.set("dimension", (long int)dim);
+    parameter_handler.enter_subsection("euler");
+    parameter_handler.set("mach_infinity", 0.3);
+    parameter_handler.leave_subsection();
+    parameter_handler.enter_subsection("ODE solver");
+    parameter_handler.set("nonlinear_max_iterations", (long int) 500);
+    parameter_handler.set("nonlinear_steady_residual_tolerance", 1e-12);
+    parameter_handler.set("initial_time_step", 0.05);
+    parameter_handler.set("time_step_factor_residual", 25.0);
+    parameter_handler.set("time_step_factor_residual_exp", 4.0);
+    parameter_handler.leave_subsection();
+
+    Parameters::AllParameters param;
+    param.parse_parameters (parameter_handler);
+
+    param.euler_param.parse_parameters (parameter_handler);
+    param.euler_param.mach_inf = 0.3;
     Physics::Euler<dim,nstate,double> euler_physics_double = Physics::Euler<dim, nstate, double>(
-                euler_param.ref_length,
-                euler_param.gamma_gas,
-                euler_param.mach_inf,
-                euler_param.angle_of_attack,
-                euler_param.side_slip_angle);
+                param.euler_param.ref_length,
+                param.euler_param.gamma_gas,
+                param.euler_param.mach_inf,
+                param.euler_param.angle_of_attack,
+                param.euler_param.side_slip_angle);
     Physics::FreeStreamInitialConditions<dim,nstate> initial_conditions(euler_physics_double);
 
     std::vector<unsigned int> n_subdivisions(dim);
@@ -146,17 +159,17 @@ int test(const unsigned int nx_ffd)
     // Output stream
     ROL::nullstream bhs; // outputs nothing
     std::filebuf filebuffer;
-    if (this->mpi_rank == 0) filebuffer.open ("flow_constraints_check.log",std::ios::out);
+    if (mpi_rank == 0) filebuffer.open ("flow_constraints_check.log",std::ios::out);
     std::ostream ostr(&filebuffer);
 
     Teuchos::RCP<std::ostream> outStream;
-    if (this->mpi_rank == 0) outStream = ROL::makePtrFromRef(ostr);
-    else if (this->mpi_rank == 1) outStream = ROL::makePtrFromRef(std::cout);
+    if (mpi_rank == 0) outStream = ROL::makePtrFromRef(ostr);
+    else if (mpi_rank == 1) outStream = ROL::makePtrFromRef(std::cout);
     else outStream = ROL::makePtrFromRef(bhs);
 
     auto con  = ROL::makePtr<FlowConstraints<dim>>(dg,ffd,ffd_design_variables_indices_dim);
 
-    auto des_var_rol_p = ROL::makePtr<ROL::Vector_SimOpt<double>>(des_var_sim_rol_p, des_var_ctl_rol_p);
+    const ROL::Ptr<ROL::Vector_SimOpt<double>> des_var_rol_p = ROL::makePtr<ROL::Vector_SimOpt<double>>(des_var_sim_rol_p, des_var_ctl_rol_p);
 
     // Temporary vectors
     const auto temp_sim = des_var_sim_rol_p->clone();
@@ -189,90 +202,72 @@ int test(const unsigned int nx_ffd)
     *outStream << "con->checkInverseAdjointJacobian_1..." << std::endl;
     con->checkInverseAdjointJacobian_1(*jv1, *v1, *temp_sim, *temp_ctl, true, *outStream);
 
-    const auto dual = des_var_sim_rol_p->clone();
-    const auto temp_sim_ctl = des_var_rol_p->clone();
-    const auto v3 = des_var_rol_p->clone();
-    const auto hv3 = des_var_rol_p->clone();
+    //const auto dual = des_var_sim_rol_p->clone();
+    //const auto temp_sim_ctl = des_var_rol_p->clone();
+    //const auto v3 = des_var_rol_p->clone();
+    //const auto hv3 = des_var_rol_p->clone();
 
-    *outStream << "con->checkApplyAdjointHessian..." << std::endl;
-    (void) con->checkApplyAdjointHessian(*des_var_rol_p, *dual, *v3, *hv3, steps, true, *outStream, order);
+    //*outStream << "con->checkApplyAdjointHessian..." << std::endl;
+    //(void) con->checkApplyAdjointHessian(*des_var_rol_p, *dual, *v3, *hv3, steps, true, *outStream, order);
 
-    con->checkAdjointConsistencyJacobian (*des_var_rol_p, v3, const Vector< Real > &x, const Vector< Real > &dualw, const Vector< Real > &dualv, const bool printToStream=true, std::ostream &outStream=std::cout)
+    {
+        *outStream << "con->checkAdjointConsistencyJacobian..." << std::endl;
+        *outStream << "Checks (w J v) versus (v Jt w)  ..." << std::endl;
+        const auto w = des_var_adj_rol_p->clone();
+        const auto v = des_var_rol_p->clone();
+        const auto x = des_var_rol_p->clone();
+        const auto temp_Jv = des_var_adj_rol_p->clone();
+        const auto temp_Jtw = des_var_rol_p->clone();
+        double tol = 1e-8;
+        con->applyJacobian(*temp_Jv,*v,*x,tol);
+        con->applyAdjointJacobian(*temp_Jtw,*w,*x,tol);
 
-    const auto direction_1 = des_var_rol_p->clone();
-    auto direction_2 = des_var_rol_p->clone();
-    direction_2->scale(0.5);
-
-    *outStream << "obj->checkHessVec..." << std::endl;
-    obj->checkHessVec( *des_var_rol_p, *direction_1, steps, true, *outStream, order);
-
-    *outStream << "obj->checkHessSym..." << std::endl;
-    obj->checkHessSym( *des_var_rol_p, *direction_1, *direction_2, true, *outStream);
-
-            //  *outStream << "Outputting Hessian..." << std::endl;
-            //  dealii::FullMatrix<double> hessian(n_design_variables, n_design_variables);
-            //  for (unsigned int i=0; i<n_design_variables; ++i) {
-            //      pcout << "Column " << i << " out of " << n_design_variables << std::endl;
-            //      auto direction_unit = des_var_ctl_rol_p->basis(i);
-            //      auto hv3 = des_var_ctl_rol_p->clone();
-            //      double tol = 1e-6;
-            //      robj->hessVec( *hv3, *direction_unit, *des_var_ctl_rol_p, tol );
-
-            //      auto result = ROL_vector_to_dealii_vector_reference(*hv3);
-            //      result.update_ghost_values();
-
-            //      for (unsigned int j=0; j<result.size(); ++j) {
-            //          hessian[j][i] = result[j];
-            //      }
-            //  }
-            //  if (mpi_rank == 0) hessian.print_formatted(*outStream, 3, true, 10, "0", 1., 0.);
-
-            *outStream << "robj->checkHessSym..." << std::endl;
-            robj->checkHessSym( *des_var_ctl_rol_p, *direction_ctl_1, *direction_ctl_2, true, *outStream);
-
-        }
-
-
-        parlist.sublist("Status Test").set("Gradient Tolerance", 1e-10);
-        parlist.sublist("Status Test").set("Iteration Limit", 5000);
-        parlist.sublist("Step").set("Type","Line Search");
-        parlist.sublist("Step").sublist("Line Search").set("Initial Step Size",1e-0);
-        parlist.sublist("Step").sublist("Line Search").set("User Defined Initial Step Size",true);
-
-        //parlist.sublist("Step").sublist("Line Search").sublist("Line-Search Method").set("Type","Iteration Scaling");
-        //parlist.sublist("Step").sublist("Line Search").sublist("Line-Search Method").set("Type","Backtracking");
-        parlist.sublist("Step").sublist("Line Search").sublist("Line-Search Method").set("Type","Cubic Interpolation");
-
-        //parlist.sublist("Step").sublist("Line Search").sublist("Curvature Condition").set("Type","Null Curvature Condition");
-        //parlist.sublist("Step").sublist("Line Search").sublist("Curvature Condition").set("Type","Strong Wolfe Conditions");
-        parlist.sublist("Step").sublist("Line Search").sublist("Curvature Condition").set("Type","Goldstein Conditions");
-
-        //parlist.sublist("Step").sublist("Line Search").sublist("Descent Method").set("Type","Newton's Method");
-
-        //parlist.sublist("Step").sublist("Line Search").sublist("Descent Method").set("Type","Newton-Krylov");
-        //parlist.sublist("General").sublist("Secant").set("Use as Preconditioner", false);
-
-
-        //parlist.sublist("Step").sublist("Line Search").sublist("Descent Method").set("Type", "Steepest Descent");
-
-        parlist.sublist("Step").sublist("Line Search").sublist("Descent Method").set("Type","Quasi-Newton Method");
-        parlist.sublist("General").sublist("Secant").set("Type","Limited-Memory BFGS");
-        parlist.sublist("General").sublist("Secant").set("Maximum Storage",(int)n_design_variables);
-        parlist.sublist("General").sublist("Secant").set("Maximum Storage",5000);
-
+        const bool printToStream = true;
+        con->checkAdjointConsistencyJacobian (*w, *v, *x, *temp_Jv, *temp_Jtw, printToStream, *outStream);
     }
 
-    *outStream << "Starting optimization..." << std::endl;
-    ROL::OptimizationSolver<double> solver( opt, parlist );
-    solver.solve( *outStream );
+    // const auto direction_1 = des_var_rol_p->clone();
+    // auto direction_2 = des_var_rol_p->clone();
+    // direction_2->scale(0.5);
 
-    ROL::Ptr< const ROL::AlgorithmState <double> > opt_state = solver.getAlgorithmState();
+    // *outStream << "obj->checkHessVec..." << std::endl;
+    // obj->checkHessVec( *des_var_rol_p, *direction_1, steps, true, *outStream, order);
 
-    ROL::EExitStatus opt_exit_state = opt_state->statusFlag;
+    // *outStream << "obj->checkHessSym..." << std::endl;
+    // obj->checkHessSym( *des_var_rol_p, *direction_1, *direction_2, true, *outStream);
+
+    // //  *outStream << "Outputting Hessian..." << std::endl;
+    // //  dealii::FullMatrix<double> hessian(n_design_variables, n_design_variables);
+    // //  for (unsigned int i=0; i<n_design_variables; ++i) {
+    // //      pcout << "Column " << i << " out of " << n_design_variables << std::endl;
+    // //      auto direction_unit = des_var_ctl_rol_p->basis(i);
+    // //      auto hv3 = des_var_ctl_rol_p->clone();
+    // //      double tol = 1e-6;
+    // //      robj->hessVec( *hv3, *direction_unit, *des_var_ctl_rol_p, tol );
+
+    // //      auto result = ROL_vector_to_dealii_vector_reference(*hv3);
+    // //      result.update_ghost_values();
+
+    // //      for (unsigned int j=0; j<result.size(); ++j) {
+    // //          hessian[j][i] = result[j];
+    // //      }
+    // //  }
+    // //  if (mpi_rank == 0) hessian.print_formatted(*outStream, 3, true, 10, "0", 1., 0.);
+
+    // *outStream << "robj->checkHessSym..." << std::endl;
+    // robj->checkHessSym( *des_var_ctl_rol_p, *direction_ctl_1, *direction_ctl_2, true, *outStream);
+
+    // *outStream << "Starting optimization..." << std::endl;
+    // ROL::OptimizationSolver<double> solver( opt, parlist );
+    // solver.solve( *outStream );
+
+    // ROL::Ptr< const ROL::AlgorithmState <double> > opt_state = solver.getAlgorithmState();
+
+    // ROL::EExitStatus opt_exit_state = opt_state->statusFlag;
 
     filebuffer.close();
 
-    return opt_exit_state;
+    return 0;
 }
 
 

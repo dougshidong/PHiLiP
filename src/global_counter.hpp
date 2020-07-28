8c4af79bde664eb038ea837b812e60d8f2ca0158
Doug Shi-Dong <doug.shidong@gmail.com>
2020-07-28 09:41:40 -0400

    Global variable counter.

--- src/global_counter.hpp
+++ src/global_counter.hpp
@@ -2,5 +2,9 @@
 #define GLOBAL_COUNTER_H_
 
 extern unsigned int n_vmult;
+extern unsigned int dRdW_form;
+extern unsigned int dRdW_mult;
+extern unsigned int dRdX_mult;
+extern unsigned int d2R_mult;
 
 #endif

bc7950a229fbbfc44955e3b819e08f96d69bc145
Doug Shi-Dong <doug.shidong@gmail.com>
2020-08-10 17:44:08 -0400

    Add missing header.

--- src/optimization/full_space_step.cpp
+++ src/optimization/full_space_step.cpp
@@ -1,3 +1,4 @@
+#include "ROL_AugmentedLagrangian.hpp"
 #include "optimization/full_space_step.hpp"
 #include "optimization/kkt_operator.hpp"
 #include "optimization/kkt_birosghattas_preconditioners.hpp"

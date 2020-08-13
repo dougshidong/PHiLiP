db31159dcc3e9f3fa60a2339884ba3b9a62be802
dougshidong <doug.shidong@gmail.com>
2020-08-13 17:21:43 -0400

    Make code more AD friendly.
    Couldn't multiple a scalar with a Tensor when using AD.
    Error messages when compiling on Ubuntu 20.04.
    Probably some compiler that changed.

--- src/physics/physics.cpp
+++ src/physics/physics.cpp
@@ -73,7 +73,9 @@ std::array<dealii::Tensor<1,dim,real>,nstate> PhysicsBase<dim,nstate,real>
 {
     std::array<dealii::Tensor<1,dim,real>,nstate> diss_flux;
     for (int i=0; i<nstate; i++) {
-        diss_flux[i] = -viscosity_coefficient*(solution_gradient[i]);
+		for (int d=0; d<dim; d++) {
+    	        diss_flux[i][d] = -viscosity_coefficient*(solution_gradient[i][d]);
+		}
     }
     return diss_flux;
 }

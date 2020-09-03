84b6843fc08f4511a057eacc1b60bfd7b883bc35
Doug Shi-Dong <doug.shidong@gmail.com>
2020-09-03 15:23:13 -0400

    Fix indent/spacing in burgers stability test.

--- src/testing/burgers_stability.cpp
+++ src/testing/burgers_stability.cpp
@@ -43,79 +43,73 @@ template <int dim, int nstate>
 int BurgersEnergyStability<dim, nstate>::run_test() const
 {
     pcout << " Running Burgers energy stability. " << std::endl;
-// dealii::Triangulation<dim> grid(
-//                 typename dealii::Triangulation<dim>::MeshSmoothing(
-//                     dealii::Triangulation<dim>::smoothing_on_refinement |
-//                     dealii::Triangulation<dim>::smoothing_on_coarsening));
- using Triangulation = dealii::Triangulation<dim>;
- std::shared_ptr<Triangulation> grid = std::make_shared<Triangulation>();
-
- double left = 0.0;
- double right = 2.0;
- const bool colorize = true;
- int n_refinements = 5;
- unsigned int poly_degree = 7;
- dealii::GridGenerator::hyper_cube(*grid, left, right, colorize);
-
- std::vector<dealii::GridTools::PeriodicFacePair<typename Triangulation::cell_iterator> > matched_pairs;
- dealii::GridTools::collect_periodic_faces(*grid,0,1,0,matched_pairs);
- grid->add_periodicity(matched_pairs);
-
-
- grid->refine_global(n_refinements);
- pcout << "Grid generated and refined" << std::endl;
-
- std::shared_ptr < PHiLiP::DGBase<dim, double> > dg = PHiLiP::DGFactory<dim,double>::create_discontinuous_galerkin(all_parameters, poly_degree, grid);
- pcout << "dg created" <<std::endl;
- dg->allocate_system ();
-
- pcout << "Implement initial conditions" << std::endl;
- dealii::FunctionParser<1> initial_condition;
- std::string variables = "x";
- std::map<std::string,double> constants;
- constants["pi"] = dealii::numbers::PI;
- std::string expression = "sin(pi*(x)) + 0.01";
- initial_condition.initialize(variables,
-                              expression,
-                              constants);
- dealii::VectorTools::interpolate(dg->dof_handler,initial_condition,dg->solution);
- // Create ODE solver using the factory and providing the DG object
- std::shared_ptr<PHiLiP::ODE::ODESolver<dim, double>> ode_solver = PHiLiP::ODE::ODESolverFactory<dim, double>::create_ODESolver(dg);
-
- double finalTime = 3.;
-
- double dt = all_parameters->ode_solver_param.initial_time_step;
- //(void) dt;
-
- //need to call ode_solver before calculating energy because mass matrix isn't allocated yet.
-
- ode_solver->advance_solution_time(0.000001);
- double initial_energy = compute_energy(dg);
-
- //currently the only way to calculate energy at each time-step is to advance solution by dt instead of finaltime
- //this causes some issues with outputs (only one file is output, which is overwritten at each time step)
- //also the ode solver output doesn't make sense (says "iteration 1 out of 1")
- //but it works. I'll keep it for now and need to modify the output functions later to account for this.
- std::ofstream myfile ("energy_plot.gpl" , std::ios::trunc);
-
- for (int i = 0; i < std::ceil(finalTime/dt); ++ i)
- {
-  ode_solver->advance_solution_time(dt);
-  double current_energy = compute_energy(dg);
-  pcout << "Energy at time " << i * dt << " is " << current_energy << std::endl;
-  myfile << i * dt << " " << current_energy << std::endl;
-  if (current_energy - initial_energy >= 0.001)
-  {
-   return 1;
-   break;
-  }
- }
- myfile.close();
-
-
- //ode_solver->advance_solution_time(finalTime);
-
- return 0; //need to change
+    using Triangulation = dealii::Triangulation<dim>;
+    std::shared_ptr<Triangulation> grid = std::make_shared<Triangulation>();
+   
+    double left = 0.0;
+    double right = 2.0;
+    const bool colorize = true;
+    int n_refinements = 5;
+    unsigned int poly_degree = 7;
+    dealii::GridGenerator::hyper_cube(*grid, left, right, colorize);
+   
+    std::vector<dealii::GridTools::PeriodicFacePair<typename Triangulation::cell_iterator> > matched_pairs;
+    dealii::GridTools::collect_periodic_faces(*grid,0,1,0,matched_pairs);
+    grid->add_periodicity(matched_pairs);
+   
+   
+    grid->refine_global(n_refinements);
+    pcout << "Grid generated and refined" << std::endl;
+   
+    std::shared_ptr < PHiLiP::DGBase<dim, double> > dg = PHiLiP::DGFactory<dim,double>::create_discontinuous_galerkin(all_parameters, poly_degree, grid);
+    pcout << "dg created" <<std::endl;
+    dg->allocate_system ();
+   
+    pcout << "Implement initial conditions" << std::endl;
+    dealii::FunctionParser<1> initial_condition;
+    std::string variables = "x";
+    std::map<std::string,double> constants;
+    constants["pi"] = dealii::numbers::PI;
+    std::string expression = "sin(pi*(x)) + 0.01";
+    initial_condition.initialize(variables,
+                                 expression,
+                                 constants);
+    dealii::VectorTools::interpolate(dg->dof_handler,initial_condition,dg->solution);
+    // Create ODE solver using the factory and providing the DG object
+    std::shared_ptr<PHiLiP::ODE::ODESolver<dim, double>> ode_solver = PHiLiP::ODE::ODESolverFactory<dim, double>::create_ODESolver(dg);
+   
+    double finalTime = 3.;
+   
+    double dt = all_parameters->ode_solver_param.initial_time_step;
+    //(void) dt;
+   
+    //need to call ode_solver before calculating energy because mass matrix isn't allocated yet.
+   
+    ode_solver->advance_solution_time(0.000001);
+    double initial_energy = compute_energy(dg);
+   
+    //currently the only way to calculate energy at each time-step is to advance solution by dt instead of finaltime
+    //this causes some issues with outputs (only one file is output, which is overwritten at each time step)
+    //also the ode solver output doesn't make sense (says "iteration 1 out of 1")
+    //but it works. I'll keep it for now and need to modify the output functions later to account for this.
+    std::ofstream myfile ("energy_plot.gpl" , std::ios::trunc);
+   
+    for (int i = 0; i < std::ceil(finalTime/dt); ++ i) {
+        ode_solver->advance_solution_time(dt);
+        double current_energy = compute_energy(dg);
+        pcout << "Energy at time " << i * dt << " is " << current_energy << std::endl;
+        myfile << i * dt << " " << current_energy << std::endl;
+        if (current_energy - initial_energy >= 0.001) {
+            return 1;
+            break;
+        }
+    }
+    myfile.close();
+   
+   
+    //ode_solver->advance_solution_time(finalTime);
+   
+    return 0; //need to change
 }
 #if PHILIP_DIM==1
     template class BurgersEnergyStability<PHILIP_DIM,PHILIP_DIM>;

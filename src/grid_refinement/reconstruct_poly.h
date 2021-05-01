fcb13802bee3706da35a88051a6cd147be9ec09c
KeiganMacLean <Keigan.MacLean@mail.mcgill.ca>
2021-05-01 15:42:55 -0400

    Document public reconstruction functions Includes descriptive latex equations (slightly complex)

--- src/grid_refinement/reconstruct_poly.h
+++ src/grid_refinement/reconstruct_poly.h
@@ -20,13 +20,18 @@ namespace PHiLiP {
 
 namespace GridRefinement {
 
-// norm types availible for reconstruction
+/// Enumeration of norms availible to be used in the patchwise reconstruction
 enum class NormType{
     H1,
     L2,
     };
 
 // forward declaration of multi-index computation from Dealii
+/// Modified implementaiton of polynomial space indexing from Deal.II
+/** Computes multi-index of polynomial term for different dimensions. 
+  * Based on the Deal.II protected function: 
+  * https://www.dealii.org/current/doxygen/deal.II/polynomial__space_8cc_source.html
+  */ 
 template <int dim>
 std::array<unsigned int, dim> compute_index(
     const unsigned int i,
@@ -38,10 +43,10 @@ class ReconstructPoly
 {
 
 public:
-    // removing the default constructor
+    /// Deleted default constructor
     ReconstructPoly() = delete;
 
-    // constructor
+    /// Constructor. Stores required information about the mesh and quadrature rules.
     ReconstructPoly(
         const dealii::hp::DoFHandler<dim>&        dof_handler,           // dof_handler
         const dealii::hp::MappingCollection<dim>& mapping_collection,    // mapping collection
@@ -49,26 +54,103 @@ public:
         const dealii::hp::QCollection<dim>&       quadrature_collection, // quadrature collection
         const dealii::UpdateFlags&                update_flags);         // update flags for for volume fe
 
-    // reinitialize the vectors
+    /// Reinitialze the internal vectors 
+    /** These vectors are used to store the obtained derivative
+      * values and directions at each mesh element.
+      */
     void reinit(const unsigned int n);
 
-    // sets the norm value
+    /// Select the Norm to be used in reconstruction
+    /** Results in a modified local reconstruction process for the patch of neighbouring elements.
+      */ 
     void set_norm_type(const NormType norm_type);
 
-    // constructs the derivatives along the chords of the cells
+    /// Construct directional derivatives along the chords of the cell
+    /** $p+1$ (or rel_order) derivatives are constructed and extracted along the specified directions
+      * from the existing cell size. Once all polynomial terms on the surrounding patch are approximated
+      * (see reconstruct_norm for description), the derivative components are obtained by evaluating this
+      * function along a given direction:
+      * 
+      * \f[
+      *     u_{\bar{\bm{x}}, p}(\bm{x}) = 
+      *     \sum_{|\bm{\alpha}| \leq p} {
+      *         \frac{\partial^{\bm{\alpha}} u(\bar{\bm{x}})}
+      *              {\bm{\alpha}!} 
+      *         (\bm{x}-\bar{\bm{x}})^{\bm{\alpha}}
+      *         }
+      * \f]
+      * \f[
+      *     D^{p+1}_{\bm{\xi}} u(\bar{\bm{x}}) h^{p+1} 
+      *     = u_{\bar{\bm{x}}, p+1}(\bm{x}+h\bm{\xi}) 
+      *     - u_{\bar{\bm{x}}, p}(\bm{x}+h\bm{\xi})
+      *     = \sum_{i=0}^{p+1}{
+      *     \frac{1}
+      *          {i! (p+1-i)!} 
+      *     \frac{\partial^{p+1} u(\bar{\bm{x}})}
+      *          {\partial x^i \partial y^{p+1-i}} 
+      *     (x-\bar{x})^i (y-\bar{y})^{p+1-i}}
+      * \f]
+      * 
+      * Ordering is based on the internal dealii numbering. Used in cases where the orientation of the element
+      * is not controlled by the refinement procedure (e.g. fixed fraction cases). Gives direct prediction of how
+      * error will change with modifying the length of these axes.
+      */ 
     void reconstruct_chord_derivative(
-        const dealii::LinearAlgebra::distributed::Vector<real>&solution,   // solution approximation to be reconstructed
-        const unsigned int                                     rel_order); // order of the apporximation
+        const dealii::LinearAlgebra::distributed::Vector<real>&solution,   ///< Solution approximation to be reconstructed
+        const unsigned int                                     rel_order); ///< Relative order of the approximation
 
-    // construct the largest set of direcitonal derivative (in descending order perpendicular planes)
+    /// Construct the set of largest perpendicular directional derivatives
+    /** $p+1$ (or rel_order) derivatives are constructed  and the largest values are extracted. In order to approximate 
+      * the "worst case" unit-ball of the error in the high-order case, the method of Dolejsi is used where values are extracted
+      * from the maximum direction in the next perpendicular hyperplane to existing directions. In this way, a set of orthogonal directions 
+      * is chosen with descending largest derivative orders. In 2D, this process can be written as solving the collection of functions
+      * to find maximums based on the patchwise reconstruction of the polynomial (obtained from reconstruction):
+      * 
+      * \f[
+      *     A_1(\bar{\bm{x}}, p) &= \max_{\left\lVert{\bm{\xi}}\right\rVert_2=1}{|D^p_{\bm{\xi}} u(\bar{\bm{x}})|}
+      * \f]
+      * \f[
+      *     \bm{\xi}_1(\bar{\bm{x}}, p) &= \operatornamewithlimits{argmax}_{\left\lVert{\bm{\xi}}\right\rVert_2=1}{|D^p_{\bm{\xi}} u(\bar{\bm{x}})|}
+      * \f]
+      * \f[
+      *     \varphi(\bar{\bm{x}}, p) & \in \left[ 0, 2\pi\right) \quad s.t. \text{ } \bm{\xi}_1 = (cos(\varphi), sin(\varphi))
+      * \f]
+      * \f[
+      *     A_2(\bar{\bm{x}}, p) &= |D^p_{\bm{\xi}_2} u(\bar{\bm{x}})|, \quad \text{where } \bm{\xi}_1 \cdot \bm{\xi}_2 = 0.
+      * \f]
+      * 
+      * In the linear case where $p=2$, these directions and values can be directly extracted from the local hessian reconstruction.
+      * Otherewise, for the high-order case, this is performed by sampling an approximately equidistributed set of points to give a "good enough"
+      * approximation. In 2D, 180 points are distributed radially on $[0,\pi]$ to give a $1^\circ$ accuracy (second half of angles will be equal 
+      * or negative depending on even/odd polynomial order). In 3D, the initial sampling is done using a fibbonaci spiral mapped to the unit sphere 
+      * (a fibbonaci sphere, see https://stackoverflow.com/questions/9600801/evenly-distributing-n-points-on-a-sphere/26127012#26127012).
+      * This provides a good enough apporximation to an even distrubution for this case. $180 \times 180 / 2$ samples are used to give a 
+      * similar angular resolution. The second components are extracted from a unit-circle on the perpendicular plane to the largest direction.
+      */
     void reconstruct_directional_derivative(
-        const dealii::LinearAlgebra::distributed::Vector<real>&solution,   // approximation to be reconstructed
-        const unsigned int                                     rel_order); // order of the apporximation
+        const dealii::LinearAlgebra::distributed::Vector<real>&solution,   ///< Solution approximation to be reconstructed
+        const unsigned int                                     rel_order); ///< Relative order of the approximation
 
-    // construct the directional derivatives based on the manufactured solution hessian
+    /// Constructs directional derivates based on the manufactured solution hessian
+    /** For $p=2$ only, gets the exact directional derivative components using the spectral decomposition of the hessian:
+      * 
+      * \f[
+      *     H = \left[\begin{matrix} u_{xx} & u_{xy} \\ u_{yx} & u_{yy} \end{matrix}\right] 
+      *     = \left[\begin{matrix} \bm{v}_0 & \bm{v}_1 \end{matrix}\right]
+      *     \left[\begin{matrix} \lambda_0 & \\ & \lambda_1 \end{matrix}\right]
+      *     \left[\begin{matrix} \bm{v}_0^T \\ \bm{v}_1^T \end{matrix}\right]
+      * \f]
+      * \f[
+      *     D^{p=2}_{\bm{\xi}} u(\bar{\bm{x}}) h^{p+1} 
+      *     = \bm{\xi}^T H \bm{\xi}
+      *     = \sum_{i=0}^{dim} {\lambda_i \left(\bm{\xi}^T \bm{v}_i\right)^2}
+      * \f]
+      * 
+      * Where then $\lambda_i$ (eigenvales) are the directional derivatives and $v_i$ (eigenvectors) are the direction vectors.
+      */
     void reconstruct_manufactured_derivative(
-        const std::shared_ptr<ManufacturedSolutionFunction<dim,real>>& manufactured_solution, // manufactured solution function
-        const unsigned int                                             rel_order); // order of the approximation
+        const std::shared_ptr<ManufacturedSolutionFunction<dim,real>>& manufactured_solution, ///< Manufactured solution function
+        const unsigned int                                             rel_order);            ///< Relative order of the approximation
 
 private:
     template <typename DoFCellAccessorType>

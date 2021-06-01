aefa3d7e5d2ddb62cb2a66b53b5f7054ff27612a
Julien Brillon <julien.brillon@mail.mcgill.ca>
2021-06-01 15:39:26 -0400

    Added documentation from the cpp file to the header file for doxygen

--- src/numerical_flux/convective_numerical_flux.hpp
+++ src/numerical_flux/convective_numerical_flux.hpp
@@ -52,7 +52,7 @@ const std::shared_ptr < Physics::PhysicsBase<dim, nstate, real> > pde_physics;
 
 };
 
-/// Base class of Roe flux with entropy fix. Derived from NumericalFluxConvective.
+/// Base class of Roe (Roe-Pike) flux with entropy fix. Derived from NumericalFluxConvective.
 template<int dim, int nstate, typename real>
 class RoeBase: public NumericalFluxConvective<dim, nstate, real>
 {
@@ -67,9 +67,10 @@ public:
 	euler_physics(std::dynamic_pointer_cast<Physics::Euler<dim,nstate,real>>(physics_input))
 	{};
 
-    /// Virtual destructor required for abstract classes.
+    /// Destructor
 	~RoeBase() {};
 
+	/// Virtual member function for evaluating the entropy fix for a Roe-Pike flux.
 	virtual void evaluate_entropy_fix (
 	    const std::array<real, 3> &eig_L,
 	    const std::array<real, 3> &eig_R,
@@ -77,6 +78,7 @@ public:
 	    const real vel2_ravg,
 	    const real sound_ravg) const = 0;
 
+	/// Virtual member function for evaluating additional modifications/corrections for a Roe-Pike flux.
 	virtual void evaluate_additional_modifications (
 	    const std::array<real, nstate> &soln_int,
 	    const std::array<real, nstate> &soln_ext,
@@ -86,6 +88,10 @@ public:
 	    dealii::Tensor<1,dim,real> &dV_tangent) const = 0;
 
 	/// Returns the convective flux at an interface
+	/// --- See Blazek 2015, p.103-105
+	/// --- Note: Modified calculation of alpha_{3,4} to use 
+    ///           dVt (jump in tangential velocities);
+    ///           expressions are equivalent.
 	std::array<real, nstate> evaluate_flux (
 	    const std::array<real, nstate> &soln_int,
 	    const std::array<real, nstate> &soln_ext,
@@ -101,6 +107,8 @@ public:
 	RoePike(std::shared_ptr <Physics::PhysicsBase<dim, nstate, real>> physics_input)
 		:	RoeBase<dim, nstate, real>(physics_input){}
 
+	/// Evaluates the entropy fix of Harten
+    /// --- See Blazek 2015, p.103-105
 	void evaluate_entropy_fix(
 	    const std::array<real, 3> &eig_L,
 	    const std::array<real, 3> &eig_R,
@@ -108,6 +116,7 @@ public:
 	    const real vel2_ravg,
 	    const real sound_ravg) const;
 
+	/// Empty function. No additional modifications for the Roe-Pike scheme.
 	void evaluate_additional_modifications(
 	    const std::array<real, nstate> &soln_int,
 	    const std::array<real, nstate> &soln_ext,
@@ -118,6 +127,7 @@ public:
 };
 
 /// L2Roe flux with entropy fix. Derived from RoeBase.
+/// --- Reference: Osswald et al. (2016 L2Roe)
 template<int dim, int nstate, typename real>
 class L2Roe: public RoeBase<dim, nstate, real>
 {
@@ -126,6 +136,9 @@ public:
 	L2Roe(std::shared_ptr <Physics::PhysicsBase<dim, nstate, real>> physics_input)
 		:	RoeBase<dim, nstate, real>(physics_input){}
 
+	/// (1) Van Leer et al. (1989 Sonic) entropy fix for acoustic waves (i.e. i=1,5)
+	/// (2) For waves (i=2,3,4) --> Entropy fix of Liou (2000 Mass)
+    /// --- See p.74 of Osswald et al. (2016 L2Roe)
 	void evaluate_entropy_fix(
 	    const std::array<real, 3> &eig_L,
 	    const std::array<real, 3> &eig_R,
@@ -133,6 +146,8 @@ public:
 	    const real vel2_ravg,
 	    const real sound_ravg) const;
 	
+	/// Osswald's two modifications to Roe-Pike scheme --> L2Roe
+	/// --- Scale jump in (1) normal and (2) tangential velocities using a blending factor
 	void evaluate_additional_modifications(
 	    const std::array<real, nstate> &soln_int,
 	    const std::array<real, nstate> &soln_ext,
@@ -142,6 +157,8 @@ public:
 	    dealii::Tensor<1,dim,real> &dV_tangent) const;
 
 protected:
+	/// Shock indicator of Wada & Liou (1994 Flux) -- Eq.(39)
+    /// --- See also p.74 of Osswald et al. (2016 L2Roe)
 	void evaluate_shock_indicator(
 		const std::array<real, 3> &eig_L,
 	    const std::array<real, 3> &eig_R,

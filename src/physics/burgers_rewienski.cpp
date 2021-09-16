2724acbfda2a87fd97acca1e9d8f48fcadcec59e
Donovan Blais <donovan.blais@outlook.com>
2021-09-16 16:20:32 -0400

    Restructured burgers_rewienski to be derived from Burgers instead of PhysicsBase.

--- src/physics/burgers_rewienski.cpp
+++ src/physics/burgers_rewienski.cpp
@@ -25,10 +25,10 @@ namespace PHiLiP {
 
             for (int istate=0; istate<nstate; ++istate) {
 
-                std::array<real,nstate> characteristic_dot_n = convective_eigenvalues(boundary_values, normal_int);
+                std::array<real,nstate> characteristic_dot_n = this->convective_eigenvalues(boundary_values, normal_int);
                 const bool inflow = (characteristic_dot_n[istate] <= 0.);
 
-                if (inflow || hasDiffusion) { // Dirichlet boundary condition
+                if (inflow || this->hasDiffusion) { // Dirichlet boundary condition
                     // soln_bc[istate] = boundary_values[istate];
                     // soln_grad_bc[istate] = soln_grad_int[istate];
 
@@ -55,108 +55,14 @@ namespace PHiLiP {
         }
 
         template <int dim, int nstate, typename real>
-        std::array<dealii::Tensor<1,dim,real>,nstate> BurgersRewienski<dim,nstate,real>
-        ::convective_flux (const std::array<real,nstate> &solution) const
-        {
-            std::array<dealii::Tensor<1,dim,real>,nstate> conv_flux;
-            for (int flux_dim=0; flux_dim<dim; ++flux_dim) {
-                for (int s=0; s<nstate; ++s) {
-                    conv_flux[s][flux_dim] = 0.5*solution[flux_dim]*solution[s];
-                }
-            }
-            return conv_flux;
-        }
-
-        template <int dim, int nstate, typename real>
-        std::array<dealii::Tensor<1,dim,real>,nstate> BurgersRewienski<dim,nstate,real>::convective_numerical_split_flux (
-                const std::array<real,nstate> &soln_const,
-                const std::array<real,nstate> & soln_loop) const
-        {
-            std::array<dealii::Tensor<1,dim,real>,nstate> conv_flux;
-            for (int flux_dim=0; flux_dim<dim; ++flux_dim) {
-                for (int s=0; s<nstate; ++s) {
-                    conv_flux[s][flux_dim] = 1./6. * (soln_const[flux_dim]*soln_const[flux_dim] + soln_const[flux_dim]*soln_loop[s] + soln_loop[s]*soln_loop[s]);
-                }
-            }
-            return conv_flux;
-        }
-
-        template <int dim, int nstate, typename real>
-        real BurgersRewienski<dim,nstate,real>
-        ::diffusion_coefficient () const
-        {
-            if(hasDiffusion) return this->diffusion_scaling_coeff;
-            const real zero = 0.0;
-            return zero;
-        }
-
-        template <int dim, int nstate, typename real>
-        std::array<real,nstate> BurgersRewienski<dim,nstate,real>
-        ::convective_eigenvalues (
-                const std::array<real,nstate> &solution,
-                const dealii::Tensor<1,dim,real> &normal) const
-        {
-            std::array<real,nstate> eig;
-            for (int i=0; i<nstate; i++) {
-                eig[i] = 0.0;
-                for (int d=0;d<dim;++d) {
-                    eig[i] += solution[d]*normal[d];
-                }
-            }
-            return eig;
-        }
-
-        template <int dim, int nstate, typename real>
-        real BurgersRewienski<dim,nstate,real>
-        ::max_convective_eigenvalue (const std::array<real,nstate> &soln) const
-        {
-            real max_eig = 0;
-            for (int i=0; i<dim; i++) {
-                //max_eig = std::max(max_eig,std::abs(soln[i]));
-                const real abs_soln = abs(soln[i]);
-                max_eig = std::max(max_eig, abs_soln);
-                //max_eig += soln[i] * soln[i];
-            }
-            return max_eig;
-        }
-
-        template <int dim, int nstate, typename real>
-        std::array<dealii::Tensor<1,dim,real>,nstate> BurgersRewienski<dim,nstate,real>
-        ::dissipative_flux (
-                const std::array<real,nstate> &/*solution*/,
-                const std::array<dealii::Tensor<1,dim,real>,nstate> &solution_gradient) const
-        {
-            std::array<dealii::Tensor<1,dim,real>,nstate> diss_flux;
-            const real diff_coeff = diffusion_coefficient();
-            for (int i=0; i<nstate; i++) {
-                for (int d1=0; d1<dim; d1++) {
-                    diss_flux[i][d1] = 0.0;
-                    for (int d2=0; d2<dim; d2++) {
-                        diss_flux[i][d1] += -diff_coeff*((this->diffusion_tensor[d1][d2])*solution_gradient[i][d2]);
-                    }
-                }
-            }
-            return diss_flux;
-        }
-
-        template <int dim, int nstate, typename real>
         std::array<real,nstate> BurgersRewienski<dim,nstate,real>
         ::source_term (
                 const dealii::Point<dim,real> &pos,
                 const std::array<real,nstate> &/*solution*/) const
         {
             std::array<real,nstate> source;
-            const real diff_coeff = diffusion_coefficient();
-            // for (int istate=0; istate<nstate; istate++) {
-            //     dealii::Tensor<1,dim,real> manufactured_gradient = this->manufactured_solution_function.gradient (pos, istate);
-            //     dealii::SymmetricTensor<2,dim,real> manufactured_hessian = this->manufactured_solution_function.hessian (pos, istate);
-            //     source[istate] = 0.0;
-            //     for (int d=0;d<dim;++d) {
-            //         real manufactured_solution = this->manufactured_solution_function.value (pos, d);
-            //         source[istate] += manufactured_solution*manufactured_gradient[d];
-            //     }
-            //     source[istate] += -diff_coeff*scalar_product((this->diffusion_tensor),manufactured_hessian);
-            // }
+            const real diff_coeff = this->diffusion_coefficient();
+
             for (int istate=0; istate<nstate; istate++) {
                 source[istate] = 0.0;
                 dealii::Tensor<1,dim,real> manufactured_gradient = this->manufactured_solution_function->gradient (pos, istate);
@@ -184,23 +90,6 @@ namespace PHiLiP {
                 source[istate] += 0.5*manufactured_solution*divergence;
             }
 
-            // for (int istate=0; istate<nstate; istate++) {
-            //     source[istate] = 0.0;
-            //     for (int d=0;d<dim;++d) {
-            //         dealii::Point<dim,real> posp = pos;
-            //         dealii::Point<dim,real> posm = pos;
-            //         posp[d] += 1e-8;
-            //         posm[d] -= 1e-8;
-            //         std::array<real,nstate> solp,solm;
-            //         for (int s=0; s<nstate; s++) {
-            //             solp[s] = this->manufactured_solution_function.value (posp, s);
-            //             solm[s] = this->manufactured_solution_function.value (posm, s);
-            //         }
-            //         std::array<dealii::Tensor<1,dim,real>,nstate> convp = convective_flux (solp);
-            //         std::array<dealii::Tensor<1,dim,real>,nstate> convm = convective_flux (solm);
-            //         source[istate] += (convp[istate][d] - convm[istate][d]) / 2e-8;
-            //     }
-            // }
             return source;
         }
 
--- src/physics/burgers_rewienski.h
+++ src/physics/burgers_rewienski.h
@@ -1,84 +1,31 @@
 #ifndef __BURGERS_REWIENSKI__
 #define __BURGERS_REWIENSKI__
 
-#include <deal.II/base/tensor.h>
-
-#include "parameters/all_parameters.h"
-#include "parameters/parameters_manufactured_solution.h"
-
-#include "physics.h"
+#include "burgers.h"
 
 namespace PHiLiP {
     namespace Physics {
-/// Burger's equation with nonlinear advective term and linear diffusive term.  Derived from PhysicsBase.
-/** State variable: \f$ u \f$
- *
- *  Convective flux \f$ \mathbf{F}_{conv} =  0.5u^2 \f$
- *
- *  Dissipative flux \f$ \mathbf{F}_{diss} = -\boldsymbol\nabla u \f$
- *
- *  Source term \f$ s(\mathbf{x}) \f$
- *
- *  Equation:
- *  \f[ \boldsymbol{\nabla} \cdot
- *         (  \mathbf{F}_{conv}( u )
- *          + \mathbf{F}_{diss}( u, \boldsymbol{\nabla}(u) )
- *      = s(\mathbf{x})
- *  \f]
- */
+/// Burgers Rewienski equation. Derived from Burgers, which is derived from PhysicsBase.
         template <int dim, int nstate, typename real>
-        class BurgersRewienski : public PhysicsBase <dim, nstate, real>
+        class BurgersRewienski : public Burgers <dim, nstate, real>
         {
-        protected:
-            /// Diffusion scaling coefficient in front of the diffusion tensor.
-            double diffusion_scaling_coeff = 0.1*atan(1)*4.0/exp(1);
         public:
-            /// Turns on convective part of the Burgers problem.
-            /** Without the nonlinear convection, it's simply diffusion */
-            const bool hasConvection;
-            /// Turns on diffusive part of the Burgers problem.
-            const bool hasDiffusion;
-
             /// Constructor
             BurgersRewienski(
-                    const bool                                                convection = true,
-                    const bool                                                diffusion = true,
-                    const dealii::Tensor<2,3,double>                          input_diffusion_tensor = Parameters::ManufacturedSolutionParam::get_default_diffusion_tensor(),
-                    std::shared_ptr< ManufacturedSolutionFunction<dim,real> > manufactured_solution_function = nullptr) :
-                    PhysicsBase<dim,nstate,real>(input_diffusion_tensor, manufactured_solution_function),
-                    hasConvection(convection),
-                    hasDiffusion(diffusion)
+                    const bool                                                convection,
+                    const bool                                                diffusion,
+                    const dealii::Tensor<2,3,double>                          input_diffusion_tensor,
+                    std::shared_ptr< ManufacturedSolutionFunction<dim,real> > manufactured_solution_function)
+                    : Burgers<dim, nstate, real>(convection,
+                                                 diffusion,
+                                                 input_diffusion_tensor,
+                                                 manufactured_solution_function)
             {
                 static_assert(nstate==dim, "Physics::Burgers() should be created with nstate==dim");
             };
 
             /// Destructor
             ~BurgersRewienski () {};
-            /// Convective flux: \f$ \mathbf{F}_{conv} =  u \f$
-            std::array<dealii::Tensor<1,dim,real>,nstate> convective_flux (const std::array<real,nstate> &solution) const;
-
-            /// Convective split flux
-            std::array<dealii::Tensor<1,dim,real>,nstate> convective_numerical_split_flux (
-                    const std::array<real,nstate> &soln_const,
-                    const std::array<real,nstate> & soln_loop) const;
-
-            /// Spectral radius of convective term Jacobian is 'c'
-            std::array<real,nstate> convective_eigenvalues (
-                    const std::array<real,nstate> &/*solution*/,
-                    const dealii::Tensor<1,dim,real> &/*normal*/) const;
-
-            /// Maximum convective eigenvalue used in Lax-Friedrichs
-            real max_convective_eigenvalue (const std::array<real,nstate> &soln) const;
-
-            //  /// Diffusion matrix is identity
-            //  std::array<dealii::Tensor<1,dim,real>,nstate> apply_diffusion_matrix (
-            //      const std::array<real,nstate> &solution,
-            //      const std::array<dealii::Tensor<1,dim,real>,nstate> &solution_grad) const;
-
-            /// Dissipative flux: u
-            std::array<dealii::Tensor<1,dim,real>,nstate> dissipative_flux (
-                    const std::array<real,nstate> &solution,
-                    const std::array<dealii::Tensor<1,dim,real>,nstate> &solution_gradient) const;
 
             /// Source term is zero or depends on manufactured solution
             std::array<real,nstate> source_term (
@@ -98,12 +45,7 @@ namespace PHiLiP {
                     std::array<real,nstate> &/*soln_bc*/,
                     std::array<dealii::Tensor<1,dim,real>,nstate> &/*soln_grad_bc*/) const;
 
-        protected:
-            /// Diffusion coefficient
-            real diffusion_coefficient () const;
         };
-
-
     } // Physics namespace
 } // PHiLiP namespace
 

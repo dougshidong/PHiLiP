8d772db0e4efc2dba3f690d7fb5d0c6d9c13856a
Julien Brillon <julien.brillon@mail.mcgill.ca>
2021-09-18 14:33:13 -0400

    TGV initial conditions for test case

---
+++ src/testing/taylor_green_vortex.cpp
@@ -0,0 +1,200 @@
+namespace PHiLiP {
+namespace Tests {
+
+// done
+template <int dim, typename real>
+InitialConditionFunction_FlowSolver<dim,real>
+::InitialConditionFunction_FlowSolver (const unsigned int nstate)
+    :
+    dealii::Function<dim,real>(nstate,0.0) // 0.0 denotes initial time (t=0)
+    , nstate(nstate)
+{ 
+    // Nothing to do here yet
+}
+// done
+template <int dim, typename real>
+TaylorGreenVortex_InitialCondition<dim,real>
+::TaylorGreenVortex_InitialCondition (
+    const unsigned int nstate,
+    const double       gamma_gas,
+    const double       mach_inf_sqr)
+    : InitialConditionFunction_FlowSolver<dim,real>(nstate)
+    , gamma_gas(gamma_gas)
+    , mach_inf_sqr(mach_inf_sqr)    
+{
+    // casting `nstate` as `int` to avoid errors
+    static_assert(((int)nstate)==dim+2, "Tests::TaylorGreenVortex_InitialCondition() should be created with nstate=dim+2");
+}
+// done
+template <int dim, typename real>
+real TaylorGreenVortex_InitialCondition<dim,real>
+::primitive_value(const dealii::Point<dim,real> &point, const unsigned int istate) const
+{
+    // Note: This is in non-dimensional form (free-stream values as reference)
+    real value = 0.;
+    if constexpr(dim == 3) {
+        const real x = point[0], y = point[1], z = point[2];
+        
+        if(istate==0) {
+            // density
+            value = 1.0;
+        }
+        if(istate==1) {
+            // x-velocity
+            value = sin(x)*cos(y)*cos(z); 
+        }
+        if(istate==2) {  
+            // y-velocity
+            value = -cos(x)*sin(y)*cos(z);
+        }
+        if(istate==3) {
+            // z-velocity
+            value = 0.0;
+        }
+        if(istate==4) {
+            // pressure
+            value = 1.0/(gamma_gas*mach_inf_sqr) + (1.0/16.0)*(cos(2.0*x)+cos(2.0*y))*(cos(2.0*z)+2.0);
+        }
+    }
+    return value;
+}
+// done
+template <int dim, typename real>
+real TaylorGreenVortex_InitialCondition<dim,real>
+::convert_primitive_to_conversative_value(
+    const dealii::Point<dim,real> &point, const unsigned int istate) const
+{
+    real value = 0.0;
+    if (dim == 3) {
+        const real rho = primitive_value(point,0);
+        const real u   = primitive_value(point,1);
+        const real v   = primitive_value(point,2);
+        const real w   = primitive_value(point,3);
+        const real p   = primitive_value(point,4);
+
+        // convert primitive to conservative solution
+        if(istate==0) value = rho; // density
+        if(istate==1) value = rho*u; // x-momentum
+        if(istate==2) value = rho*v; // y-momentum
+        if(istate==3) value = rho*w; // z-momentum
+        if(istate==4) value = p/(1.4-1.0) + 0.5*rho*(u*u + v*v + w*w); // total energy
+    }
+
+    return value;
+}
+// done
+template <int dim, typename real>
+inline real TaylorGreenVortex_InitialCondition<dim,real>
+::value(const dealii::Point<dim,real> &point, const unsigned int istate) const
+{
+    real value = 0.0;
+    value = convert_primitive_to_conversative_value(point,istate);
+    return value;
+}
+// done
+template <int dim, typename real>
+dealii::Tensor<1,dim,real> TaylorGreenVortex_InitialCondition<dim,real>
+::primitive_gradient (const dealii::Point<dim,real> &point, const unsigned int istate) const
+{
+    dealii::Tensor<1,dim,real> gradient;
+    // Gradients of primitive variables 
+    if (dim == 2) {
+        const real x = point[0], y = point[1], z = point[2];
+
+        if(istate==0) {
+            // density
+            gradient[0] = 0.0; // dx
+            gradient[1] = 0.0; // dy
+            gradient[2] = 0.0; // dz
+        }
+        if(istate==1) {
+            // x-velocity
+            gradient[0] =  cos(x)*cos(y)*cos(z); // dx
+            gradient[1] = -sin(x)*sin(y)*cos(z); // dy
+            gradient[2] = -sin(x)*cos(y)*sin(z); // dz
+        }
+        if(istate==2) {
+            // y-velocity
+            gradient[0] = -sin(x)*sin(y)*cos(z); // dx
+            gradient[1] = -cos(x)*cos(y)*cos(z); // dy
+            gradient[2] =  cos(x)*sin(y)*sin(z); // dz
+        }
+        if(istate==3) {
+            // z-velocity
+            gradient[0] = 0.0; // dx
+            gradient[1] = 0.0; // dy
+            gradient[2] = 0.0; // dz
+        }
+        if(istate==4) {
+            // pressure
+            gradient[0] = -(1.0/8.0)*sin(2.0*x)*(cos(2.0*z)+2.0); // dx
+            gradient[1] = -(1.0/8.0)*sin(2.0*y)*(cos(2.0*z)+2.0); // dy
+            gradient[2] = -(1.0/8.0)*(cos(2.0*x)+cos(2.0*y))*sin(2.0*z); // dz
+        }
+    }
+    return gradient;
+}
+// done
+template <int dim, typename real>
+dealii::Tensor<1,dim,real> TaylorGreenVortex_InitialCondition<dim,real>
+::convert_primitive_to_conversative_gradient (const dealii::Point<dim,real> &point, const unsigned int istate) const
+{
+    dealii::Tensor<1,dim,real> gradient;
+    if (dim == 3) {
+        const real rho = primitive_value(point,0);
+        const real u   = primitive_value(point,1);
+        const real v   = primitive_value(point,2);
+        const real w   = primitive_value(point,3);
+        const real p   = primitive_value(point,4);
+        const dealii::Tensor<1,dim,real> rho_grad = primitive_gradient(point,0);
+        const dealii::Tensor<1,dim,real> u_grad   = primitive_gradient(point,1);
+        const dealii::Tensor<1,dim,real> v_grad   = primitive_gradient(point,2);
+        const dealii::Tensor<1,dim,real> w_grad   = primitive_gradient(point,3);
+        const dealii::Tensor<1,dim,real> p_grad   = primitive_gradient(point,4);
+        
+        // convert to primitive to gradient of conservative variables using product rule
+        if(istate==0) {
+            // density
+            for(int d=0; d<dim; d++) { 
+                gradient[d] = rho_grad[d];
+            }
+        }
+        if(istate==1) {
+            // x-momentum
+            for(int d=0; d<dim; d++) {
+                gradient[d] = u*rho_grad[d] + rho*u_grad[d];
+            }
+        }
+        if(istate==2) {
+            // y-momentum
+            for(int d=0; d<dim; d++) {
+                gradient[d] = v*rho_grad[d] + rho*v_grad[d];
+            }
+        }
+        if(istate==3) {
+            // z-momentum
+            for(int d=0; d<dim; d++) {
+                gradient[d] = w*rho_grad[d] + rho*w_grad[d];
+            }
+        }
+        if(istate==4) {
+            // total energy
+            for(int d=0; d<dim; d++) {
+                gradient[d] = p_grad[d]/(1.4-1.0) + 0.5*rho_grad[d]*(u*u + v*v + w*w) + rho*(u*u_grad[d]+v*v_grad[d]+w*w_grad[d]);
+            }
+        }
+    }
+    return gradient;
+}
+// done
+template <int dim, typename real>
+inline dealii::Tensor<1,dim,real> TaylorGreenVortex_InitialCondition<dim,real>
+::gradient (const dealii::Point<dim,real> &point, const unsigned int istate) const
+{
+    dealii::Tensor<1,dim,real> gradient = convert_primitive_to_conversative_gradient(point, istate);
+    return gradient;
+}
+
+} // Tests namespace
+} // PHiLiP namespace
+
---
+++ src/testing/taylor_green_vortex.h
@@ -0,0 +1,107 @@
+#ifndef __INITIALCONDITIONFUNCTION_H__
+#define __INITIALCONDITIONFUNCTION_H__
+
+#include <deal.II/lac/vector.h>
+
+#include <deal.II/base/function.h>
+
+//#include <Sacado.hpp>
+//
+//#include "physics/physics.h"
+//#include "numerical_flux/numerical_flux.h"
+// #include "parameters/all_parameters.h"
+
+namespace PHiLiP {
+namespace Tests {
+
+
+/// Manufactured solution used for grid studies to check convergence orders.
+/** This class also provides derivatives necessary  to evaluate source terms.
+ */
+template <int dim, typename real>
+class InitialConditionFunction_FlowSolver : public dealii::Function<dim,real>
+{
+// We want the Point to be templated on the type,
+// however, dealii does not template that part of the Function.
+// Therefore, we end up overloading the functions and need to "import"
+// those non-overloaded functions to avoid the warning -Woverloaded-virtual
+// See: https://stackoverflow.com/questions/18515183/c-overloaded-virtual-function-warning-by-clang
+protected:
+    using dealii::Function<dim,real>::value;
+    using dealii::Function<dim,real>::gradient;
+    using dealii::Function<dim,real>::hessian;
+    // using dealii::Function<dim,real>::vector_gradient; // TO DO: is this needed?
+public:
+    const unsigned int nstate; ///< Corresponds to n_components in the dealii::Function
+    /// Constructor
+    InitialConditionFunction_FlowSolver(const unsigned int nstate);
+    /// Destructor
+    ~InitialConditionFunction_FlowSolver() {};
+
+    /// Value of the initial condition
+    virtual real value (const dealii::Point<dim,real> &point, const unsigned int istate = 0) const = 0;
+
+    /// Gradient of the initial condition
+    virtual dealii::Tensor<1,dim,real> gradient (const dealii::Point<dim,real> &point, const unsigned int istate = 0) const = 0;
+
+    /// Hessian of the initial condition
+    virtual dealii::SymmetricTensor<2,dim,real> hessian (const dealii::Point<dim,real> &point, const unsigned int istate = 0) const = 0;
+
+    /// See dealii::Function<dim,real>::vector_gradient [TO DO: Is this necessary?]
+    // void vector_gradient (const dealii::Point<dim,real> &p,
+    //                       std::vector<dealii::Tensor<1,dim, real> > &gradients) const;
+protected:
+
+};
+
+template <int dim, typename real>
+class TaylorGreenVortex_InitialCondition
+    : public InitialConditionFunction_FlowSolver<dim,real>
+{
+// We want the Point to be templated on the type,
+// however, dealii does not template that part of the Function.
+// Therefore, we end up overloading the functions and need to "import"
+// those non-overloaded functions to avoid the warning -Woverloaded-virtual
+// See: https://stackoverflow.com/questions/18515183/c-overloaded-virtual-function-warning-by-clang
+protected:
+    using dealii::Function<dim,real>::value;
+    using dealii::Function<dim,real>::gradient;
+    using dealii::Function<dim,real>::hessian;
+    using dealii::Function<dim,real>::vector_gradient;
+public:
+    /// Constructor for TaylorGreenVortex_InitialCondition
+    /** Calls the Function(const unsigned int n_components) constructor in deal.II
+     *  This sets the public attribute n_components = nstate, which can then be accessed
+     *  by all the other functions
+     */
+    TaylorGreenVortex_InitialCondition (
+        const unsigned int nstate,
+        const double       gamma_gas,
+        const double       mach_inf_sqr);
+
+    const double gamma_gas; ///< Constant heat capacity ratio of fluid.
+    const double mach_inf_sqr; ///< Farfield Mach number squared.
+        
+    /// Value of initial condition expressed in terms of conservative variables
+    real value (const dealii::Point<dim> &point, const unsigned int istate = 0) const override;
+    /// Gradient of initial condition expressed in terms of conservative variables
+    dealii::Tensor<1,dim,real> gradient (const dealii::Point<dim,real> &point, const unsigned int istate = 0) const override;
+protected:
+    /// Value of initial condition expressed in terms of primitive variables
+    real primitive_value(const dealii::Point<dim,real> &point, const unsigned int istate = 0) const;
+    /// Gradient of initial condition expressed in terms of primitive variables
+    dealii::Tensor<1,dim,real> primitive_gradient (const dealii::Point<dim,real> &point, const unsigned int istate = 0) const;
+    /// Hessian of initial condition expressed in terms of primitive variables
+    dealii::SymmetricTensor<2,dim,real> primitive_hessian (const dealii::Point<dim,real> &point, const unsigned int istate = 0) const;
+    /// Converts value from: primitive to conservative
+    real convert_primitive_to_conversative_value(const dealii::Point<dim,real> &point, const unsigned int istate = 0) const;
+    /// Converts gradient from: primitive to conservative
+    dealii::Tensor<1,dim,real> convert_primitive_to_conversative_gradient (const dealii::Point<dim,real> &point, const unsigned int istate = 0) const;
+};
+
+// protected:
+//     using dealii::Function<dim,real>::value;
+
+} // Tests namespace
+} // PHiLiP namespace
+#endif

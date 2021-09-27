0d87a9c7fbff01f310f8ab7478baacc53806f51e
Julien Brillon <julien.brillon@mail.mcgill.ca>
2021-09-27 19:35:59 -0400

    Option to run on tmpdir by default

--- job_parameters_file_PHiLiP.sh
+++ job_parameters_file_PHiLiP.sh
@@ -2,7 +2,7 @@
 #SBATCH --time=2:00:00
 #SBATCH --account=rrg-nadaraja-ac
 #SBATCH --job-name=run_parameters_file
-#SBATCH --output=%x.out ##%x-%j.out                   ## <-- use this to append job ID
+#SBATCH --output=%x.out
 #SBATCH --nodes=1
 #SBATCH --ntasks-per-node=2                           ## <-- there are max 40 per node
 #SBATCH --mem=32G                                     ## <-- total shared memory; default unit is megabytes
@@ -13,7 +13,9 @@ SLURM_USER="username"                    ## <-- Enter beluga username here
 PARAMETERS_FILE="my_parameters_file.prm" ## <-- Enter .prm filename here
 PHiLiP_DIMENSIONS="2"                    ## WARNING: must correspond to the DIM in the .prm file
 NUM_PROCS="2"                            ## WARNING: must correspond to --ntasks
-PHiLiP_EXECUTABLE_NAME="PHiLiP_${PHiLiP_DIMENSIONS}D"
+RUN_ON_TMPDIR=true                       ## Set as true for fast write speeds (default) -- WARNING: Output files will only be copied to your output file directory once `mpirun` has completed. 
+
+PHiLiP_EXECUTABLE="/home/${SLURM_USER}/scratch/PHiLiP_${PHiLiP_DIMENSIONS}D"
 OUTPUT_FILES_DIRECTORY_NAME="output_files"
 
 ## Below are the modules needed to run the executable
@@ -23,13 +25,17 @@ module load StdEnv/2020 # not needed?
 module load gcc/9.3.0 # not needed?
 module load openmpi/4.0.3 # required
 
-cd ${SLURM_TMPDIR}
-
-mpirun -n ${NUM_PROCS} "${SLURM_SUBMIT_DIR}/${PHiLiP_EXECUTABLE_NAME}" -i "${SLURM_SUBMIT_DIR}/${PARAMETERS_FILE}"
-
 if ! [ -d "${SLURM_SUBMIT_DIR}/${OUTPUT_FILES_DIRECTORY_NAME}" ]; then
         mkdir ${SLURM_SUBMIT_DIR}/${OUTPUT_FILES_DIRECTORY_NAME};
 fi
 
-# Get output files, exclude subdirectories
-rsync -axvH --no-g --no-p --exclude='*/' ${SLURM_TMPDIR}/* ${SLURM_SUBMIT_DIR}/${OUTPUT_FILES_DIRECTORY_NAME}
+if [ ${RUN_ON_TMPDIR} = true ]; then
+        cd ${SLURM_TMPDIR};      
+fi
+
+mpirun -n ${NUM_PROCS} "${PHiLiP_EXECUTABLE}" -i "${SLURM_SUBMIT_DIR}/${PARAMETERS_FILE}"
+
+if [ ${RUN_ON_TMPDIR} = true ]; then
+        # Get output files, exclude subdirectories
+        rsync -axvH --no-g --no-p --exclude='*/' ${SLURM_TMPDIR}/* ${SLURM_SUBMIT_DIR}/${OUTPUT_FILES_DIRECTORY_NAME};
+fi

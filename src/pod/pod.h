398b9378ce65678ba4dbdca742894898c5c0d71e
donovan97 <donovan.blais@outlook.com>
2021-09-30 20:58:58 -0400

    Untab POD class definition once. Removed tests with no checking condition (burgers_rewienski_snapshot.cpp and reduced_order.cpp) from CMakeLists and tests.cpp.

--- src/pod/pod.h
+++ src/pod/pod.h
@@ -12,31 +12,31 @@
 namespace PHiLiP {
 namespace ProperOrthogonalDecomposition {
 
-    /// Class for Proper Orthogonal Decomposition reduced order modelling
-    class POD
-    {
-    public:
+/// Class for Proper Orthogonal Decomposition reduced order modelling
+class POD
+{
+public:
 
-        int num_basis; ///< Number of basis functions to keep for the reduced order model
-        std::unique_ptr<dealii::FullMatrix<double>> svd_u; ///< U matrix output from SVD
-        std::unique_ptr<dealii::FullMatrix<double>> pod_basis; ///< First num_basis columns of svd_u
+    int num_basis; ///< Number of basis functions to keep for the reduced order model
+    std::unique_ptr<dealii::FullMatrix<double>> svd_u; ///< U matrix output from SVD
+    std::unique_ptr<dealii::FullMatrix<double>> pod_basis; ///< First num_basis columns of svd_u
 
-        /// Constructor
-        POD(int num_basis);
+    /// Constructor
+    POD(int num_basis);
 
-        /// Destructor
-        ~POD () {};
+    /// Destructor
+    ~POD () {};
 
-        /// Get full POD basis consisting of svd_u
-        dealii::FullMatrix<double> get_full_pod_basis();
+    /// Get full POD basis consisting of svd_u
+    dealii::FullMatrix<double> get_full_pod_basis();
 
-        /// Get reduced POD basis consisting of the first num_basis columns of svd_u
-        dealii::FullMatrix<double> get_reduced_pod_basis();
+    /// Get reduced POD basis consisting of the first num_basis columns of svd_u
+    dealii::FullMatrix<double> get_reduced_pod_basis();
 
-    protected:
-        const MPI_Comm mpi_communicator; ///< MPI communicator.
-        dealii::ConditionalOStream pcout; ///< Parallel std::cout that only outputs on mpi_rank==0
-    };
+protected:
+    const MPI_Comm mpi_communicator; ///< MPI communicator.
+    dealii::ConditionalOStream pcout; ///< Parallel std::cout that only outputs on mpi_rank==0
+};
 
 }
 }

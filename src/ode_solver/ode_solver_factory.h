9a19788a46a0b4f44d6c7cac2a22efce0e27415b
Donovan Blais <48806319+donovan97@users.noreply.github.com>
2022-01-27 09:42:24 -0500

    Update src/ode_solver/ode_solver_factory.h
    Co-authored-by: Julien Brillon <43619715+jbrillon@users.noreply.github.com>

--- src/ode_solver/ode_solver_factory.h
+++ src/ode_solver/ode_solver_factory.h
@@ -25,7 +25,7 @@ public:
     /// Creates either POD-Galerkin or POD-Petrov-Galerkin ODE solver based on parameter value (POD basis given)
     static std::shared_ptr<ODESolverBase<dim,real,MeshType>> create_ODESolver(std::shared_ptr< DGBase<dim, real, MeshType> > dg_input, std::shared_ptr<ProperOrthogonalDecomposition::POD<dim>> pod);
 
-    /// Creates either implicit or explicit ODE solver based on manual input(no POD basis given)
+    /// Creates either implicit or explicit ODE solver based on manual input (no POD basis given)
     static std::shared_ptr<ODESolverBase<dim,real,MeshType>> create_ODESolver_manual(Parameters::ODESolverParam::ODESolverEnum ode_solver_type, std::shared_ptr< DGBase<dim, real, MeshType> > dg_input);
 
     /// Creates either POD-Galerkin or POD-Petrov-Galerkin ODE solver based on manual input (POD basis given)

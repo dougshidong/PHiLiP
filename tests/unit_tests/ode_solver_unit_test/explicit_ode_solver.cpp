d7366eb4cbfffa897d662ea5c8d27dcbe56e0a3a
cpethrick <c.pethrick@gmail.com>
2022-02-01 15:09:35 -0500

    Modified to allow 1D periodic advection

--- tests/unit_tests/ode_solver_unit_test/explicit_ode_solver.cpp
+++ tests/unit_tests/ode_solver_unit_test/explicit_ode_solver.cpp
@@ -44,8 +44,14 @@ const int dim = PHILIP_DIM;
     //setting periodic BC 
     //to do: change this to work with 1D
     std::vector<dealii::GridTools::PeriodicFacePair<typename Triangulation::cell_iterator> > matched_pairs;
-    dealii::GridTools::collect_periodic_faces(*grid,0,1,0,matched_pairs);
-    dealii::GridTools::collect_periodic_faces(*grid,2,3,1,matched_pairs);
+    if (dim == 2){
+        dealii::GridTools::collect_periodic_faces(*grid,0,1,0,matched_pairs);
+        dealii::GridTools::collect_periodic_faces(*grid,2,3,1,matched_pairs);
+    }
+    else if (dim == 1){
+        dealii::GridTools::collect_periodic_faces(*grid,0,1,0,matched_pairs);
+    }
+
     grid->add_periodicity(matched_pairs);
 
     grid->refine_global(n_refinements);
@@ -61,10 +67,16 @@ const int dim = PHILIP_DIM;
     all_parameters.ode_solver_param.nonlinear_max_iterations = 500;
     all_parameters.ode_solver_param.print_iteration_modulo = 100;
     
-    const double dt = 0.01; // later refine this 
+
+    int n_time_refinements = 4;
+    double dt = 0.1;
+    const double refine_ratio = 0.1;
+
+    for (int i = 0; i < n_time_refinements; ++i){
 
     //initial_time_step is not modified by explicit ODE solver
     all_parameters.ode_solver_param.initial_time_step = dt;
+    std::cout << "Using time step = " << dt << std::endl;
 
     unsigned int space_poly_degree = 5;
     std::shared_ptr < PHiLiP::DGBase<dim, double> > dg = PHiLiP::DGFactory<dim,double>::create_discontinuous_galerkin(&all_parameters, space_poly_degree, grid);
@@ -72,23 +84,31 @@ const int dim = PHILIP_DIM;
 
     //initial conditions
     //to do: make 1D ICs
-#if PHILIP_DIM == 2	
+
     std::cout << "Implement initial conditions" << std::endl;
-    dealii::FunctionParser<2> initial_condition;
-    std::string variables = "x,y";
+    dealii::FunctionParser<dim> initial_condition;
+    std::string variables;
     std::map<std::string,double> constants;
     constants["pi"] = dealii::numbers::PI;
     std::string expression_initial = "exp( -( 20*(x-1)*(x-1) + 20*(y-1)*(y-1) ) )";
+    if (dim == 2){
+        variables = "x,y";
+        expression_initial = "exp( -( 20*(x-1)*(x-1) + 20*(y-1)*(y-1) ) )";
+    }
+    else if (dim == 1){
+        variables = "x";
+        expression_initial = "exp(- 20 * (x-1) * (x-1))";	
+    }
     initial_condition.initialize(variables,
-    expression_initial,
-    constants);
+		    expression_initial,
+	       	    constants);
     dealii::VectorTools::interpolate(dg->dof_handler,initial_condition,dg->solution);
-#endif
     
+
     // Create ODE solver using the factory and providing the DG object
     std::shared_ptr<PHiLiP::ODE::ODESolverBase<dim, double>> ode_solver = PHiLiP::ODE::ODESolverFactory<dim, double>::create_ODESolver(dg);
 
-    double finalTime = 0.0;//1;
+    double finalTime = 0.0;//0.1;
 
     //double dt = all_parameters->ode_solver_param.initial_time_step;
     ode_solver->advance_solution_time(finalTime);
@@ -101,14 +121,22 @@ const int dim = PHILIP_DIM;
     //		VectorTools::compute_global_error()
     //	eventually also format into table
     //
-    //
-   const double advection_speed = 1.0;
+    
+    //THESE ADVECTION SPEEDS ARE INCORRECT
+   const double advection_speed_x = 1.1;
+   const double advection_speed_y = -atan(1)*4.0/exp(1); //from convection_diffusion in physics
 
-#if PHILIP_DIM == 2
-    dealii::FunctionParser<2> exact_solution;
-    constants["a"] = advection_speed; //CHECK WHERE THIS IS STORED
+    dealii::FunctionParser<dim> exact_solution;
+    constants["a_x"] = advection_speed_x; //CHECK WHERE THIS IS STORED
+    constants["a_y"] = advection_speed_y;
     constants["t"] = finalTime;
-    std::string expression_exact = "exp( -( 20*(x-1-a*t)*(x-1-a*t) + 20*(y-1-0*t)*(y-1-0*t) ) )";
+    std::string expression_exact;
+    if (dim == 2){
+        expression_exact = "exp( -( 20*(x-1-a_x*t)*(x-1-a_x*t) + 20*(y-1-a_y*t)*(y-1-a_y*t) ) )";
+    }
+    else if (dim == 1){
+        expression_exact = "exp( - 20*(x-1-a_x*t)*(x-1-a_x*t)) ";
+    }
     exact_solution.initialize(variables,
     		    expression_exact,
     		    constants);
@@ -118,15 +146,17 @@ const int dim = PHILIP_DIM;
 		    dg->solution, 
 		    exact_solution, 
 		    difference_per_cell, 
-		    dealii::QGauss<dim>(2), //check that this is correct polynomial degree
+		    dealii::QGauss<dim>(space_poly_degree+1), //check that this is correct polynomial degree
 		    dealii::VectorTools::L2_norm);
     const double L2_error = 
 	    dealii::VectorTools::compute_global_error(*grid,
 			    difference_per_cell,
 			    dealii::VectorTools::L2_norm);
     std::cout << "Computed error is " << L2_error << std::endl;
-#endif
+    std::cout << "Number of cells is "<< grid->n_active_cells()<<std::endl;
 
+    dt *= refine_ratio;
+    }//time refinement loop
     //notes
     //	when finalTime = 0, computed error is 1.7739e-08
 

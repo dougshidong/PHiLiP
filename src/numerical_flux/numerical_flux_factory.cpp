03291f2d21d8e9126de60602ce807a13c7346307
Julien Brillon <43619715+jbrillon@users.noreply.github.com>
2022-04-07 17:02:50 -0400

    progress backup on seg. fault fix -- will not compile

--- src/numerical_flux/numerical_flux_factory.cpp
+++ src/numerical_flux/numerical_flux_factory.cpp
@@ -15,12 +15,14 @@ NumericalFluxFactory<dim, nstate, real>
     AllParam::ConvectiveNumericalFlux conv_num_flux_type,
     std::shared_ptr<Physics::PhysicsBase<dim, nstate, real>> physics_input)
 {
+    // checks if conv_numerical_flux_type exists only for Euler equations
+    const bool is_euler_flux = ((conv_num_flux_type == AllParam::roe) ||
+                                (conv_num_flux_type == AllParam::l2roe));
+
     if(conv_num_flux_type == AllParam::lax_friedrichs) {
         return std::make_unique< LaxFriedrichs<dim, nstate, real> > (physics_input);
-    } else if(conv_num_flux_type == AllParam::roe) {
-        if constexpr (dim+2==nstate) return std::make_unique< RoePike<dim, nstate, real> > (physics_input);
-    } else if(conv_num_flux_type == AllParam::l2roe) {
-        if constexpr (dim+2==nstate) return std::make_unique< L2Roe<dim, nstate, real> > (physics_input);
+    } else if(is_euler_flux) {
+        return create_euler_convective_numerical_flux(conv_num_flux_type, physics_input);
     } else if (conv_num_flux_type == AllParam::split_form) {
         return std::make_unique< SplitFormNumFlux<dim, nstate, real> > (physics_input);
     }
@@ -28,6 +30,47 @@ NumericalFluxFactory<dim, nstate, real>
     std::cout << "Invalid convective numerical flux" << std::endl;
     return nullptr;
 }
+
+template <int dim, int nstate, typename real>
+std::unique_ptr< NumericalFluxConvective<dim,nstate,real> >
+NumericalFluxFactory<dim, nstate, real>
+::create_euler_convective_numerical_flux(
+    AllParam::ConvectiveNumericalFlux conv_num_flux_type,
+    std::shared_ptr<Physics::PhysicsBase<dim, nstate, real>> physics_input)
+{
+#if PHILIP_DIM==3
+    std::shared_ptr<Physics::PhysicsBase<dim, nstate, real>> physics_to_be_passed = physics_input;
+    try { 
+        // check if physics_input is a PhysicsModel
+        const auto &physics_model = dynamic_cast<const Physics::PhysicsModel<dim,dim+2,real,dim+2>&>(*physics_input);
+        // check if the physics_baseline in the PhysicsModel object is Euler or derived from Euler (e.g. NavierStokes)
+        const auto &physics_baseline = dynamic_cast<const Physics::Euler<dim,dim+2,real>&>(*physics_model->physics_baseline);
+        
+        if(physics_model->n_model_equations > 0) {
+            std::cout << "Error: Cannot create_euler_convective_numerical_flux() for nstate_baseline_physics != nstate." << std::endl;
+            std::abort();
+        } else {
+            // pass the baseline physics object of type Euler
+            physics_to_be_passed = physics_model->physics_baseline;
+        }
+    } catch (std::bad_cast&) {
+        std::cout << "Invalid convective numerical flux for physics_model and/or corresponding baseline_physics_type" << std::endl;
+        std::abort();
+    }
+    if(conv_num_flux_type == AllParam::roe) {
+        if constexpr (dim+2==nstate) return std::make_unique< RoePike<dim, nstate, real> > (physics_to_be_passed);
+    } else if(conv_num_flux_type == AllParam::l2roe) {
+        if constexpr (dim+2==nstate) return std::make_unique< L2Roe<dim, nstate, real> > (physics_to_be_passed);
+    }
+#else
+    if(conv_num_flux_type == AllParam::roe) {
+        if constexpr (dim+2==nstate) return std::make_unique< RoePike<dim, nstate, real> > (physics_input);
+    } else if(conv_num_flux_type == AllParam::l2roe) {
+        if constexpr (dim+2==nstate) return std::make_unique< L2Roe<dim, nstate, real> > (physics_input);
+    }
+#endif
+}
+
 template <int dim, int nstate, typename real>
 std::unique_ptr< NumericalFluxDissipative<dim,nstate,real> >
 NumericalFluxFactory<dim, nstate, real>

7f38f792fa2549e436040ddc01c6d4a420829cf6
Julien Brillon <43619715+jbrillon@users.noreply.github.com>
2022-04-14 22:26:07 -0400

    proof of concept for saving and loading solution the files

--- src/testing/taylor_green_vortex_restart_check.cpp
+++ src/testing/taylor_green_vortex_restart_check.cpp
@@ -18,6 +18,22 @@ int TaylorGreenVortexRestartCheck<dim, nstate>::run_test() const
     std::unique_ptr<FlowSolver<dim,nstate>> flow_solver = FlowSolverFactory<dim,nstate>::create_FlowSolver(this->all_parameters);
     static_cast<void>(flow_solver->run_test());
 
+    dealii::LinearAlgebra::distributed::Vector<double> old_solution(flow_solver->dg->solution);
+    old_solution.update_ghost_values();
+    dealii::parallel::distributed::SolutionTransfer<dim, dealii::LinearAlgebra::distributed::Vector<double>, dealii::DoFHandler<dim>> solution_transfer(flow_solver->dg->dof_handler);
+    solution_transfer.prepare_for_coarsening_and_refinement(old_solution);
+    flow_solver->dg->triangulation->save("saved-solution");
+
+    // loading the file
+    flow_solver->dg->triangulation->load("saved-solution");
+    // --- after allocate_dg
+    // TO DO: Read section "Note on usage with DoFHandler with hp-capabilities" and add the stuff im missing
+    // ------ Ref: https://www.dealii.org/current/doxygen/deal.II/classparallel_1_1distributed_1_1SolutionTransfer.html
+    dealii::parallel::distributed::SolutionTransfer<dim, dealii::LinearAlgebra::distributed::Vector<double>, dealii::DoFHandler<dim>> solution_transfer2(flow_solver->dg->dof_handler);
+    flow_solver->dg->solution.zero_out_ghosts();
+    solution_transfer2.deserialize(flow_solver->dg->solution);
+    flow_solver->dg->solution.update_ghost_values();
+
     // Compute kinetic energy
     std::unique_ptr<PeriodicCubeFlow<dim, nstate>> flow_solver_case = std::make_unique<PeriodicCubeFlow<dim,nstate>>(this->all_parameters);
     const double kinetic_energy_computed = flow_solver_case->compute_kinetic_energy(*(flow_solver->dg));

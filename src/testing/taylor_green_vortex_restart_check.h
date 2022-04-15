a613ff6e4315445e9d20e9a4ecbf1c2fad69f989
Julien Brillon <43619715+jbrillon@users.noreply.github.com>
2022-04-15 12:46:47 -0400

    solution saved matches solution loaded from restart file -- ready to implement in FlowSolver

--- src/testing/taylor_green_vortex_restart_check.h
+++ src/testing/taylor_green_vortex_restart_check.h
@@ -2,6 +2,7 @@
 #define __TAYLOR_GREEN_VORTEX_RESTART_CHECK__
 
 #include "tests.h"
+#include "dg/dg.h"
 
 namespace PHiLiP {
 namespace Tests {
@@ -22,6 +23,15 @@ public:
 
     /// Run test
     int run_test () const override;
+protected:
+    double compare_solutions(
+        DGBase<dim, double> &dg,
+        const dealii::LinearAlgebra::distributed::Vector<double> solution_reference,
+        const dealii::LinearAlgebra::distributed::Vector<double> solution_to_be_checked) const;
+
+    double integrate_solution_over_domain(
+        DGBase<dim, double> &dg,
+        const dealii::LinearAlgebra::distributed::Vector<double> solution_input) const;
 };
 
 } // End of Tests namespace

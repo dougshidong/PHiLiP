5865e7f117cdf8c7ab05db00b3006e5f594cfab3
Julien Brillon <43619715+jbrillon@users.noreply.github.com>
2022-04-26 13:01:20 -0400

    updating get_NACA0012_mesh_files_cluster.sh

--- get_NACA0012_mesh_files_cluster.sh
+++ get_NACA0012_mesh_files_cluster.sh
@@ -2,7 +2,7 @@
 # DESCRIPTION: Copies the large mesh files for NACA0012 that cannot be stored on GitHub
 # NOTE: This is currently setup only for the Narval cluster
 PATH_TO_FILES=~/projects/def-nadaraja/Libraries/NACA0012MeshFiles
-FILENAMES=(naca0012_hopw_ref2.msh naca0012.msh)
+FILENAMES=(naca0012_hopw_ref5.msh naca0012_hopw_ref4.msh naca0012_hopw_ref3.msh naca0012_hopw_ref2.msh naca0012_hopw_ref1.msh naca0012.msh)
 TARGET_DIR=tests/integration_tests_control_files/euler_integration/naca0012/
 
 for file in ${FILENAMES[@]}; do

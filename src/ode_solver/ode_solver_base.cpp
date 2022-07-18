5ccf71146d24fa84f5e1b4d8284ecc49a62d0316
Maya Tatarelli <49986410+MayaTatarelli@users.noreply.github.com>
2022-07-18 16:02:06 -0400

    temporary change to patch seg fault when using p-refinement

--- src/ode_solver/ode_solver_base.cpp
+++ src/ode_solver/ode_solver_base.cpp
@@ -202,14 +202,14 @@ int ODESolverBase<dim,real,MeshType>::steady_state (const bool output_solution_f
                             && this->residual_norm_decrease > ode_param.nonlinear_steady_residual_tolerance;
     }
     // TO DO: Uncomment this later -- testing
-    // if (this->residual_norm > 1e5
-    //     || std::isnan(this->residual_norm)
-    //     || CFL_factor <= 1e-2)
-    // {
-    //     this->dg->solution = initial_solution;
+    if (this->residual_norm > 1e5
+        || std::isnan(this->residual_norm)
+        || CFL_factor <= 1e-2)
+    {
+    //    this->dg->solution = initial_solution;
 
-    //     if(CFL_factor <= 1e-2) this->dg->right_hand_side.add(1.0);
-    // }
+        if(CFL_factor <= 1e-2) this->dg->right_hand_side.add(1.0);
+    }
 
     if (ode_param.output_solution_vector_modulo > 0) {
         for (unsigned int i = 0; i < this->dg->solution.size(); ++i) {

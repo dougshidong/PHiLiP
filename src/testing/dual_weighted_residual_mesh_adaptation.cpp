2d22e3961528be8d9e4bbbea4d20060c059f0ae0
PranshulThakur <pranshul777@gmail.com>
2022-08-18 16:35:18 -0400

    Cleaned up comments and added the option to use rectangular non periodic grid

--- src/testing/dual_weighted_residual_mesh_adaptation.cpp
+++ src/testing/dual_weighted_residual_mesh_adaptation.cpp
@@ -30,59 +30,21 @@ int DualWeightedResidualMeshAdaptation<dim, nstate> :: run_test () const
     
     std::unique_ptr<FlowSolver::FlowSolver<dim,nstate>> flow_solver = FlowSolver::FlowSolverFactory<dim,nstate>::select_flow_case(&param, parameter_handler);
     flow_solver->run();
-   /* 
-    for (unsigned int poly_degree = p_start; poly_degree <= p_end; ++poly_degree)
+
+    // Check location of the most refined cell
+    dealii::Point<dim> refined_cell_coord = flow_solver->dg->coordinates_of_highest_refined_cell(check_for_p_refined_cell);
+    pcout<<" Coordinates of the most refined cell (x,y) = ( "<<refined_cell_coord[0]<<", "<<refined_cell_coord[1]<<")"<<std::endl;
+    // Check if the mesh is refined near the shock i.e x,y in [0.3,0.6].
+    if ((refined_cell_coord[0] > 0.3) && (refined_cell_coord[0] < 0.6) && (refined_cell_coord[1] > 0.3) && (refined_cell_coord[1] < 0.6))
     {
-        for (unsigned int igrid=0; igrid<n_grids; ++igrid) 
-        {
-            // Create grid.
-            using Triangulation = dealii::parallel::distributed::Triangulation<dim>;
-            std::shared_ptr<Triangulation> grid = std::make_shared<Triangulation>(
-                 MPI_COMM_WORLD,
-                 typename dealii::Triangulation<dim>::MeshSmoothing(
-                 dealii::Triangulation<dim>::smoothing_on_refinement |
-                 dealii::Triangulation<dim>::smoothing_on_coarsening));
-
-            // Currently, the domain is [0,1]
-            bool colorize = true;
-            dealii::GridGenerator::hyper_cube(*grid, 0, 1, colorize);
-            const int steps_to_create_grid = initial_grid_size + igrid;
-            grid->refine_global(steps_to_create_grid);
-
-            std::shared_ptr< DGBase<dim, double, Triangulation> > dg
-                = DGFactory<dim,double,Triangulation>::create_discontinuous_galerkin(
-                 &param,
-                 poly_degree,
-                 poly_degree+6,
-                 poly_degree,
-                 grid);
-    
-            dg->allocate_system();
-    
-            InitialConditionFunction_Zero<dim,nstate,double> initial_conditions;
-            const auto mapping = *(flow_solver->dg->high_order_grid->mapping_fe_field);
-            dealii::VectorTools::interpolate(mapping, flow_solver->dg->dof_handler, initial_conditions, flow_solver->dg->solution);
-        
-            // generate ODE solver
-            std::shared_ptr< ODE::ODESolverBase<dim,double,Triangulation> > ode_solver = ODE::ODESolverFactory<dim,double,Triangulation>::create_ODESolver(dg);
-
-            ode_solver->steady_state();
-        */  
-
-        // Check location of the most refined cell
-        dealii::Point<dim> refined_cell_coord = flow_solver->dg->coordinates_of_highest_refined_cell(check_for_p_refined_cell);
-        pcout<<" Coordinates of the most refined cell (x,y) = ( "<<refined_cell_coord[0]<<", "<<refined_cell_coord[1]<<")"<<std::endl;
-        // Check if the mesh is refined near the shock i.e x,y in [0.3,0.6].
-        if ((refined_cell_coord[0] > 0.3) && (refined_cell_coord[0] < 0.6) && (refined_cell_coord[1] > 0.3) && (refined_cell_coord[1] < 0.6))
-        {
-            pcout<<"Mesh is refined near the shock. Test passed!"<<std::endl;
-            return 0; // Mesh adaptation test passed.
-        }
-        else
-        {
-            pcout<<"Mesh Adaptation has failed."<<std::endl;
-            return 1; // Mesh adaptation failed.
-        }
+        pcout<<"Mesh is refined near the shock. Test passed!"<<std::endl;
+        return 0; // Mesh adaptation test passed.
+    }
+    else
+    {
+        pcout<<"Mesh Adaptation has failed."<<std::endl;
+        return 1; // Mesh adaptation failed.
+    }
 }
 
 #if PHILIP_DIM==2

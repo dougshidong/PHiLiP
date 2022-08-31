08c6378b4b3710c6016c4e7c7ddbb77c228f6635
jbrillon <43619715+jbrillon@users.noreply.github.com>
2022-08-31 17:52:12 -0400

    progress on reading the file

--- src/physics/initial_conditions/set_initial_condition.cpp
+++ src/physics/initial_conditions/set_initial_condition.cpp
@@ -90,6 +90,137 @@ void SetInitialCondition<dim,nstate,real>::project_initial_condition(
     }
 }
 
+template <int dim, int nstate>
+std::string FlowSolver<dim,nstate>::get_restart_filename_without_extension(const int restart_index_input) const {
+    // returns the restart file index as a string with appropriate padding
+    std::string restart_index_string = std::to_string(restart_index_input);
+    const unsigned int length_of_index_with_padding = 5;
+    const int number_of_zeros = length_of_index_with_padding - restart_index_string.length();
+    restart_index_string.insert(0, number_of_zeros, '0');
+
+    const std::string prefix = "restart-";
+    const std::string restart_filename_without_extension = prefix+restart_index_string;
+
+    return restart_filename_without_extension;
+}
+
+template<int dim, int nstate, typename real>
+void SetInitialCondition<dim,nstate,real>::read_values_from_file_and_project(
+        std::shared_ptr < PHiLiP::DGBase<dim,real> > &dg) 
+{
+    // (1) Get filename based on MPI rank
+    //-------------------------------------------------------------
+    int mpi_rank = dealii::Utilities::MPI::this_mpi_process(dg->mpi_communicator);
+    // -- Get padding
+    std::string mpi_rank_string = std::to_string(mpi_rank);
+    const unsigned int length_of_index_with_padding = 5;
+    const int number_of_zeros = length_of_index_with_padding - mpi_rank_string.length();
+    mpi_rank_string.insert(0, number_of_zeros, '0');
+    // -- Assemble filename string
+    const std::string prefix = "setup-";
+    const std::string filename_without_extension = prefix+mpi_rank_string;
+    const std::string filename = filename_without_extension + std::to_string(".dat");
+    //-------------------------------------------------------------
+
+    // (2) Read file
+    //-------------------------------------------------------------
+    std::string line;
+    std::string::size_type sz1;
+
+    std::ifstream FILE (filename);
+    std::getline(FILE, line); // read first line: DOFs
+    
+    // check that the file is not empty
+    if (line.empty()) {
+        pcout << "Error: Trying to read empty file named " << data_table_filename << std::endl;
+        std::abort();
+    } else {
+        const unsigned int number_of_degrees_of_freedom_per_state_DG = dg->dof_handler.n_dofs()/nstate;
+        const unsigned int number_of_degrees_of_freedom_per_state_file = std::stoi(line);
+        if(number_of_degrees_of_freedom_per_state_file != number_of_degrees_of_freedom_per_state_DG) {
+            std::cout << "ERROR: Cannot read initial condition. "
+                      << "Number of degrees of freedom per state do not match expected by DG in file: " 
+                      << filename "\n Aborting..." << std::endl;
+            std::abort();
+        }
+    }
+
+    std::getline(FILE, line); // read first line of data
+
+    // TO DO: Continue from here
+    
+    // check that there indeed is data to be read
+    if (line.empty()) {
+        pcout << "Error: Table has no data to be read" << std::endl;
+        std::abort();
+    }
+
+    std::vector<double> current_line_values(number_of_columns);
+    while (!line.empty()) {
+        std::string dummy_line = line;
+
+        current_line_values[0] = std::stod(dummy_line,&sz1);
+        for(int i=1; i<number_of_columns; ++i) {
+            dummy_line = dummy_line.substr(sz1);
+            sz1 = 0;
+            current_line_values[i] = std::stod(dummy_line,&sz1);
+        }
+
+        // Add data entries to table
+        for(int i=0; i<number_of_columns; ++i) {
+            data_table->add_value(data_column_names[i], current_line_values[i]);
+            data_table->set_precision(data_column_names[i], 16);
+            data_table->set_scientific(data_column_names[i], true);
+        }
+        std::getline(FILE, line); // read next line
+    }
+
+    // Commented since this has not yet been tested
+    // dealii::LinearAlgebra::distributed::Vector<double> solution_no_ghost;
+    // solution_no_ghost.reinit(dg->locally_owned_dofs, MPI_COMM_WORLD);
+    // dealii::AffineConstraints affine_constraints(dof_handler.locally_owned_dofs());
+    // dealii::VectorTools::project(*(dg->high_order_grid->mapping_fe_field),dg->dof_handler,affine_constraints,dg->volume_quadrature_collection,*initial_condition_function,solution_no_ghost);
+    // dg->solution = solution_no_ghost;
+
+    //Note that for curvilinear, can't use dealii interpolate since it doesn't project at the correct order.
+    //Thus we interpolate it directly.
+    const auto mapping = (*(dg->high_order_grid->mapping_fe_field));
+    dealii::hp::MappingCollection<dim> mapping_collection(mapping);
+    dealii::hp::FEValues<dim,dim> fe_values_collection(mapping_collection, dg->fe_collection, dg->volume_quadrature_collection, 
+                                dealii::update_quadrature_points);
+    const unsigned int max_dofs_per_cell = dg->dof_handler.get_fe_collection().max_dofs_per_cell();
+    std::vector<dealii::types::global_dof_index> current_dofs_indices(max_dofs_per_cell);
+    OPERATOR::vol_projection_operator<dim,2*dim> vol_projection(1, dg->max_degree, dg->max_grid_degree);
+    vol_projection.build_1D_volume_operator(dg->oneD_fe_collection_1state[dg->max_degree], dg->oneD_quadrature_collection[dg->max_degree]);
+    for (auto current_cell = dg->dof_handler.begin_active(); current_cell!=dg->dof_handler.end(); ++current_cell) {
+        if (!current_cell->is_locally_owned()) continue;
+    
+        const int i_fele = current_cell->active_fe_index();
+        const int i_quad = i_fele;
+        const int i_mapp = 0;
+        fe_values_collection.reinit (current_cell, i_quad, i_mapp, i_fele);
+        const dealii::FEValues<dim,dim> &fe_values = fe_values_collection.get_present_fe_values();
+        const unsigned int poly_degree = i_fele;
+        const unsigned int n_quad_pts = dg->volume_quadrature_collection[poly_degree].size();
+        const unsigned int n_dofs_cell = dg->fe_collection[poly_degree].dofs_per_cell;
+        const unsigned int n_shape_fns = n_dofs_cell/nstate;
+        current_dofs_indices.resize(n_dofs_cell);
+        current_cell->get_dof_indices (current_dofs_indices);
+        for(int istate=0; istate<nstate; istate++){
+            std::vector<double> exact_value(n_quad_pts);
+            for(unsigned int iquad=0; iquad<n_quad_pts; iquad++){
+                const dealii::Point<dim> qpoint = (fe_values.quadrature_point(iquad));
+                exact_value[iquad] = initial_condition_function->value(qpoint, istate);
+            }   
+            std::vector<double> sol(n_shape_fns);
+            vol_projection.matrix_vector_mult_1D(exact_value, sol, vol_projection.oneD_vol_operator);
+            for(unsigned int ishape=0; ishape<n_shape_fns; ishape++){
+                dg->solution[current_dofs_indices[ishape+istate*n_shape_fns]] = sol[ishape];
+            }
+        }
+    }
+}
+
 template class SetInitialCondition<PHILIP_DIM, 1, double>;
 template class SetInitialCondition<PHILIP_DIM, 2, double>;
 template class SetInitialCondition<PHILIP_DIM, 3, double>;

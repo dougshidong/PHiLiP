7bbed740b515725363a8eae9493421a03610c354
Julien Brillon <43619715+jbrillon@users.noreply.github.com>
2022-09-02 11:02:02 -0400

    MPI_3D_EULER_INTEGRATION_GAUSSIAN_BUMP now passing

--- src/physics/initial_conditions/initial_condition.cpp
+++ src/physics/initial_conditions/initial_condition.cpp
@@ -219,7 +219,7 @@ InitialConditionFactory<dim,nstate, real>::create_InitialConditionFunction(
     } else if (flow_type == FlowCaseEnum::burgers_viscous_snapshot) {
         if constexpr (dim==1 && nstate==dim)  return std::make_shared<InitialConditionFunction_BurgersViscous<dim,nstate,real> > ();
     } else if (flow_type == FlowCaseEnum::naca0012 || flow_type == FlowCaseEnum::gaussian_bump) {
-        if constexpr (dim==2 && nstate==dim+2){
+        if constexpr ((dim==2 || dim==3) && nstate==dim+2){
             Physics::Euler<dim,nstate,double> euler_physics_double = Physics::Euler<dim, nstate, double>(
                     param->euler_param.ref_length,
                     param->euler_param.gamma_gas,

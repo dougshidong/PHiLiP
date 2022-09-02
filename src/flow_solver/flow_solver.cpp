c0c54aa10ca0b5f9fe2a1f8b161177c2d0b015b5
PranshulThakur <90003446+PranshulThakur@users.noreply.github.com>
2022-09-02 11:56:04 -0400

    Update src/flow_solver/flow_solver.cpp
    Co-authored-by: Julien Brillon <43619715+jbrillon@users.noreply.github.com>

--- src/flow_solver/flow_solver.cpp
+++ src/flow_solver/flow_solver.cpp
@@ -355,7 +355,7 @@ void FlowSolver<dim,nstate>::perform_steady_state_mesh_adaptation() const
         }
         
         meshadaptation->adapt_mesh();
-        ode_solver->steady_state();
+        this->ode_solver->steady_state();
         residual_norm = ode_solver->residual_norm;
         flow_solver_case->steady_state_postprocessing(dg); 
     }

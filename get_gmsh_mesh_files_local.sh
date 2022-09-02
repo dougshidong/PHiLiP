a3b89c833d47ade7e5d78406426ba7a6b9ed74ac
Julien Brillon <43619715+jbrillon@users.noreply.github.com>
2022-09-02 10:32:18 -0400

    adding 3D gaussian bump mesh and GMSH reader test

--- get_gmsh_mesh_files_local.sh
+++ get_gmsh_mesh_files_local.sh
@@ -6,7 +6,7 @@ gdown 1n5g3KOYJgsnzEtZIhPujbKS5sN0ztYWE # naca0012_hopw_ref2.msh
 gdown 1Iv8qfwh_KCv9KDeOXlBhmCaPTNiPtjQG # naca0012_hopw_ref3.msh
 gdown 15Tt4ZEcZye0Q-P2ynDy67ETqeuCgINjK # naca0012_hopw_ref4.msh
 gdown 1qxWlxhqK3_OrPUe9gcBpUMdny_dfrrBy # naca0012_hopw_ref5.msh
-# gdown  # 3d_gaussian_bump.msh
+gdown 1wpQ_uJcGqkqISiUpim-2kIujurYc2u4R # 3d_gaussian_bump.msh
 gdown 1e_vjwogI9CTIBmolbT0EatnuXcfqP_Pi # SD7003.msh
 # move files to appropriate directory
 mv *.msh ${TARGET_DIR}
@@ -18,5 +18,6 @@ gdown 1HQAoa_dS8U91r0oPPuo1NN9ozXOnjymj # airfoil.msh
 gdown 1Uqyi_JM6qA_Fk7YLSxOLjANdxNMGXWoi # 3D_CUBE_2ndOrder.msh
 gdown 1e_vjwogI9CTIBmolbT0EatnuXcfqP_Pi # SD7003.msh
 gdown 1n5g3KOYJgsnzEtZIhPujbKS5sN0ztYWE # naca0012_hopw_ref2.msh
+gdown 1wpQ_uJcGqkqISiUpim-2kIujurYc2u4R # 3d_gaussian_bump.msh
 # move files to appropriate directory
 mv *.msh ${TARGET_DIR}

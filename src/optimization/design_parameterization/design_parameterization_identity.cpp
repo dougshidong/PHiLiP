ade3824e8bff025b3751c6f1789c8ad2984e15a1
PranshulThakur <pranshul777@gmail.com>
2022-09-23 10:17:28 -0400

    Included previously untracked identity parameterization files

---
+++ src/optimization/design_parameterization/design_parameterization_identity.cpp
@@ -0,0 +1,82 @@
+#include "design_parameterization_identity.hpp"
+#include <deal.II/dofs/dof_tools.h>
+#include <deal.II/lac/dynamic_sparsity_pattern.h>
+#include <deal.II/lac/sparsity_tools.h>
+
+namespace PHiLiP {
+
+template<int dim>
+DesignParameterizationIdentity<dim> :: DesignParameterizationIdentity(
+    std::shared_ptr<HighOrderGrid<dim,double>> _high_order_grid)
+    : DesignParameterizationBase<dim>(_high_order_grid)
+    {}
+
+template<int dim>
+void DesignParameterizationIdentity<dim> :: initialize_design_variables(VectorType &design_var)
+{
+    design_var.reinit(this->high_order_grid->volume_nodes); // Copies both the values and parallel distribution layout.
+    current_volume_nodes = design_var;
+}
+
+template<int dim>
+void DesignParameterizationIdentity<dim> :: compute_dXv_dXp(MatrixType &dXv_dXp) const
+{
+    // There might not be the best way to create parallel partitioned Identity matrix. To be updated if found.
+    const dealii::IndexSet &volume_range = this->high_order_grid->volume_nodes.get_partitioner()->locally_owned_range();
+    const unsigned int n_vol_nodes = this->high_order_grid->volume_nodes.size();
+    dealii::DynamicSparsityPattern dsp(n_vol_nodes, n_vol_nodes, volume_range);
+
+    for(unsigned int i=0; i<n_vol_nodes; ++i)
+    {
+        if(!volume_range.is_element(i)) continue;
+        dsp.add(i,i);
+    }
+
+    dealii::IndexSet locally_relevant_dofs;
+    dealii::DoFTools::extract_locally_relevant_dofs(this->high_order_grid->dof_handler_grid, locally_relevant_dofs);
+
+    dealii::SparsityTools::distribute_sparsity_pattern(dsp, volume_range, this->mpi_communicator, locally_relevant_dofs);
+
+    dXv_dXp.reinit(volume_range, volume_range, dsp, this->mpi_communicator);
+
+    for(unsigned int i=0; i<n_vol_nodes; ++i)
+    {
+        if(!volume_range.is_element(i)) continue;
+        dXv_dXp.set(i,i,1.0);
+    }
+
+    dXv_dXp.compress(dealii::VectorOperation::insert);
+}
+
+template<int dim>
+bool DesignParameterizationIdentity<dim> ::update_mesh_from_design_variables(
+    const MatrixType &dXv_dXp,
+    const VectorType &design_var)
+{
+    AssertDimension(dXv_dXp.n(), design_var.size());
+    
+    // check if design variables have changed.
+    VectorType diff = design_var;
+    diff -= current_volume_nodes;
+    bool design_variable_has_changed = this->has_design_variable_been_updated(current_volume_nodes, design_var);
+    bool mesh_updated;
+    if(!(design_variable_has_changed))
+    {
+        mesh_updated = false;
+        return mesh_updated;
+    }
+
+    current_volume_nodes = design_var;
+    dXv_dXp.vmult(this->high_order_grid->volume_nodes, design_var);
+    mesh_updated = true;
+    return mesh_updated;
+}
+
+template<int dim>
+unsigned int DesignParameterizationIdentity<dim> :: get_number_of_design_variables() const
+{
+    return this->high_order_grid->volume_nodes.size();
+}
+
+template class DesignParameterizationIdentity<PHILIP_DIM>;
+} // namespace PHiLiP
---
+++ src/optimization/design_parameterization/design_parameterization_identity.hpp
@@ -0,0 +1,40 @@
+#ifndef __DESIGN_PARAMETERIZATION_IDENTITY_H__
+#define __DESIGN_PARAMETERIZATION_IDENTITY_H__
+
+#include "design_parameterization_base.hpp"
+
+namespace PHiLiP {
+
+/// Identity design parameterization. Control variables are all volume nodes.
+template<int dim>
+class DesignParameterizationIdentity : public DesignParameterizationBase<dim> {
+public:
+    /// Constructor
+    DesignParameterizationIdentity(
+        std::shared_ptr<HighOrderGrid<dim,double>> _high_order_grid); 
+    
+    /// Destructor
+    ~DesignParameterizationIdentity() override {} 
+    
+    /// Initializes design variables with volume nodes and set locally owned and ghost indices. Overrides the virtual function in base class.
+    void initialize_design_variables(VectorType &design_var) override;
+    
+    /// Computes the derivative of volume nodes w.r.t. design parameters. Overrides the virtual function in base class.
+    /** As the volume nodes are the design parameters, dXv_dXp is identity.
+     */
+    void compute_dXv_dXp(MatrixType &dXv_dXp) const override;
+    
+    /// Checks if the design variables have changed and updates volume nodes. 
+    bool update_mesh_from_design_variables(
+        const MatrixType &dXv_dXp,
+        const VectorType &design_var) override;
+
+    /// Returns the number of design variables (no. of volume nodes in this case).
+    unsigned int get_number_of_design_variables() const override;
+
+private:
+    VectorType current_volume_nodes;
+};
+
+} // namespace PHiLiP
+#endif

86481c243f0df247b171c39ec82204fac1deba65
PranshulThakur <pranshul777@gmail.com>
2022-09-24 17:01:03 -0400

    Fixed comment

--- src/optimization/design_parameterization/inner_vol_parameterization.cpp
+++ src/optimization/design_parameterization/inner_vol_parameterization.cpp
@@ -142,7 +142,7 @@ bool DesignParameterizationInnerVol<dim> ::update_mesh_from_design_variables(
     change_in_des_var -= current_design_var;
 
     current_design_var = design_var;
-    dXv_dXp.vmult_add(this->high_order_grid->volume_nodes, change_in_des_var); // Xv = Xv + dXv_dXp*(Xv,new - Xv); Gives Xv for surface nodes and Xv,new for inner vol nodes. 
+    dXv_dXp.vmult_add(this->high_order_grid->volume_nodes, change_in_des_var); // Xv = Xv + dXv_dXp*(Xp,new - Xp); Gives Xv for surface nodes and Xp,new for inner vol nodes. 
     mesh_updated = true;
     return mesh_updated;
 }

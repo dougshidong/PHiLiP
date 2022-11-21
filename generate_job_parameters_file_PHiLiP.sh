da8037973cc085d8248f1ddd0f6e2e8698bbc97a
Julien Brillon <43619715+jbrillon@users.noreply.github.com>
2022-11-21 11:22:48 -0500

    Update to job submission script (#179)
    * updating job submission script with memory per node parameter for faster queue times
    
    * Update generate_job_parameters_file_PHiLiP.sh
    
    * updating compile script for faster queue times

--- generate_job_parameters_file_PHiLiP.sh
+++ generate_job_parameters_file_PHiLiP.sh
@@ -1,10 +1,11 @@
 # This file quickly generates the job paramters file
 
 filename="my_job_parameters_file.sh"
-time="2:00:00" # wall time
+walltime="2:00:00" # wall time
 job_name="run_parameters_file" # slurm job name
 nodes=1 # number of nodes
-ntasks_per_node=40 # number of processors per node; refer to https://docs.computecanada.ca/wiki/Advanced_MPI_scheduling
+ntasks_per_node=16 # number of processors per node; refer to https://docs.computecanada.ca/wiki/Advanced_MPI_scheduling
+memory_per_node="63G" # requested memory per node; NOTE: Must correspond to ntasks_per_node*memory_per_core; refer to https://docs.computecanada.ca/wiki/Advanced_MPI_scheduling
 user_email="firstname.lastname@mail.mcgill.ca" # for receiving email notifications
 compute_canada_username="username"
 parameters_file="my_parameters_file.prm" # input parameter file for PHiLiP
@@ -21,7 +22,7 @@ run_on_temp_dir=false
 
 source ./create_job_parameters_file.sh \
 ${filename} \
-${time} \
+${walltime} \
 ${job_name} \
 ${nodes} \
 ${ntasks_per_node} \
@@ -29,4 +30,5 @@ ${user_email} \
 ${compute_canada_username} \
 ${parameters_file} \
 ${PHiLiP_DIM} \
-${run_on_temp_dir}
+${run_on_temp_dir} \
+${memory_per_node}

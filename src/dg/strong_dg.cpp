9443afa56d6d74f369c62211af5d24c93de0d67b
Carolyn Pethrick <94074298+cpethrick@users.noreply.github.com>
2023-02-16 16:50:47 -0500

    Update src/dg/strong_dg.cpp

--- src/dg/strong_dg.cpp
+++ src/dg/strong_dg.cpp
@@ -1725,7 +1725,7 @@ void DGStrong<dim,nstate,real,MeshType>::assemble_face_term_strong(
                          : (abs(unit_ref_normal_int[1]) >= 1e-11 ? 1 
                          : (abs(unit_ref_normal_int[2]) >= 1e-11 ? 2 : 1000));
     }else{
-      pcout<<"Error with normals. Assume dim <=3."<<std::endl;
+      pcout<<"Error with normals. Assume dim <=3. Aborting..."<<std::endl;
       std::abort();
     }
     const int dim_not_zero = dim_not_zero_probe;

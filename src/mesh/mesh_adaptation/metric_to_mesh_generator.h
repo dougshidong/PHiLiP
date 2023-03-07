395b6b17f45735169fe9ebff4379b9e6bc38e4cb
PranshulThakur <pranshul777@gmail.com>
2023-03-07 15:47:31 -0500

    Created a basic structure of metric_to_mesh_generator.

---
+++ src/mesh/mesh_adaptation/metric_to_mesh_generator.h
@@ -0,0 +1,66 @@
+#ifndef __METRIC_TO_MESH_GENERATOR_H__
+#define __METRIC_TO_MESH_GENERATOR_H__
+
+#include <deal.II/fe/mapping_fe_field.h>
+#include <deal.II/distributed/tria.h>
+#include <deal.II/grid/tria.h>
+
+namespace PHiLiP {
+/// Class to convert metric field to mesh using BAMG.
+#if PHILIP_DIM==1 // dealii::parallel::distributed::Triangulation<dim> does not work for 1D
+template <int dim, int nstate, typename real, typename MeshType = dealii::Triangulation<dim>>
+#else
+template <int dim, int nstate, typename real, typename MeshType = dealii::parallel::distributed::Triangulation<dim>>
+#endif
+class MetricToMeshGenerator {
+
+	using VectorType = dealii::LinearAlgebra::distributed::Vector<double>; ///< Alias for dealii's parallel distributed vector.
+	using DoFHandlerType = dealii::DoFHandler<PHILIP_DIM>; ///< Alias for declaring DofHandler
+
+public:
+	/// Constructor.
+	MetricToMeshGenerator(
+		std::shared_ptr<dealii::MappingFEField<dim,dim,VectorType,DoFHandlerType>> _volume_nodes_mapping,
+		std::shared_ptr<MeshType> _triangulation);
+
+	/// Destructor
+	~MetricToMeshGenerator(){};
+	
+	/// Interpolates cellwise metric field to vertices.
+	void interpolate_metric_to_vertices(const std::vector<dealii::Tensor<2, dim, real>> &cellwise_optimal_metric);
+
+private:
+	/// DoFHandler to get global index of vertices.
+	dealii::DoFHandler<PHILIP_DIM> dof_handler_vertices;
+
+	/// Continuous FE of degree 1. 
+	const dealii::FE_Q<dim> fe_q;
+
+	///FESystem for vertices, created with nstate = 1 to relate an entire vertex of size dim by a single dof.
+	const dealii::FESystem<dim> fe_system;
+
+	/// Stores optimal metric at vertices
+	std::vector<dealii::Tensor<2, dim, real>> optimal_metric_at_vertices;
+
+    /// Alias for MPI_COMM_WORLD
+    MPI_Comm mpi_communicator;
+    
+    /// std::cout only on processor #0.
+    dealii::ConditionalOStream pcout;
+
+    /// Processor# of current processor.
+    int mpi_rank;
+
+    /// Total no. of processors
+    int n_mpi;
+	
+	/// Mapping field to update physical quadrature points, jacobians etc with the movement of volume nodes.
+	std::shared_ptr<dealii::MappingFEField<dim,dim,VectorType,DoFHandlerType>> volume_nodes_mapping,
+
+	/// Stores triangulation currently in use.
+	std::shared_ptr<MeshType> triangulation;
+
+}; // class ends
+
+} // PHiLiP namespace
+#endif

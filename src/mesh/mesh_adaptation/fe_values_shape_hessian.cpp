2d6a4e672bfab00a3d67970e34ae988a79c1d9e0
PranshulThakur <pranshul777@gmail.com>
2023-03-22 14:53:44 -0400

    Added equations to transform reference shape hessians to phyical ones.

--- src/mesh/mesh_adaptation/fe_values_shape_hessian.cpp
+++ src/mesh/mesh_adaptation/fe_values_shape_hessian.cpp
@@ -3,10 +3,82 @@
 namespace PHiLiP {
 
 template<int dim>
-dealii::Tensor<2,dim,double> FEValuesShapeHessian<dim> :: shape_hessian_component()
+void FEValuesShapeHessian<dim> ::  reinit(const dealii::FEValues<dim,dim> &fe_values_volume, const unsigned int iquad)
 {
-    dealii::Tensor<2,dim,double> zero_tensor;
-    return zero_tensor;
+    jacobian_inverse = dealii::Tensor<2,dim,double>(fe_values_volume.inverse_jacobian(iquad));
+    jacobian_inverse_transpose = dealii::transpose(jacobian_inverse);
+    const dealii::Tensor<2,dim,double> jacobian = dealii::Tensor<2,dim,double>(fe_values_volume.jacobian(iquad));
+    const dealii::Tensor<3,dim,double> jacobian_pushed_forward_grad = fe_values_volume.jacobian_pushed_forward_grad(iquad);
+    std::cout<<"Jacobian = "<<jacobian<<std::endl;
+    std::cout<<"Inverse jacobian = "<<jacobian_inverse<<std::endl;
+    std::cout<<"Inverse jacobian transpose = "<<jacobian_inverse_transpose<<std::endl;
+    std::cout<<"Jacobian pushed forward grad = "<<jacobian_pushed_forward_grad<<std::endl;
+    
+    ref_point = fe_values_volume.get_quadrature().point(iquad);
+
+    // Compute derivative of jacobian inverse w.r.t. physical coordinates.
+    // Using equation from dealii's documentation on Mapping, https://www.dealii.org/current/doxygen/deal.II/classMapping.html.
+    derivative_jacobian_inverse_wrt_phys_q = 0;
+    for(unsigned int i=0; i<dim; ++i)
+    {
+        for(unsigned int j=0; j<dim; ++j)
+        {
+            for(unsigned int k=0; k<dim; ++k)
+            {
+                for(unsigned int n=0; n<dim; ++n)
+                {
+                    derivative_jacobian_inverse_wrt_phys_q[i][j][k] -= jacobian_pushed_forward_grad[n][i][k]*jacobian_inverse[n][j];
+                }
+            }
+        }
+    }
+
+}
+
+template<int dim>
+dealii::Tensor<2,dim,double> FEValuesShapeHessian<dim> :: shape_hessian_component(
+        const unsigned int idof, 
+        const unsigned int /*iquad*/, 
+        const unsigned int istate, 
+        const dealii::FESystem<dim,dim> &fe_ref)
+{
+    dealii::Tensor<1,dim,double> shape_grad_ref = fe_ref.shape_grad_component(idof, ref_point, istate);
+    dealii::Tensor<2,dim,double> shape_hessian_ref = fe_ref.shape_grad_grad_component(idof, ref_point, istate);
+
+    // Compute first term: J^{-T} \varphi_{\epsilon \epsilon} J^{-1}.
+    // Using (A^T*B*A)_{i,j} = a_{ki} * b_{kl} * a_{lj} (in index notation).
+    dealii::Tensor<2,dim,double> phys_hessian_term1; // initialized to 0
+    for(unsigned int i=0; i<dim; ++i)
+    {
+        for(unsigned int j=0; j<dim; ++j)
+        {
+            for(unsigned int k=0; k<dim; ++k)
+            {
+                for(unsigned int l=0; l<dim; ++l)
+                {
+                    phys_hessian_term1[i][j] += jacobian_inverse[k][i] * shape_hessian_ref[k][l] * jacobian_inverse[l][j];
+                }
+            }
+        }
+    }
+
+    // Compute second term: \varphi_{\epsilon}^T * (J^{-1})_x
+    // Using v^T*E_xx (i,j)  =  v_k (d^2 E_k/(dx_i dx_j) = v_k E_xx(k,i,j), with E_xx a third order tensor and v a vector.
+    dealii::Tensor<2,dim,double> phys_hessian_term2; // initilized to 0
+    for(unsigned int i=0; i<dim; ++i)
+    {
+        for(unsigned int j=0; j<dim; ++j)
+        {
+            for(unsigned int k=0; k<dim; ++k)
+            {
+                phys_hessian_term2[i][j] += shape_grad_ref[k] * derivative_jacobian_inverse_wrt_phys_q[k][i][j];
+            }
+        }
+    }
+
+    dealii::Tensor<2,dim,double> shape_hessian_phys = phys_hessian_term1;
+    shape_hessian_phys += phys_hessian_term2;
+    return shape_hessian_phys;
 }
 
 template class FEValuesShapeHessian<PHILIP_DIM>;
--- src/mesh/mesh_adaptation/fe_values_shape_hessian.h
+++ src/mesh/mesh_adaptation/fe_values_shape_hessian.h
@@ -2,6 +2,7 @@
 #define __FE_VALUES_SHAPE_HESSIAN_H__
 
 #include <deal.II/fe/fe_values.h>
+#include <deal.II/fe/fe_system.h>
 
 namespace PHiLiP {
 /// Class to evaluate hessians of shape functions w.r.t. physical quadrature points.
@@ -15,21 +16,30 @@ public:
     /// Destructor
     ~FEValuesShapeHessian() {};
     
-    /// Store inverse jacobian and 3rd order tensors which are common at a quadrature point.
-    void reinit();
+    /// Store inverse jacobian and 3rd order tensors which will be the same for a combination of cell/physical quadrature point.
+    /** @note Currently, this class is designed to compute hessians at only one quadrature point, as required for computing hessians to get optimal metric field.
+     */
+    void reinit(const dealii::FEValues<dim,dim> &fe_values_volume, const unsigned int iquad); 
 
     /// Evaluates hessian of shape functions w.r.t. phyical quadrature points.
-    dealii::Tensor<2,dim,double> shape_hessian_component();
+    dealii::Tensor<2,dim,double> shape_hessian_component(
+        const unsigned int idof, 
+        const unsigned int iquad, 
+        const unsigned int istate, 
+        const dealii::FESystem<dim,dim> &fe_ref);
 
 private:
     /// Stores inverse jacobian of mapping betwenn reference and physical cell.
-    dealii::Tensor<2,dim,double> inverse_jacobian;
+    dealii::Tensor<2,dim,double> jacobian_inverse;
     
     /// Stores transpose of inverse jacobian of mapping betwenn reference and physical cell.
-    dealii::Tensor<2,dim,double> inverse_jacobian_transpose;
+    dealii::Tensor<2,dim,double> jacobian_inverse_transpose;
 
     /// Stores derivative of the jacobian inverse w.r.t. physical quadrature point.
-    dealii::Tensor<3,dim,double> derivative_jacobian_inverse_wrt_phys_q; 
+    dealii::Tensor<3,dim,double> derivative_jacobian_inverse_wrt_phys_q;
+    
+    /// Stores reference quadrature point.
+    dealii::Point<dim> ref_point;
 };
 } // PHiLiP namespace
 #endif

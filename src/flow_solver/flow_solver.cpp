f0ff082fc58af43408be861c78610722a6b6fa28
Julien Brillon <43619715+jbrillon@users.noreply.github.com>
2023-05-01 13:34:19 -0400

    adding allocate_AD_matrices flag

--- src/flow_solver/flow_solver.cpp
+++ src/flow_solver/flow_solver.cpp
@@ -42,10 +42,12 @@ FlowSolver<dim, nstate>::FlowSolver(
 , dg(DGFactory<dim,double>::create_discontinuous_galerkin(&all_param, poly_degree, flow_solver_param.max_poly_degree_for_adaptation, grid_degree, flow_solver_case->generate_grid()))
 {
     flow_solver_case->set_higher_order_grid(dg);
-    if (flow_solver_param.steady_state == false) {
-        dg->allocate_system(false,false,false);
+    if (ode_param.allocate_AD_matrices) {
+        pcout << "Note: Allocating DG with AD matrices." << std::endl;
+        dg->allocate_system(true,false,false); // FlowSolver only requires dRdW to be allocated
     } else {
-        dg->allocate_system(true,false,false);
+        pcout << "Note: Allocating DG without AD matrices." << std::endl;
+        dg->allocate_system(false,false,false);
     }
 
     if(ode_param.ode_solver_type == Parameters::ODESolverParam::pod_galerkin_solver || ode_param.ode_solver_type == Parameters::ODESolverParam::pod_petrov_galerkin_solver){

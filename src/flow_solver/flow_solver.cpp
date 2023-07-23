9b755b50755c09445442ee8dd031f8a4b6aed192
Julien Brillon <43619715+jbrillon@users.noreply.github.com>
2023-07-23 12:21:39 -0400

    progress towards fixing bug with restart file dump every delta t

--- src/flow_solver/flow_solver.cpp
+++ src/flow_solver/flow_solver.cpp
@@ -437,6 +437,9 @@ int FlowSolver<dim,nstate>::run() const
         //----------------------------------------------------
 #if PHILIP_DIM>1
         double current_desired_time_for_output_restart_files_every_dt_time_intervals = ode_solver->current_time; // when used, same as the initial time
+        while(current_desired_time_for_output_restart_files_every_dt_time_intervals <= ode_solver->current_time) {
+            current_desired_time_for_output_restart_files_every_dt_time_intervals += flow_solver_param.output_restart_files_every_dt_time_intervals;
+        }
 #endif
         //----------------------------------------------------
         // Initialize time step

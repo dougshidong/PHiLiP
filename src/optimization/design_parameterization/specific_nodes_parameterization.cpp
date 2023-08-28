ac1be1184a1424ac9cdb0f368c1f33692ac5784d
PranshulThakur <pranshul777@gmail.com>
2023-08-28 11:31:53 -0400

    Changed specific node parameterization to allow for nodes along 4 lines.

--- src/optimization/design_parameterization/specific_nodes_parameterization.cpp
+++ src/optimization/design_parameterization/specific_nodes_parameterization.cpp
@@ -27,6 +27,14 @@ void SpecificNodesParameterization<dim> :: compute_control_index_to_vol_index()
     // Get locally owned volume and surface ranges of indices held by current processor.
     const dealii::IndexSet &volume_range = this->high_order_grid->volume_nodes.get_partitioner()->locally_owned_range();
     const dealii::IndexSet &surface_range = this->high_order_grid->surface_nodes.get_partitioner()->locally_owned_range();
+    
+    dealii::Point<dim> A_right, B_right, C_right, D_left, E_left, F_left;
+    A_right[0] = -0.107; A_right[1] = 0.0;
+    B_right[0] = 0.2; B_right[1] = 3.0;
+    C_right[0] = 0.2; C_right[1] = -3.0;
+    D_left[0] = 1.0; D_left[1] = 0.0;
+    E_left[0] = 3.0; E_left[1] = 3.0;
+    F_left[0] = 3.0; F_left[1] = -3.0;
 
     for(unsigned int i_vol = 0; i_vol<n_vol_nodes; ++i_vol) 
     {
@@ -46,14 +54,12 @@ void SpecificNodesParameterization<dim> :: compute_control_index_to_vol_index()
             const double x = this->high_order_grid->volume_nodes(i_vol);
             const double y = this->high_order_grid->volume_nodes(i_vol+1);
 
-            dealii::Point<dim> A_right, B_right, C_right;
-            A_right[0] = -0.107; A_right[1] = 0.0;
-            B_right[0] = 0.2; B_right[1] = 3.0;
-            C_right[0] = 0.2; C_right[1] = -3.0;
             const bool is_part_of_line1 = check_if_node_belongs_to_the_line(A_right, B_right, x, y);
             const bool is_part_of_line2 = check_if_node_belongs_to_the_line(A_right, C_right, x, y);
+            const bool is_part_of_line3 = check_if_node_belongs_to_the_line(D_left, E_left, x, y);
+            const bool is_part_of_line4 = check_if_node_belongs_to_the_line(D_left, F_left, x, y);
 
-            if( is_part_of_line1 || is_part_of_line2 )
+            if( is_part_of_line1 || is_part_of_line2 || is_part_of_line3 || is_part_of_line4 )
             {
                 is_a_control_node(i_vol) = 1;
                 is_a_control_node(i_vol+1) = 1;

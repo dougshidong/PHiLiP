9d421c48952e4c04116d17c6e731ac13499a9ec2
PranshulThakur <pranshul777@gmail.com>
2023-09-13 10:45:13 -0400

    Updated parameterization for high order nodes.

--- src/optimization/design_parameterization/specific_nodes_parameterization.hpp
+++ src/optimization/design_parameterization/specific_nodes_parameterization.hpp
@@ -33,10 +33,9 @@ public:
     
     void compute_control_index_to_vol_index();
 
-    bool check_if_node_belongs_to_the_line(
-        const dealii::Point<dim> &start_point, 
-        const dealii::Point<dim> &end_point,
-        const double x, const double y) const;
+    bool check_if_node_belongs_to_the_region_between_lines(
+        const double x, 
+        const double y) const;
 
 private:
     

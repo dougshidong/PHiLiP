b3e49a46b680fe0bb1a23ad048a13fd8f572963e
dougshidong <doug.shidong@gmail.com>
2023-09-22 03:11:45 -0400

    Add documentation to LocalSolution.

--- src/solution/local_solution.hpp
+++ src/solution/local_solution.hpp
@@ -7,10 +7,14 @@
 
 template <typename real, int dim, int n_components>
 struct LocalSolution {
+    /// Solution coefficients in the finite element basis.
     std::vector<real> coefficients;
+    /// Reference to the finite element system used to represent the solution.
     const dealii::FESystem<dim, dim> &finite_element;
+    /// Number of degrees of freedom.
     const unsigned int n_dofs;
 
+    /// Constructor
     LocalSolution(const dealii::FESystem<dim, dim> &finite_element)
         : finite_element(finite_element), n_dofs(finite_element.dofs_per_cell) {
         Assert(n_components == finite_element.n_components(),
@@ -18,6 +22,7 @@ struct LocalSolution {
         coefficients.resize(finite_element.dofs_per_cell);
     }
 
+    /// Obtain values at unit points.
     std::vector<std::array<real, n_components>> evaluate_values(
         const std::vector<dealii::Point<dim>> &unit_points) const {
         const unsigned int n_points = unit_points.size();
@@ -37,6 +42,8 @@ struct LocalSolution {
         return values;
     }
 
+    /// Obtain reference gradients at unit points.
+    /// Note that the gradients are not physical since they do not include metric terms.
     std::vector<std::array<dealii::Tensor<1, dim, real>, n_components>> evaluate_reference_gradients(
         const std::vector<dealii::Point<dim>> &unit_points) const {
         const unsigned int n_points = unit_points.size();

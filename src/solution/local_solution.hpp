8065f72295ad57ec835d434572adebe4af573de0
dougshidong <doug.shidong@gmail.com>
2023-09-26 16:00:20 -0400

    Add DGBaseState files and move implementation of LocalSolution to .cpp.

--- src/solution/local_solution.hpp
+++ src/solution/local_solution.hpp
@@ -8,7 +8,8 @@
 namespace PHiLiP {
 
 template <typename real, int dim, int n_components>
-struct LocalSolution {
+class LocalSolution {
+   public:
     /// Solution coefficients in the finite element basis.
     std::vector<real> coefficients;
     /// Reference to the finite element system used to represent the solution.
@@ -17,56 +18,16 @@ struct LocalSolution {
     const unsigned int n_dofs;
 
     /// Constructor
-    LocalSolution(const dealii::FESystem<dim, dim> &finite_element)
-        : finite_element(finite_element), n_dofs(finite_element.dofs_per_cell) {
-        Assert(n_components == finite_element.n_components(),
-               dealii::ExcMessage("Number of components must match finite element system"));
-        coefficients.resize(finite_element.dofs_per_cell);
-    }
+    LocalSolution(const dealii::FESystem<dim, dim> &finite_element);
 
     /// Obtain values at unit points.
-    std::vector<std::array<real, n_components>> evaluate_values(
-        const std::vector<dealii::Point<dim>> &unit_points) const {
-        const unsigned int n_points = unit_points.size();
-        std::vector<std::array<real, n_components>> values(n_points);
-
-        for (unsigned int i_point = 0; i_point < n_points; ++i_point) {
-            for (int i_component = 0; i_component < n_components; ++i_component) {
-                values[i_point][i_component] = 0;
-            }
-            for (unsigned int i_dof = 0; i_dof < n_dofs; ++i_dof) {
-                const int i_component = finite_element.system_to_component_index(i_dof).first;
-                values[i_point][i_component] += coefficients[i_dof] * finite_element.shape_value_component(
-                                                                          i_dof, unit_points[i_point], i_component);
-            }
-        }
-
-        return values;
-    }
+    std::vector<std::array<real, n_components>> evaluate_values(const std::vector<dealii::Point<dim>> &unit_points) const;
 
     /// Obtain reference gradients at unit points.
     /// Note that the gradients are not physical since they do not include metric terms.
     std::vector<std::array<dealii::Tensor<1, dim, real>, n_components>> evaluate_reference_gradients(
-        const std::vector<dealii::Point<dim>> &unit_points) const {
-        const unsigned int n_points = unit_points.size();
-        std::vector<std::array<dealii::Tensor<1, dim, real>, n_components>> gradients(unit_points.size());
-
-        for (unsigned int i_point = 0; i_point < n_points; ++i_point) {
-            for (int i_component = 0; i_component < n_components; ++i_component) {
-                gradients[i_point][i_component] = 0;
-            }
-            for (unsigned int i_dof = 0; i_dof < n_dofs; ++i_dof) {
-                const int i_component = finite_element.system_to_component_index(i_dof).first;
-                dealii::Tensor<1, dim, double> shape_grad =
-                    finite_element.shape_grad_component(i_dof, unit_points[i_point], i_component);
-                for (int d = 0; d < dim; ++d) {
-                    gradients[i_point][i_component][d] += coefficients[i_dof] * shape_grad[d];
-                }
-            }
-        }
-        return gradients;
-    }
+        const std::vector<dealii::Point<dim>> &unit_points) const;
 };
 
-}
+}  // namespace PHiLiP
 #endif  // PHILIP_LOCAL_SOLUTION_HPP

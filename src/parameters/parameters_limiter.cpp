a370b61cd94204520132cf6ef15c63beef25bbb2
Shruthi Srinivasan <ssaishruthi98@gmail.com>
2023-09-28 13:38:47 -0400

    Modified indentation of the parameters_limiter file to make it more consistent

--- src/parameters/parameters_limiter.cpp
+++ src/parameters/parameters_limiter.cpp
@@ -11,36 +11,38 @@ void LimiterParam::declare_parameters (dealii::ParameterHandler &prm)
     prm.enter_subsection("limiter");
     {
         prm.declare_entry("use_OOA", "false",
-            dealii::Patterns::Bool(),
-            "Does not perform Convergence Test by default. Otherwise, performs Convergence Test.");
+                          dealii::Patterns::Bool(),
+                          "Does not perform Convergence Test by default. Otherwise, performs Convergence Test.");
 
         prm.declare_entry("bound_preserving_limiter", "none",
-            dealii::Patterns::Selection(
-                "none | "
-                "maximum_principle | "
-                "positivity_preservingZhang2010 | "
-                "positivity_preservingWang2012 "),
-            "The type of limiter we want to apply to the solution. "
-            "Choices are "
-            " <none | "
-            " maximum_principle | "
-            " positivity_preservingZhang2010 | "
-            " positivity_preservingWang2012>.");
+                           dealii::Patterns::Selection(
+                           "none | "
+                           "maximum_principle | "
+                           "positivity_preservingZhang2010 | "
+                           "positivity_preservingWang2012 "),
+                           "The type of limiter we want to apply to the solution. "
+                           "Choices are "
+                           " <none | "
+                           " maximum_principle | "
+                           " positivity_preservingZhang2010 | "
+                           " positivity_preservingWang2012>.");
 
         prm.declare_entry("pos_eps", "1e-13",
-            dealii::Patterns::Double(1e-20, 1e200),
-            "Lower bound for density used in Positivity-Preserving Limiter. Small value greater than zero, less than solution at all times.");
+                          dealii::Patterns::Double(1e-20, 1e200),
+                          "Lower bound for density used in Positivity-Preserving Limiter. Small value greater than zero, less than solution at all times.");
 
         prm.declare_entry("use_tvb_limiter", "false",
-            dealii::Patterns::Bool(),
-            "Applies TVB Limiter to solution. Tune M and h to obtain favourable results.");
+                          dealii::Patterns::Bool(),
+                          "Applies TVB Limiter to solution. Tune M and h to obtain favourable results.");
 
         prm.declare_entry("tvb_h", "1.0",
-            dealii::Patterns::Double(0, 1e200),
-            "Maximum delta_x.");
+                          dealii::Patterns::Double(0, 1e200),
+                          "Maximum delta_x.");
 
         using convert_tensor = dealii::Patterns::Tools::Convert<dealii::Tensor<1, 4, double>>;
-        prm.declare_entry("tvb_M", "0,0,0,0", *convert_tensor::to_pattern(), "TVB Limiter tuning parameters for each state.");
+        prm.declare_entry("tvb_M", "0,0,0,0", 
+                          *convert_tensor::to_pattern(), 
+                          "TVB Limiter tuning parameters for each state.");
     }
     prm.leave_subsection();
 }

ec080bfef5cbc47298e39588cadc1a8329cd5e7e
Julien Brillon <43619715+jbrillon@users.noreply.github.com>
2023-10-13 15:24:12 -0400

    progress towards DG strong channel flow

--- src/dg/strong_dg_les.cpp
+++ src/dg/strong_dg_les.cpp
@@ -344,6 +344,57 @@ void DGStrongLES_ShearImproved<dim,nstate,real,MeshType>::update_cellwise_mean_q
     this->pde_model_double->cellwise_mean_strain_rate_tensor_magnitude.update_ghost_values();
 }
 
+template<int dim, int nstate>
+double DGStrong_ChannelFlow<dim, nstate>::get_bulk_density() const
+{
+    double integral_value = 0.0;
+
+    // Overintegrate the error to make sure there is not integration error in the error estimate
+    int overintegrate = 10; // TO DO: could reduce this to reduce computational cost
+    dealii::QGauss<dim> quad_extra(this->max_degree+1+overintegrate);
+    dealii::FEValues<dim,dim> fe_values_extra(*(this->high_order_grid->mapping_fe_field), this->fe_collection[this->max_degree], quad_extra,
+                                              dealii::update_values /*| dealii::update_gradients*/ | dealii::update_JxW_values | dealii::update_quadrature_points);
+
+    const unsigned int n_quad_pts = fe_values_extra.n_quadrature_points;
+    std::array<double,nstate> soln_at_q;
+    // std::array<dealii::Tensor<1,dim,double>,nstate> soln_grad_at_q;
+
+    std::vector<dealii::types::global_dof_index> dofs_indices (fe_values_extra.dofs_per_cell);
+    for (auto cell : this->dof_handler.active_cell_iterators()) {
+        if (!cell->is_locally_owned()) continue;
+        fe_values_extra.reinit (cell);
+        cell->get_dof_indices (dofs_indices);
+
+        // double cellwise_integrand_value = 0.0;
+        for (unsigned int iquad=0; iquad<n_quad_pts; ++iquad) {
+
+            std::fill(soln_at_q.begin(), soln_at_q.end(), 0.0);
+            // for (int s=0; s<nstate; ++s) {
+            //     for (int d=0; d<dim; ++d) {
+            //         soln_grad_at_q[s][d] = 0.0;
+            //     }
+            // }
+            for (unsigned int idof=0; idof<fe_values_extra.dofs_per_cell; ++idof) {
+                const unsigned int istate = fe_values_extra.get_fe().system_to_component_index(idof).first;
+                soln_at_q[istate] += this->solution[dofs_indices[idof]] * fe_values_extra.shape_value_component(idof, iquad, istate);
+                // soln_grad_at_q[istate] += this->solution[dofs_indices[idof]] * fe_values_extra.shape_grad_component(idof,iquad,istate);
+            }
+            // const dealii::Point<dim> qpoint = (fe_values_extra.quadrature_point(iquad));
+
+            double integrand_value = soln_at_q[0]; // density
+            // cellwise_integrand_value += integrand_value * fe_values_extra.JxW(iquad);
+            integral_value += integrand_value * fe_values_extra.JxW(iquad);
+        }
+        // // get cell index
+        // const dealii::types::global_dof_index cell_index = cell->active_cell_index();
+        // const double cellwise_average = cellwise_integrand_value/this->pde_model_double->cellwise_volume[cell_index];
+        // integral_value += cellwise_average;
+    }
+    const double mpi_sum_integral_value = dealii::Utilities::MPI::sum(integral_value, this->mpi_communicator);
+    const double averaged_value = mpi_sum_integral_value/this->domain_volume;// volume division is accomplished cellwise
+    return averaged_value;
+}
+
 #if PHILIP_DIM==3
 template class DGStrongLES <PHILIP_DIM, PHILIP_DIM+2, double, dealii::Triangulation<PHILIP_DIM>>;
 template class DGStrongLES <PHILIP_DIM, PHILIP_DIM+2, double, dealii::parallel::shared::Triangulation<PHILIP_DIM>>;
--- src/dg/strong_dg_les.hpp
+++ src/dg/strong_dg_les.hpp
@@ -95,7 +95,45 @@ protected:
 
     using DGBase<dim,real,MeshType>::pcout; ///< Parallel std::cout that only outputs on mpi_rank==0
     
-}; // end of DGStrongLES class
+}; // end of DGStrongLES_ShearImproved class
+
+/// DGStrongLES_ShearImproved class templated on the number of state variables
+/*  Contains the functions that need to be templated on the number of state variables.
+ */
+#if PHILIP_DIM==1 // dealii::parallel::distributed::Triangulation<dim> does not work for 1D
+template <int dim, int nstate, typename real, typename MeshType = dealii::Triangulation<dim>>
+#else
+template <int dim, int nstate, typename real, typename MeshType = dealii::parallel::distributed::Triangulation<dim>>
+#endif
+class DGStrong_ChannelFlow: public DGStrong<dim, nstate, real, MeshType>
+{
+protected:
+    /// Alias to base class Triangulation.
+    using Triangulation = typename DGStrongLES<dim,nstate,real,MeshType>::Triangulation;
+
+public:
+    /// Constructor
+    DGStrong_ChannelFlow(
+        const Parameters::AllParameters *const parameters_input,
+        const unsigned int degree,
+        const unsigned int max_degree_input,
+        const unsigned int grid_degree_input,
+        const std::shared_ptr<Triangulation> triangulation_input);
+
+    /// Destructor
+    ~DGStrong_ChannelFlow();
+
+protected:
+    /// Update the cellwise mean quantities
+    void update_cellwise_mean_quantities() override;
+
+    // const bool do_compute_filtered_solution; ///< Flag to compute the filtered solution
+    // const bool apply_modal_high_pass_filter_on_filtered_solution; ///< Flag to apply modal high pass filter on the filtered solution
+    // const unsigned int poly_degree_max_large_scales; ///< For filtered solution; lower bound of high pass filter
+
+    using DGBase<dim,real,MeshType>::pcout; ///< Parallel std::cout that only outputs on mpi_rank==0
+    
+}; // end of DGStrong_ChannelFlow class
 
 } // PHiLiP namespace
 

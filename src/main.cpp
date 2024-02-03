013a28fe20a6755ac913d1897493a31deacc824a
Liki Ramos Matsuyama <132537734+Liki-RM@users.noreply.github.com>
2024-02-03 16:12:33 -0500

    adding TO DO

--- src/main.cpp
+++ src/main.cpp
@@ -60,7 +60,7 @@ int main (int argc, char *argv[])
         AssertDimension(all_parameters.dimension, PHILIP_DIM);
 
         const int max_dim = PHILIP_DIM;
-        const int max_nstate = 5;
+        const int max_nstate = 5; // TO DO: you need to update this when you increase nspecies
 
         if(all_parameters.run_type == PHiLiP::Parameters::AllParameters::RunType::flow_simulation) {
             std::unique_ptr<PHiLiP::FlowSolver::FlowSolverBase> flow_solver = PHiLiP::FlowSolver::FlowSolverFactory<max_dim,max_nstate>::create_flow_solver(&all_parameters,parameter_handler);

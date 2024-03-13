5c33b5dd78767f1f40da6413589521e45436bc2f
Dominic Roy <152512321+DominicRoy2@users.noreply.github.com>
2024-03-13 11:16:38 -0400

    Fixing header file

--- src/physics/euler.h
+++ src/physics/euler.h
@@ -226,6 +226,19 @@ public:
     ///
     /// Opposite of convert_primitive_to_conservative
     std::array<real,nstate> convert_primitive_to_conservative ( const std::array<real,nstate> &primitive_soln ) const;
+    
+    /** Obtain gradient of primitive variables from gradient of conservative variables */
+    template<typename real2>
+    std::array<dealii::Tensor<1,dim,real2>,nstate> 
+    convert_conservative_gradient_to_primitive_gradient_templated (
+        const std::array<real2,nstate> &conservative_soln,
+        const std::array<dealii::Tensor<1,dim,real2>,nstate> &conservative_soln_gradient) const;
+
+    /** Obtain gradient of primitive variables from gradient of conservative variables */
+    std::array<dealii::Tensor<1,dim,real>,nstate> 
+    convert_conservative_gradient_to_primitive_gradient (
+        const std::array<real,nstate> &conservative_soln,
+        const std::array<dealii::Tensor<1,dim,real>,nstate> &conservative_soln_gradient) const;
 
     /** Obtain gradient of conservative variables from gradient of primitive variables */
     std::array<dealii::Tensor<1,dim,real>,nstate> 

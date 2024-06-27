b4d3919e299adcc8449264c6e8a348135d5b2d3c
Shruthi <ssaishruthi98@gmail.com>
2024-06-27 18:26:07 -0400

    Made edits to the convergence tests to include L1 and L_inf errors

--- src/testing/bound_preserving_limiter_tests.h
+++ src/testing/bound_preserving_limiter_tests.h
@@ -41,7 +41,7 @@ private:
         double final_time) const;
 
     /// Calculate and return the L2 Error
-    double calculate_l2error(std::shared_ptr<DGBase<dim, double>> flow_solver_dg, const int poly_degree, const double final_time) const;
+    std::array<double,3> calculate_l_n_error(std::shared_ptr<DGBase<dim, double>> flow_solver_dg, const int poly_degree, const double final_time) const;
 };
 
 } // End of Tests namespace

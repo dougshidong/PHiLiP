48a61cc5db996d1210f6ed9241f672c5b4f2135f
jbrillon <43619715+jbrillon@users.noreply.github.com>
2024-07-06 12:44:27 -0400

    reducing threshold percent diff for WM check to 5 percent

--- src/testing/turbulent_channel_flow_skin_friction_check.cpp
+++ src/testing/turbulent_channel_flow_skin_friction_check.cpp
@@ -311,7 +311,7 @@ int TurbulentChannelFlowSkinFrictionCheck<dim, nstate>::run_test() const
              " (from friction Reynolds number value is: " << this->get_wall_shear_stress_from_friction_reynolds_number() << ")" << std::endl;
         const double percent_error_wall_shear_stress_wall_model = 100.0*abs(computed_wall_shear_stress_wall_model - expected_wall_shear_stress)/expected_wall_shear_stress;
         pcout << " - percent error is " << percent_error_wall_shear_stress_wall_model << " %" << std::endl;
-        if(percent_error_wall_shear_stress_wall_model > 30.0) {
+        if(percent_error_wall_shear_stress_wall_model > 5.0) {
             pcout << "Error: considerable difference between wall model shear stress and expected value." << std::endl;
             return 1;
         } else {

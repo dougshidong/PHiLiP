dougshidong/PHiLiP

commit 7028c9f02c01d1d0e8552783f2cea78dd738760b

Multicore Non-Negative Least Squares Code and Tests (#263)
# Multicore Non-Negative Least Squares Code and Tests
Pull request for the addition of allowing the Non-Negative Least Squares
(NNLS) to be run on multiple cores.
## NNLS_solver.cpp

- The input matrix and vector can now exist within multiple cores
- The output matrix will now have the map and distribution of
A.DomainMap()
- All calculations will still happen on one core, but this adds the
functionality of running NNLS with more than 1 core, speeding up other
areas of the code.

## helper_functions.cpp

- Fixed the epetra_to_eig_vec function to properly output the
Eigen::VectorXd to all cores
- Added eig_to_epetra_vec function to output an Epetra_Vector from data
in a Eigen::VectorXd

## 2D_inviscid_isentropic_vortex.prm
- Corrected the interpolate initial condition parameter

## NNLS_tests.cpp
- Implemented new helper_function
- Added new test for multicore NNLS

src/linear_solver/NNLS_solver.cpp
@@ -8,23 +8,16 @@ NNLS_solver::NNLS_solver(
   const dealii::ParameterHandler &parameter_handler_input,
   const Epetra_CrsMatrix &A, 
   Epetra_MpiComm &Comm, 
-  Epetra_Vector &b):
-    all_parameters(parameters_input),
-    parameter_handler(parameter_handler_input),
-    A_(A),
-    Comm_(Comm), 
-    b_(b), 
-    x_(A.ColMap()), 
-    LS_iter_(1000), 
-    LS_tol_(10E-8), 
-    Z(A_.NumMyCols()), 
-    P(A_.NumMyCols()), 
-    iter_solver_(false), 
-    grad_exit_crit_(false) 
-    {
-      index_set = Eigen::VectorXd::LinSpaced(A_.NumMyCols(), 0, A_.NumMyCols() -1); // Indices proceeding and including numInactive are in the P set (Inactive/Positive)
-      Z.flip(); // All columns begin in the Z set (Active)
-    }
+  Epetra_Vector &b): NNLS_solver(
+    parameters_input,
+    parameter_handler_input,
+    A,
+    Comm,
+    b,
+    false,
+    false,
+    1000,
+    10E-8) {}
 
 NNLS_solver::NNLS_solver(
   const Parameters::AllParameters *const parameters_input,
@@ -32,23 +25,16 @@ NNLS_solver::NNLS_solver(
   const Epetra_CrsMatrix &A, 
   Epetra_MpiComm &Comm, 
   Epetra_Vector &b,
-  bool grad_exit_crit):
-    all_parameters(parameters_input),
-    parameter_handler(parameter_handler_input),
-    A_(A),  
-    Comm_(Comm), 
-    b_(b), 
-    x_(A.ColMap()), 
-    LS_iter_(1000), 
-    LS_tol_(10E-8), 
-    Z(A_.NumMyCols()), 
-    P(A_.NumMyCols()), 
-    iter_solver_(false), 
-    grad_exit_crit_(grad_exit_crit) 
-    {
-      index_set = Eigen::VectorXd::LinSpaced(A_.NumMyCols(), 0, A_.NumMyCols() -1); // Indices proceeding and including numInactive are in the P set (Inactive/Positive)
-      Z.flip(); // All columns begin in the Z set (Active)
-    }
+  bool grad_exit_crit): NNLS_solver(
+    parameters_input,
+    parameter_handler_input,
+    A,
+    Comm,
+    b,
+    grad_exit_crit,
+    false,
+    1000,
+    10E-8) {}
 
 NNLS_solver::NNLS_solver(
   const Parameters::AllParameters *const parameters_input,
@@ -58,23 +44,16 @@ NNLS_solver::NNLS_solver(
   Epetra_Vector &b, 
   bool iter_solver, 
   int LS_iter, 
-  double LS_tol):
-    all_parameters(parameters_input),
-    parameter_handler(parameter_handler_input),
-    A_(A), 
-    Comm_(Comm), 
-    b_(b), 
-    x_(A.ColMap()), 
-    LS_iter_(LS_iter), 
-    LS_tol_(LS_tol),
-    Z(A_.NumMyCols()), 
-    P(A_.NumMyCols()), 
-    iter_solver_(iter_solver), 
-    grad_exit_crit_(false) 
-    {
-      index_set = Eigen::VectorXd::LinSpaced(A_.NumMyCols(), 0, A_.NumMyCols() -1); // Indices proceeding and including numInactive are in the P set (Inactive/Positive)
-      Z.flip(); // All columns begin in the Z set (Active)
-    }
+  double LS_tol): NNLS_solver(
+    parameters_input,
+    parameter_handler_input,
+    A,
+    Comm,
+    b,
+    false,
+    iter_solver,
+    LS_iter,
+    LS_tol) {}
 
 NNLS_solver::NNLS_solver(
   const Parameters::AllParameters *const parameters_input,
@@ -88,18 +67,19 @@ NNLS_solver::NNLS_solver(
   double LS_tol):
     all_parameters(parameters_input),
     parameter_handler(parameter_handler_input),
-    A_(A), 
     Comm_(Comm), 
-    b_(b), 
-    x_(A.ColMap()), 
+    A_(allocateMatrixToSingleCore(A)), 
+    b_(allocateVectorToSingleCore(b)), 
+    x_(A_.ColMap()), 
+    multi_x_(A.DomainMap()),
     LS_iter_(LS_iter), 
     LS_tol_(LS_tol),
-    Z(A_.NumMyCols()), 
-    P(A_.NumMyCols()), 
+    Z(A_.NumGlobalCols()), 
+    P(A_.NumGlobalCols()), 
     iter_solver_(iter_solver), 
     grad_exit_crit_(grad_exit_crit) 
     {
-      index_set = Eigen::VectorXd::LinSpaced(A_.NumMyCols(), 0, A_.NumMyCols() -1); // Indices proceeding and including numInactive are in the P set (Inactive/Positive)
+      index_set = Eigen::VectorXd::LinSpaced(A_.NumGlobalCols(), 0, A_.NumGlobalCols() -1); // Indices proceeding and including numInactive are in the P set (Inactive/Positive)
       Z.flip(); // All columns begin in the Z set (Active)
     }
 
@@ -108,9 +88,9 @@ void NNLS_solver::Epetra_PermutationMatrix(Epetra_CrsMatrix &P_mat){
   // No longer in use
   double posOne = 1.0;
   for(int i = 0; i < P_mat.NumMyCols(); i++){
-    int GlobalRow = P_mat.GRID(i);
+    int globalRow = P_mat.GRID(i);
     if (P[i] == 1) {
-      P_mat.InsertGlobalValues(GlobalRow, 1, &posOne , &i);
+      P_mat.InsertGlobalValues(globalRow, 1, &posOne , &i);
     }
   }
 }
@@ -148,14 +128,14 @@ void NNLS_solver::SubIntoX(Epetra_Vector &temp){
   // Substitute new values into the solution vector
   std::vector<int> colMap(A_.NumGlobalCols());
   int numCol = 0;
-  for(int j = 0; j < x_.GlobalLength(); j++){
+  for(int j = 0; j < x_.MyLength(); j++){
     int idx = index_set[j];
     if (P[idx] == 1) {
       colMap[idx] = numCol;
       numCol++;
     }
   }
-  for(int j = 0; j < x_.GlobalLength(); j++){
+  for(int j = 0; j < x_.MyLength(); j++){
     if (P[j] == 1) {
       x_[j] = temp[colMap[j]];
     }
@@ -166,14 +146,14 @@ void NNLS_solver::AddIntoX(Epetra_Vector &temp, double alpha){
   // Add vector temp time scalar alpha into the vector x
   std::vector<int> colMap(A_.NumGlobalCols());
   int numCol = 0;
-  for(int j = 0; j < x_.GlobalLength(); j++){
+  for(int j = 0; j < x_.MyLength(); j++){
     int idx = index_set[j];
     if (P[idx] == 1) {
       colMap[idx] = numCol;
       numCol++;
     }
   }
-  for(int j = 0; j < x_.GlobalLength(); j++){
+  for(int j = 0; j < x_.MyLength(); j++){
     if (P[j] == 1) {
       x_[j] += alpha*(temp[colMap[j]] -x_[j]);
     }
@@ -199,9 +179,9 @@ void NNLS_solver::moveToInactiveSet(int idx){
 }
 
 bool NNLS_solver::solve(){
+  const int rank = this->Comm_.MyPID();
   iter_ = 0;
   numInactive_ = 0;
-
   // Pre-mult by A^T
   Epetra_CrsMatrix AtA(Epetra_DataAccess::View, A_.ColMap(), A_.NumMyCols());
   EpetraExt::MatrixMatrix::Multiply(A_, true, A_, false, AtA);
@@ -213,42 +193,43 @@ bool NNLS_solver::solve(){
   Epetra_Vector Ax (A_.RowMap());
   Epetra_MultiVector gradient (A_.ColMap(), 1);
   Epetra_MultiVector residual (A_.RowMap(), 1);
-
   Eigen::VectorXd grad_eig (gradient.GlobalLength());
   Epetra_Vector grad_col (A_.ColMap());
 
   // OUTER LOOP
   while(true){
-    // Early exit if all variables are inactive, which breaks 'maxCoeff' below.
+    // Early exit if all variables are inactive, which breaks 'maxCoeff' below. 
     if (A_.NumGlobalCols() == numInactive_){
+      multi_x_ = allocateVectorToMultipleCores(this->x_);
       return true;
     }
-
     AtA.Multiply(false, x_, AtAx);
     gradient = Atb;
     gradient.Update(-1.0, AtAx, 1.0); // gradient = A^T * (b-A*x)
 
     grad_col = *gradient(0);
-    for(int i = 0; i < gradient.GlobalLength() ; ++i){
+    for(int i = 0; i < gradient.MyLength() ; ++i){
       grad_eig[i] = grad_col[i];
     }
-    
+
     // Find the maximum element of the gradient in the active set
     // Move that variable to the inactive set
     int numActive = A_.NumGlobalCols() - numInactive_;
     int argmaxGradient = -1;
-    const double maxGradient = grad_eig(index_set.tail(numActive)).maxCoeff(&argmaxGradient);
+    double maxGradient;
+    if(rank==0){
+      maxGradient = grad_eig(index_set.tail(numActive)).maxCoeff(&argmaxGradient);
+    }
+    MPI_Bcast(&maxGradient,1,MPI_DOUBLE,0,MPI_COMM_WORLD);
     argmaxGradient += numInactive_;
-
+    MPI_Bcast(&argmaxGradient,1, MPI_INT,0,MPI_COMM_WORLD);
     residual = b_;
     A_.Multiply(false, x_, Ax);
     residual.Update(-1.0, Ax, 1.0); // residual = b - A*x
     double normRes[1];
     residual.Norm2(normRes);
-
     double normb[1];
     b_.Norm2(normb);
-
     // Old exit condition dependent on the maxGradient
     // Original exit condition presented in "SOLVING LEAST SQUARES PROBLEMS", by Charles L. Lawson and
     // Richard J. Hanson, Prentice-Hall, 1974
@@ -260,6 +241,7 @@ bool NNLS_solver::solve(){
         std::cout << normb[0] << std::endl;
         std::cout << "Norm-2 of the residual (b-A*x)" << std::endl;
         std::cout << normRes[0] << std::endl;
+        multi_x_ = allocateVectorToMultipleCores(this->x_);
         return true;
       }
     }
@@ -274,27 +256,27 @@ bool NNLS_solver::solve(){
         std::cout << normb[0] << std::endl;
         std::cout << "Norm-2 of the residual (b-A*x)" << std::endl;
         std::cout << normRes[0] << std::endl;
+        multi_x_ = allocateVectorToMultipleCores(this->x_);
         return true;
       }
     }
     
     moveToInactiveSet(argmaxGradient);
 
-    // INNER LOOP
+    // INNER LOOP 
     bool no_feasible_soln = true;
     while(no_feasible_soln){
       // Check if max. number of iterations is reached
       if (iter_ >= all_parameters->hyper_reduction_param.NNLS_max_iter){
+        multi_x_ = allocateVectorToMultipleCores(this->x_);
         return false;
-      }
-
+      } 
       // Create matrix P_mat with columns from set P
-      Epetra_Map Map(A_.NumGlobalRows(),0,Comm_);
-      Epetra_Map ColMap(numInactive_,0,Comm_);
+      Epetra_Map Map(A_.NumGlobalRows(),(rank == 0) ?  A_.NumGlobalRows() : 0, 0, Comm_);
+      Epetra_Map ColMap(numInactive_,(rank == 0) ?  numInactive_ : 0, 0, Comm_);
       Epetra_CrsMatrix P_mat(Epetra_DataAccess::Copy, Map, numInactive_);
       PositiveSetMatrix(P_mat);
       P_mat.FillComplete(ColMap, Map);
-
       // Create temporary solution vector temp which is only the length of numInactive
       Epetra_Vector temp(P_mat.ColMap());
 
@@ -304,7 +286,6 @@ bool NNLS_solver::solve(){
 
       Epetra_Vector Ptb (P_mat.ColMap());
       P_mat.Multiply(true, b_, Ptb);
-
       // An iterative solver can be used by setting iter_solver_ to true
       if (iter_solver_){
         // Solve least-squares problem in inactive set only
@@ -338,18 +319,24 @@ bool NNLS_solver::solve(){
       bool feasible = true;
       double alpha = Eigen::NumTraits<Eigen::VectorXd::Scalar>::highest();
       int infeasibleIdx = -1;
-      for(int k = 0; k < numInactive_; k++){
-        int idx = index_set[k];
-        if (temp[k] < 0){
-          // t should always be in [0,1]
-          double t = -x_[idx]/(temp[k] - x_[idx]);
-          if (alpha > t){
-            alpha = t;
-            infeasibleIdx = k;
-            feasible = false;
+      if(rank == 0){ // Will only proceed on the root as the other cores do no have access to this information
+        for(int k = 0; k < numInactive_; k++){
+          int idx = index_set[k];
+          if (temp[k] < 0){
+            // t should always be in [0,1]
+            double t = -x_[idx]/(temp[k] - x_[idx]);
+            if (alpha > t){
+              alpha = t;
+              infeasibleIdx = k;
+              feasible = false;
+            }
           }
         }
       }
+      // Broadcast these variables so that the loop does not fail on other cores
+      MPI_Bcast(&feasible,1,MPI_CXX_BOOL,0,MPI_COMM_WORLD);
+      MPI_Bcast(&infeasibleIdx, 1, MPI_INT,0,MPI_COMM_WORLD);
+      MPI_Bcast(&alpha,1,MPI_DOUBLE,0,MPI_COMM_WORLD);
       eigen_assert(feasible || 0 <= infeasibleIdx);
 
       // If solution is feasible, exit to outer loop
@@ -365,8 +352,57 @@ bool NNLS_solver::solve(){
         moveToActiveSet(infeasibleIdx);
       }
     }
-    
   }
 }
 
+Epetra_CrsMatrix NNLS_solver::allocateMatrixToSingleCore(const Epetra_CrsMatrix &A){
+  // Gather Matrix Information
+  const int A_rows = A.NumGlobalRows();
+  const int A_cols = A.NumGlobalCols();
+  const int rank = Comm_.MyPID(); 
+  // Create new maps for one core and gather old maps
+  Epetra_Map single_core_row_A (A_rows, (rank == 0) ?  A_rows : 0, 0 , Comm_);
+  Epetra_Map single_core_col_A (A_cols, (rank == 0) ?  A_cols : 0, 0 , Comm_);
+  Epetra_Map old_row_map_A = A.RowMap();
+  Epetra_Map old_domain_map_A = A.DomainMap();
+  // Create Epetra_importer object
+  Epetra_Import A_importer(single_core_row_A,old_row_map_A);
+  // Create new A matrix
+  Epetra_CrsMatrix A_temp (Epetra_DataAccess::Copy, single_core_row_A, A_cols);
+  // Load the data from matrix A (Multi core) into A_temp (Single core)
+  A_temp.Import(A, A_importer, Epetra_CombineMode::Insert);
+  A_temp.FillComplete(single_core_col_A,single_core_row_A);
+  return A_temp;
+};
+
+Epetra_Vector NNLS_solver::allocateVectorToSingleCore(const Epetra_Vector &b){
+  // Gather Vector Information
+  const int rank = Comm_.MyPID();
+  const int b_size = b.GlobalLength();
+  // Create new map for one core and gather old map
+  Epetra_Map single_core_b (b_size, (rank == 0) ? b_size : 0, 0, Comm_);
+  Epetra_BlockMap old_map_b = b.Map();
+  // Create Epetra_importer object
+  Epetra_Import b_importer(single_core_b, old_map_b);
+  // Create new b vector
+  Epetra_Vector b_temp (single_core_b); 
+  // Load the data from vector b (Multi core) into b_temp (Single core)
+  b_temp.Import(b, b_importer, Epetra_CombineMode::Insert);
+  return b_temp;
+};
+
+Epetra_Vector NNLS_solver::allocateVectorToMultipleCores(const Epetra_Vector &c)
+{
+  // Create new multi core map and gather old single core map
+  Epetra_BlockMap old_map_c = c.Map();
+  Epetra_BlockMap multi_core_c = this->multi_x_.Map();
+  // Create Epetra_importer object
+  Epetra_Import c_importer (multi_core_c, old_map_c);
+  // Create new c vector
+  Epetra_Vector c_temp (multi_core_c);
+  // Load the data from vector c (Single core) into c_temp (Multicore)
+  c_temp.Import(c, c_importer, Epetra_CombineMode::Insert);
+  return c_temp;
+
+};
 } // PHiLiP namespace

src/linear_solver/NNLS_solver.h
@@ -1,9 +1,3 @@
-/* Non-Negagive Least Squares Solver for Epetra Structures
- * Based on the NNLS in Eigen unsupported and in MATLAB:
- * https://gitlab.com/libeigen/eigen/-/blob/master/unsupported/Eigen/NNLS
- * https://www.mathworks.com/help/matlab/ref/lsqnonneg.html
- */
-
 /** From Eigen NNLS:
  * Non-Negagive Least Squares Algorithm for Eigen.
  *
@@ -57,6 +51,7 @@
 #include <Epetra_CrsMatrix.h>
 #include <Epetra_Vector.h>
 #include <Epetra_MultiVector.h>
+#include <Epetra_Import.h>
 #include <Epetra_LinearProblem.h>
 #include <EpetraExt_MatrixMatrix.h>
 #include <AztecOO.h>
@@ -67,6 +62,12 @@
 
 using Eigen::Matrix;
 namespace PHiLiP {
+/**  Non-Negagive Least Squares Solver for Epetra Structures
+ *   Based on the NNLS in Eigen unsupported and in MATLAB:
+ *   https://gitlab.com/libeigen/eigen/-/blob/master/unsupported/Eigen/NNLS
+ *   https://www.mathworks.com/help/matlab/ref/lsqnonneg.html
+ */
+
 class NNLS_solver
 {
 public:
@@ -98,7 +99,7 @@ public:
         int LS_iter, 
         double LS_tol);
 
-    /// Constructor w/ Gradient Exit Condition & Iterative Linear Solver
+    /// Common Constructor w/ Gradient Exit Condition & Iterative Linear Solver
     NNLS_solver(
         const Parameters::AllParameters *const parameters_input,
         const dealii::ParameterHandler &parameter_handler_input,
@@ -117,12 +118,13 @@ public:
     bool solve();
 
     /// Returns protected approximate solution
-    Epetra_Vector & getSolution() {return x_;}
+    Epetra_Vector & getSolution() {return multi_x_;}
 
-    // Initiliazes the solution vector, must be used before .solve is called
+    /// Initiliazes the solution vector, must be used before .solve is called
     void startingSolution(Epetra_Vector &start) {
+        Epetra_Vector start_single_core = allocateVectorToSingleCore(start);
         P.flip();
-        SubIntoX(start);
+        SubIntoX(start_single_core);
         P.flip();}
   
     const Parameters::AllParameters *const all_parameters; ///< Pointer to all parameters
@@ -131,22 +133,37 @@ public:
     const dealii::ParameterHandler &parameter_handler;
 
 protected:
-    const Epetra_CrsMatrix A_;
+    /// Epetra Commuicator Object with MPI
     Epetra_MpiComm Comm_;
+    /// Epetra Matrix A allocated to a single core
+    const Epetra_CrsMatrix A_;
+    /// Epetra Vector b allocated to a single core
     Epetra_Vector b_;
+    /// Epetra Vector x, to be solved. Allocated to a single core
     Epetra_Vector x_;
-    int LS_iter_; // needed if the an iterative solver is used
-    double LS_tol_; // needed if the an iterative solver is used
+    /// Epetra_Vector x, to be solved. Allocated to multiple cores
+    Epetra_Vector multi_x_;
+    /// Needed if the an iterative solver is used
+    int LS_iter_;
+    /// Needed if the an iterative solver is used
+    double LS_tol_; 
+    /// Number of inactive points
     int numInactive_;
+    /// Vector of booleans representing the columns in the active set
     std::vector<bool> Z;
+    /// Vector of boolean representing the columns in the inactive set
     std::vector<bool> P;
 
 public:
+    /// Boolean used for iterative solvers
     bool iter_solver_;
+    /// Boolean to use an exit Condition depending on maximum gradent
     bool grad_exit_crit_;
+    /// Number of iterations in the NNLS solver
     int iter_;
 
 protected:
+    /// Eigen Vector of the index_set
     Eigen::VectorXd index_set;
 
 private:
@@ -167,6 +184,24 @@ private:
 
     /// Moves the column at idx into the inactive set (updating the index_set, Z, P, and numInactive_)
     void moveToInactiveSet(int idx);
+
+    /** @brief Re-allocates solution to multiple cores
+    *   @param c Epetra_vector to reallocate on multiple cores
+    *   @return An Epetra_vector with the data of c distributed on multiple cores
+    */
+    Epetra_Vector allocateVectorToMultipleCores(const Epetra_Vector &c);
+
+    /** @brief Allocates the incoming A Matrix to a single core
+    *   @param A Epetra_CrsMatrix to reallocate on a single core
+    *   @return An Epetra_CrsMatrix with the data of A distributed on a single core
+    */
+    Epetra_CrsMatrix allocateMatrixToSingleCore(const Epetra_CrsMatrix &A);
+    
+    /** @brief Allocates the incoming b vector to a single core
+    *   @param b Epetra_Vector to reallocate on a single core
+    *   @return An Epetra_Vector with the data of b distributed on a single core
+    */
+    Epetra_Vector allocateVectorToSingleCore(const Epetra_Vector &b);
 };
 } // PHiLiP namespace
 #endif  // NNLS_H

src/linear_solver/helper_functions.cpp
@@ -14,29 +14,51 @@
 #include <EpetraExt_MatrixMatrix.h>
 
 using namespace Eigen;
-/// @brief Fills the entries in an empty Eigen::MatrixXd from an Epetra_Vector structure
-/// @param col length of the vector 
-/// @param x Full Epetra vector to copy
-/// @param x_eig Empty Eigen::MatrixXd
+/**  @brief Fills the entries in an empty Eigen::MatrixXd from an Epetra_Vector structure
+*    @param col length of the vector 
+*    @param x Full Epetra vector to copy
+*    @param x_eig Empty Eigen::MatrixXd
+*/
 void epetra_to_eig_vec(int col, Epetra_Vector &x, Eigen::MatrixXd &x_eig){
+  // Gather local information
+  int local_size = x.MyLength();
+  const Epetra_Comm& comm = x.Comm();
+  int np = comm.NumProc();
+  
+  std::vector<double> x_values(col);
+  std::vector<int> local_sizes(np);
+  std::vector<int> displacements(np, 0);
+  // Gather all local_size into local_sizes. Vector will be the same globally
+  MPI_Allgather(&local_size, 1, MPI_INT,
+                local_sizes.data(), 1, MPI_INT,
+                MPI_COMM_WORLD);
+  // Calculate the global coordinates necessary
+  for (int i = 1; i < np; ++i){
+    displacements[i] = displacements[i-1] + local_sizes[i-1];
+  }
+  // Store the Epetra_vector values into x_values
+  MPI_Allgatherv(x.Values(), local_size, MPI_DOUBLE,
+                x_values.data(), local_sizes.data(), displacements.data(), MPI_DOUBLE,
+                MPI_COMM_WORLD);
   // Convert epetra vector to eigen vector
   for(int i = 0; i < col; i++){
-    x_eig(i,0) = x[i];
+    x_eig(i,0) = x_values[i];
   }
 }
 
-/// @brief Returns an Epetra_CrsMatrix with the entries from an Eigen::MatrixXd structure
-/// @param A_eig Full Eigen Matrix to copy
-/// @param col number of columns
-/// @param row number of rows
-/// @param Comm MpiComm for Epetra Maps
-/// @return Full Epetra_CrsMatrixss
+/**  @brief Returns an Epetra_CrsMatrix with the entries from an Eigen::MatrixXd structure
+*    @param A_eig Full Eigen Matrix to copy
+*    @param col number of columns
+*    @param row number of rows
+*    @param Comm MpiComm for Epetra Maps
+*    @return Full Epetra_CrsMatrix
+*/
 Epetra_CrsMatrix eig_to_epetra_matrix(Eigen::MatrixXd &A_eig, int col, int row, Epetra_MpiComm &Comm){
   // Create an empty Epetra structure with the right dimensions
   Epetra_Map RowMap(row,0,Comm);
   Epetra_Map ColMap(col,0,Comm);
   Epetra_CrsMatrix A(Epetra_DataAccess::Copy, RowMap, col);
-  const int numMyElements = RowMap.NumGlobalElements();
+  const int numMyElements = RowMap.NumMyElements();
 
   // Fill the Epetra_CrsMatrix from the Eigen::MatrixXd
   for (int localRow = 0; localRow < numMyElements; ++localRow){
@@ -61,4 +83,23 @@ MatrixXd epetra_to_eig_matrix(Epetra_CrsMatrix A_epetra){
       }
   }
   return A;
+}
+
+/** @brief Returns an Epetra_Vector with entries from an Eigen::Vector structure
+*   @param a_eigen Eigen Vector to copy
+*   @param size size of vector
+*   @param MpiComm for Epetra Maps
+*   @return Epetra_Vector
+*/
+Epetra_Vector eig_to_epetra_vector(Eigen::VectorXd &a_eigen, int size, Epetra_MpiComm &Comm){
+  // Create an Epetra Vector distributed along all cores in Comm
+  Epetra_Map vecMap(size,0,Comm);
+  Epetra_Vector a_epetra(vecMap);
+  // Fill the Epetra_Vector with values from the Eigen Vector
+  const int numMyElements = vecMap.NumMyElements();
+  for (int localElement = 0; localElement < numMyElements; localElement++){
+    const int globalElement = vecMap.GID(localElement);
+    a_epetra.ReplaceGlobalValues(1, &a_eigen(globalElement), &globalElement);
+  }
+  return a_epetra;
 }

src/linear_solver/helper_functions.h
@@ -32,6 +32,13 @@ void epetra_to_eig_vec(int col, Epetra_Vector &x, Eigen::MatrixXd &x_eig);
 /// @return Full Epetra_CrsMatrixss
 Epetra_CrsMatrix eig_to_epetra_matrix(Eigen::MatrixXd &A_eig, int col, int row, Epetra_MpiComm &Comm);
 
+/// @brief Returns an Epetra_Vector with entries from an Eigen::Vector structure
+/// @param a_eigen Eigen Vector to copy
+/// @param size size of vector
+/// @param MpiComm for Epetra Maps
+/// @return Epetra_Vector
+Epetra_Vector eig_to_epetra_vector(Eigen::VectorXd &a_eigen, int size, Epetra_MpiComm &Comm);
+
 /// @brief Load data from CSV file into an Eigen Matrix of type M
 /// @tparam M type of Eigen matrix
 /// @param path filepath to csv
@@ -59,5 +66,7 @@ M load_csv (const std::string & path) {
 }
 
 MatrixXd epetra_to_eig_matrix(Epetra_CrsMatrix A_epetra);
+
+
 
 #endif

tests/integration_tests_control_files/isentropic_vortex_integration/2D_inviscid_isentropic_vortex.prm
@@ -48,5 +48,5 @@ subsection flow_solver
     set grid_right_bound = 10.0
     set number_of_grid_elements_per_dimension = 32
   end
-  set interpolate_initial_condition = true
+  set apply_initial_condition_method = interpolate_initial_condition_function
 end

tests/unit_tests/linear_solver/CMakeLists.txt
@@ -55,6 +55,8 @@ COMMAND ${MPIGO} $<TARGET_FILE:Tests.exe> nIter)
 ADD_TEST(NAME NNLS_max_iter
 COMMAND ${MPIGO} $<TARGET_FILE:Tests.exe> maxIter)
 
+ADD_TEST(NAME NNLS_multi_core
+COMMAND mpirun -n ${MPIMAX} $<TARGET_FILE:Tests.exe> multiCore)
 
 #set(TEST_SRC
 #	NNLS_tests.cpp)

commit fad742c53f8f36aab77fc96c91b7f000cb36b98a

Small bugfix from PR #263 (#265)
Compiling the most recent version of master on my computer gave the
following error, introduced in PR #263:

![image](https://github.com/user-attachments/assets/5188f7c1-baae-40d6-b531-72a54ca78c95)

I removed the semicolons and can confirm that the code builds in both
release and debug mode.

src/linear_solver/NNLS_solver.cpp
@@ -355,7 +355,8 @@ bool NNLS_solver::solve(){
   }
 }
 
-Epetra_CrsMatrix NNLS_solver::allocateMatrixToSingleCore(const Epetra_CrsMatrix &A){
+Epetra_CrsMatrix NNLS_solver::allocateMatrixToSingleCore(const Epetra_CrsMatrix &A)
+{
   // Gather Matrix Information
   const int A_rows = A.NumGlobalRows();
   const int A_cols = A.NumGlobalCols();
@@ -373,9 +374,10 @@ Epetra_CrsMatrix NNLS_solver::allocateMatrixToSingleCore(const Epetra_CrsMatrix 
   A_temp.Import(A, A_importer, Epetra_CombineMode::Insert);
   A_temp.FillComplete(single_core_col_A,single_core_row_A);
   return A_temp;
-};
+}
 
-Epetra_Vector NNLS_solver::allocateVectorToSingleCore(const Epetra_Vector &b){
+Epetra_Vector NNLS_solver::allocateVectorToSingleCore(const Epetra_Vector &b)
+{
   // Gather Vector Information
   const int rank = Comm_.MyPID();
   const int b_size = b.GlobalLength();
@@ -389,7 +391,7 @@ Epetra_Vector NNLS_solver::allocateVectorToSingleCore(const Epetra_Vector &b){
   // Load the data from vector b (Multi core) into b_temp (Single core)
   b_temp.Import(b, b_importer, Epetra_CombineMode::Insert);
   return b_temp;
-};
+}
 
 Epetra_Vector NNLS_solver::allocateVectorToMultipleCores(const Epetra_Vector &c)
 {
@@ -404,5 +406,5 @@ Epetra_Vector NNLS_solver::allocateVectorToMultipleCores(const Epetra_Vector &c)
   c_temp.Import(c, c_importer, Epetra_CombineMode::Insert);
   return c_temp;
 
-};
+}
 } // PHiLiP namespace
de0b71534dacb628c5f9d67c9f5702659b3ef9a3
Carolyn <c.pethrick@gmail.com>
2025-10-09 13:56:51 -0400

    Change to main.cpp to use the sum of dimension and temporal_dimension to template PHiLiP

--- src/main.cpp
+++ src/main.cpp
@@ -57,12 +57,16 @@ int main (int argc, char *argv[])
         pcout << "Reading input..." << std::endl;
         all_parameters.parse_parameters (parameter_handler);
 
-        AssertDimension(all_parameters.dimension, PHILIP_DIM);
+        pcout << "HERE" << std::endl;
+
+        AssertDimension(all_parameters.dimension+all_parameters.temporal_dimension, PHILIP_DIM);
+        pcout << "HERE NOW" << std::endl;
 
         const int max_dim = PHILIP_DIM;
         const int max_nstate = 5;
 
         if(all_parameters.run_type == PHiLiP::Parameters::AllParameters::RunType::flow_simulation) {
+            pcout << "About to start flow simulation with max_dim = " << max_dim << std::endl;
             std::unique_ptr<PHiLiP::FlowSolver::FlowSolverBase> flow_solver = PHiLiP::FlowSolver::FlowSolverFactory<max_dim,max_nstate>::create_flow_solver(&all_parameters,parameter_handler);
             run_error = flow_solver->run();
             pcout << "Flow simulation complete with run error code: " << run_error << std::endl;

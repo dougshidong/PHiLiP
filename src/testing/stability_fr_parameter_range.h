36bde68dbbd99d4836465951e0ef64a7774eb80b
Carolyn <c.pethrick@gmail.com>
2025-10-30 09:40:02 -0400

    Modify fr c test to use the general_refinement_study framework

--- src/testing/stability_fr_parameter_range.h
+++ src/testing/stability_fr_parameter_range.h
@@ -1,7 +1,7 @@
 #ifndef __STABILITY_FR_PARAMETERS_RANGE_H__
 #define __STABILITY_FR_PARAMETERS_RANGE_H__
 
-#include "tests.h"
+#include "general_refinement_study.h"
 #include "dg/dg_base.hpp"
 #include "parameters/all_parameters.h"
 
@@ -10,14 +10,19 @@ namespace Tests {
 
 /// Burgers' periodic unsteady test
 template <int dim, int nstate>
-class StabilityFRParametersRange: public TestsBase
+class StabilityFRParametersRange: public GeneralRefinementStudy<dim,nstate>
 {
 public:
     /// Constructor
-    explicit StabilityFRParametersRange(const Parameters::AllParameters *const parameters_input);
+    explicit StabilityFRParametersRange(const Parameters::AllParameters *const parameters_input,
+            const dealii::ParameterHandler &parameter_handler_input);
 
     /// Run test
     int run_test () const override;
+    
+    /// Reinitialize parameters for the c loop.
+    Parameters::AllParameters reinit_params_c_value(const Parameters::AllParameters *parameters_in, const double c_value) const;
+
 };
 
 } // End of Tests namespace

81de3314dfc5c516b9fa585a27a09f4e6ca817a3
DominicRoy2 <152512321+DominicRoy2@users.noreply.github.com>
2025-11-07 12:19:52 -0500

    Updating lins in compile script

--- job_compile_PHiLiP_Rorqual.sh
+++ job_compile_PHiLiP_Rorqual.sh
@@ -4,8 +4,8 @@
 #SBATCH --job-name=compile_PHiLiP
 #SBATCH --output=%x-%j.out
 #SBATCH --nodes=1
-#SBATCH --ntasks-per-node=16                          ## <-- refer to https://docs.computecanada.ca/wiki/Advanced_MPI_scheduling
-#SBATCH --mem=63G                                     ## <-- total shared memory per node; refer to https://docs.computecanada.ca/wiki/Advanced_MPI_scheduling
+#SBATCH --ntasks-per-node=16                          ## <-- refer to https://docs.alliancecan.ca/wiki/Rorqual/en
+#SBATCH --mem=63G                                     ## <-- total shared memory per node; refer to https://docs.alliancecan.ca/wiki/Rorqual/en
 #SBATCH --mail-user=firstname.lastname@mail.mcgill.ca ## <-- for receiving job updates via email
 #SBATCH --mail-type=ALL                               ## <-- what kind of updates to receive by email
 
